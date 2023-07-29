c12820a74569c768032384d4a25a487cefc379f3
Bareq <bareqaltaamah@gmail.com>
2023-07-29 10:47:20 +0300

    add identity service

---
+++ service_identity/src/main/kotlin/org/thechance/service_identity/Application.kt
@@ -0,0 +1,20 @@
+package org.thechance.service_identity
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.thechance.service_identity.plugins.configureMonitoring
+import org.thechance.service_identity.plugins.configureRouting
+import org.thechance.service_identity.plugins.configureSerialization
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+}
