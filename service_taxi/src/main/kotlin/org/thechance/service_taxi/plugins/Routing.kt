5f883974fa8094761d0119157100d1f5cae42488
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-07-30 10:43:52 +0300

    solve package typo

--- service_taxi/src/main/kotlin/org/thechance/service_taxi/plugins/Routing.kt
+++ service_taxi/src/main/kotlin/org/thechance/service_taxi/plugins/Routing.kt
@@ -2,11 +2,11 @@ package org.thechance.service_taxi.plugins
 
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
-import org.thechance.service_taxi.endpoints.testRoutes
+import org.thechance.service_taxi.api.endpoints.taxiRoutes
 
 fun Application.configureRouting(
 ) {
     routing {
-        testRoutes()
+        taxiRoutes()
     }
 }
