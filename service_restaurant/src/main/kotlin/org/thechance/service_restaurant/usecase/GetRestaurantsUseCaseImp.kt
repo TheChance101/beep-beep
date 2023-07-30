601a5be4b794040f9878222d8fdd01ecfb1a78cd
Nada Feteiha <nada.feteiha@gmail.com>
2023-07-30 15:06:56 -0700

    rename Gateway

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/usecase/GetRestaurantsUseCaseImp.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/usecase/GetRestaurantsUseCaseImp.kt
@@ -5,7 +5,7 @@ import org.thechance.service_restaurant.api.usecases.GetRestaurantsUseCase
 import org.thechance.service_restaurant.usecase.gateway.RestaurantGateway
 
 @Single
-class GetRestaurantsUseCaseImp(private val restaurant: RestaurantGateway) : GetRestaurantsUseCase {
-    override suspend operator fun invoke() = restaurant.getRestaurants()
+class GetRestaurantsUseCaseImp(private val restaurantGateway: RestaurantGateway) : GetRestaurantsUseCase {
+    override suspend operator fun invoke() = restaurantGateway.getRestaurants()
 
 }
