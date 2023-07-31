b74545c48cd5c11b704a2e3d194fdb951e3c8a1d
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-08-01 01:55:27 +0300

    separate pagination route

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/api/endpoints/MealsRoutes.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/api/endpoints/MealsRoutes.kt
@@ -13,19 +13,23 @@ import org.thechance.service_restaurant.api.models.mappers.toEntity
 import org.thechance.service_restaurant.api.usecases.MealUseCasesContainer
 import org.thechance.service_restaurant.api.utils.extractInt
 import org.thechance.service_restaurant.api.utils.extractString
+import org.thechance.service_restaurant.data.collection.MealCollection
+import org.thechance.service_restaurant.data.collection.mapper.toEntity
 
 fun Route.mealRoutes() {
 
     val mealUseCasesContainer: MealUseCasesContainer by inject()
 
-    route("meal") {
-
-        get("/page/{page}") {
+    route("meals") {
+        get("/{page}") {
             val page = call.parameters.extractInt("page") ?: 1
             val limit = call.parameters.extractInt("limit") ?: 10
-            val meals = mealUseCasesContainer.getMealsUseCase(page , limit)
+            val meals = mealUseCasesContainer.getMealsUseCase(page, limit)
             call.respond(meals.toDto())
         }
+    }
+
+    route("meal") {
 
         get("/{id}") {
             val id = call.parameters.extractString("id") ?: throw NotFoundException("ID not provided")
