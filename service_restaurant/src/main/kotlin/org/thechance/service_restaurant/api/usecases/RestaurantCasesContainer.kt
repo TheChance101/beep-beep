66aae7020bd501cf2e0e23f12643e8f58cef1e9c
Nada Feteiha <nada.feteiha@gmail.com>
2023-07-30 22:27:44 -0700

    add restaurant use-cases interface

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/api/usecases/RestaurantCasesContainer.kt
+++
@@ -1,9 +0,0 @@
-package org.thechance.service_restaurant.api.usecases
-
-import org.koin.core.annotation.Single
-
-@Single
-data class RestaurantCasesContainer(
-    val getRestaurants: GetRestaurantsUseCase,
-    val addRestaurant: CreateRestaurantUseCase
-)
