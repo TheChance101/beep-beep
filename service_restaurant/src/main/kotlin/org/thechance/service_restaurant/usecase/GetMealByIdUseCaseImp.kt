eca17244fd41223f56b69b1dc43fb5eeeacb6403
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-07-31 09:59:03 +0300

    add get meal use case implementation

---
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/usecase/GetMealByIdUseCaseImp.kt
@@ -0,0 +1,13 @@
+package org.thechance.service_restaurant.usecase
+
+import io.ktor.server.plugins.*
+import org.koin.core.annotation.Single
+import org.thechance.service_restaurant.api.usecases.GetMealByIdUseCase
+import org.thechance.service_restaurant.entity.Meal
+import org.thechance.service_restaurant.usecase.gateway.MealGateway
+
+@Single
+class GetMealByIdUseCaseImp(private val mealGateway: MealGateway) : GetMealByIdUseCase {
+    override suspend fun invoke(id: String): Meal = mealGateway.getMealById(id) ?: throw BadRequestException(",,")
+
+}
