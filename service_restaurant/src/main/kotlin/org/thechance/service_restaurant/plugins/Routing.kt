4c7531c5437ddd98f65355d6d1464abb5d90535f
Nada Feteiha <nada.feteiha@gmail.com>
2023-07-31 12:39:09 -0700

    add Category gateway

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/plugins/Routing.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/plugins/Routing.kt
@@ -2,11 +2,13 @@ package org.thechance.service_restaurant.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
+import org.thechance.service_restaurant.api.endpoints.categoryRoutes
 import org.thechance.service_restaurant.api.endpoints.restaurantRoutes
 
 fun Application.configureRouting(
 ) {
     routing {
         restaurantRoutes()
+        categoryRoutes()
     }
 }
--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/usecase/gateway/RestaurantGateway.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/usecase/gateway/RestaurantGateway.kt
@@ -1,11 +1,23 @@
 package org.thechance.service_restaurant.usecase.gateway
 
+import org.thechance.service_restaurant.entity.Category
 import org.thechance.service_restaurant.entity.Restaurant
 
 interface RestaurantGateway {
 
+    //region Category
+    suspend fun getCategories(): List<Category>
+    suspend fun getCategory(categoryId: String): Category?
+    suspend fun addCategory(category: Category): Boolean
+    suspend fun updateCategory(category: Category): Boolean
+    suspend fun deleteCategory(categoryId: String): Boolean
+    //endregion
+
+    //region restaurant
     suspend fun getRestaurants(): List<Restaurant>
 
     suspend fun addRestaurant(name: String): Boolean
+
+    //endregion
 
 }
