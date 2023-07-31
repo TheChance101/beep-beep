cb406098d1bb6a038cf9802fc13c6138fbc31ca5
Nada Feteiha <nada.feteiha@gmail.com>
2023-07-30 22:28:11 -0700

    refactor restaurant entity

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/entity/Restaurant.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/entity/Restaurant.kt
@@ -1,7 +1,47 @@
 package org.thechance.service_restaurant.entity
 
+import org.thechance.service_restaurant.api.models.RestaurantDto
+import org.thechance.service_restaurant.data.collection.RestaurantCollection
+import java.time.LocalTime
 
 data class Restaurant(
-    val name: String,
     val id: String?,
-)
+    val name: String,
+    val description: String,
+    val priceLevel: String,
+    val rate: Double,
+    val phone: String,
+    val openingTime: String,
+    val closingTime: String,
+
+//    val address: ObjectId,
+//    val meals: List<ObjectId>,
+//    val categories: List<ObjectId>,
+) {
+    fun toDto(): RestaurantDto {
+        return RestaurantDto(
+            id = id ?: "",
+            name = name,
+            description = description,
+            priceLevel = priceLevel,
+            rate = rate,
+            phone = phone,
+            openingTime = openingTime,
+            closingTime = closingTime
+        )
+    }
+
+    fun toCollection(): RestaurantCollection {
+        return RestaurantCollection(
+            name = name,
+            description = description,
+            priceLevel = priceLevel,
+            rate = rate,
+            phone = phone,
+            openingTime = openingTime,
+            closingTime = closingTime
+        )
+    }
+}
+
+fun List<Restaurant>.toDto(): List<RestaurantDto> = map { it.toDto() }
