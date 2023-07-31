e98db16cff9760085f4358f23dc6a2d3341d48ff
Nada Feteiha <nada.feteiha@gmail.com>
2023-07-30 22:30:13 -0700

    refactor application

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/Application.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/Application.kt
@@ -9,7 +9,7 @@ import org.thechance.service_restaurant.plugins.configureRouting
 import org.thechance.service_restaurant.plugins.configureSerialization
 
 fun main() {
-    embeddedServer(Netty, port = 8081, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
