988b9fbc00bcbd856fef6f719b36e2eb73b4bb11
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-08-01 01:52:13 +0300

    remove id default value

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/entity/Meal.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/entity/Meal.kt
@@ -2,7 +2,7 @@ package org.thechance.service_restaurant.entity
 
 
 data class Meal(
-    val id: String? = null,
+    val id: String,
     val name: String? = null,
     val description: String? = null,
     val price: Int? = null,
