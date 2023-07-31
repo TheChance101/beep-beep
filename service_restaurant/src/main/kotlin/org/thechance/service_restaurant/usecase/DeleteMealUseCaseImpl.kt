cf5c178887eb29d4a65f03dea0b264e8c8cc70d0
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-07-31 09:58:17 +0300

    add delete use case implementation

---
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/usecase/DeleteMealUseCaseImpl.kt
@@ -0,0 +1,12 @@
+package org.thechance.service_restaurant.usecase
+
+import org.koin.core.annotation.Single
+import org.thechance.service_restaurant.api.usecases.DeleteMealUseCase
+import org.thechance.service_restaurant.usecase.gateway.MealGateway
+
+@Single
+class DeleteMealUseCaseImpl(private val mealGateway: MealGateway) : DeleteMealUseCase {
+    override suspend fun invoke(id: String) : Boolean  = mealGateway.deleteMealById(id)
+
+
+}
