683a99c0d5e5d7112a8f466846b429fdc5ce500b
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-07-31 03:01:21 +0300

    add meal collection

---
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/data/collection/MealCollection.kt
@@ -0,0 +1,19 @@
+package org.thechance.service_restaurant.data.collection
+
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+
+@Serializable
+data class MealCollection(
+    @BsonId
+    @Contextual
+    @SerialName("_id")
+    val id: ObjectId,
+    val name: String,
+    val description: String,
+    val price: Double,
+    val isDeleted: Boolean = false
+)
