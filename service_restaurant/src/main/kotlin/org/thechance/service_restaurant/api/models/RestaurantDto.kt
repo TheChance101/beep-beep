542739f583cb50cf3ecb3de544a572b1c7bd583f
Nada Feteiha <nada.feteiha@gmail.com>
2023-07-30 22:28:33 -0700

    refactor Dto

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/api/models/RestaurantDto.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/api/models/RestaurantDto.kt
@@ -1,10 +1,30 @@
 package org.thechance.service_restaurant.api.models
 
 import kotlinx.serialization.Serializable
+import org.thechance.service_restaurant.entity.Restaurant
 
 @Serializable
 data class RestaurantDto(
-    val id: String,
-    val name: String,
-)
+    val id: String? = null,
+    val name: String? = null,
+    val description: String? = null,
+    val priceLevel: String? = null,
+    val rate: Double? = null,
+    val phone: String? = null,
+    val openingTime: String? = null,
+    val closingTime: String? = null,
+) {
+    fun toEntity(): Restaurant {
+        return Restaurant(
+            id = id ?: "",
+            name = name ?: "",
+            description = description ?: "",
+            priceLevel = priceLevel ?: "",
+            rate = rate ?: -1.0,
+            phone = phone ?: "",
+            openingTime = openingTime ?: "",
+            closingTime = closingTime ?: ""
+        )
+    }
+}
 
