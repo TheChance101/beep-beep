37a9a6827d705b3fbaff5a15bcde640af09d4d49
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-07-31 10:05:47 +0300

    add helper methods

---
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/api/utils/Extensions.kt
@@ -0,0 +1,11 @@
+package org.thechance.service_restaurant.api.utils
+
+import io.ktor.http.*
+
+fun Parameters.extractString(key: String): String? {
+    return this[key]?.trim()?.takeIf { it.isNotEmpty() }
+}
+
+fun Parameters.extractInt(key: String): Int? {
+    return this[key]?.toIntOrNull()
+}
