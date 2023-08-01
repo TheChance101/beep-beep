0db2d7f03e350a199c0dcfb0ce2982394e797627
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-08-02 02:21:31 +0300

    create add cuisine to implementation

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/usecase/cuisine/AddCuisineToMealUseCaseImp.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/usecase/cuisine/AddCuisineToMealUseCaseImp.kt
@@ -6,7 +6,7 @@ import org.thechance.service_restaurant.usecase.meal.AddCuisineToMealUseCase
 
 @Single
 class AddCuisineToMealUseCaseImp(private val mealGateway: MealGateway) : AddCuisineToMealUseCase {
-    
+
     override suspend fun invoke(mealId: String, cuisineId: String): Boolean =
         mealGateway.addCuisineToMeal(mealId, cuisineId)
 
