9887bc98cecc5f073c942d6b703f22adcb50b745
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-08-02 02:32:01 +0300

    refactor meal mappers

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/api/models/mappers/MealMapper.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/api/models/mappers/MealMapper.kt
@@ -4,9 +4,11 @@ import org.thechance.service_restaurant.api.models.MealDto
 import org.thechance.service_restaurant.entity.Meal
 
 fun Meal.toDto(): MealDto =
-    MealDto(id = id, name = name, description = description, price = price)
+    MealDto(id = id, name = name, description = description, price = price, cuisines = cuisines)
 
 fun List<Meal>.toDto(): List<MealDto> = this.map { it.toDto() }
 
 fun MealDto.toEntity(): Meal =
-    Meal(id = id, name = name, description = description, price = price)
+    Meal(id = id, name = name, description = description, price = price, cuisines = cuisines)
+
+
