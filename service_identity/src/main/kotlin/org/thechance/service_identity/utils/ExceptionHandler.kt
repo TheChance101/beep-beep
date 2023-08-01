73cd4a29627ec87824cb761fb1c6ec7dec1d2cde
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-02 02:43:19 +0300

    create and implement ExceptionHandler

--- service_identity/src/main/kotlin/org/thechance/service_identity/utils/ExceptionHandler.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/utils/ExceptionHandler.kt
@@ -1,2 +1,23 @@
 package org.thechance.service_identity.utils
 
+import io.ktor.http.*
+import io.ktor.server.application.*
+import org.thechance.service_identity.api.ServerResponse
+
+fun handleException(cause: Throwable, call: ApplicationCall) {
+
+    val messageResponse = when (cause) {
+        is IdNotFoundException -> {
+            ServerResponse.error(errorMessage = "Id Not Found", code = HttpStatusCode.NotFound.value)
+        }
+
+        is InvalidIdException -> {
+            ServerResponse.error(errorMessage = "Invalid Id", code = HttpStatusCode.BadRequest.value)
+        }
+
+        else -> {
+            ServerResponse.error(cause.message.toString(), HttpStatusCode.InternalServerError.value)
+        }
+    }
+
+}
