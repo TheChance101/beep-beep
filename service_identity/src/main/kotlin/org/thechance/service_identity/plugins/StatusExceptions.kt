6a9a6fad5fd1c55fd4e620938fa72719d8731113
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-02 02:42:57 +0300

    add configuration for status page plugin

---
+++ service_identity/src/main/kotlin/org/thechance/service_identity/plugins/StatusExceptions.kt
@@ -0,0 +1,14 @@
+package org.thechance.service_identity.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.plugins.statuspages.*
+import org.thechance.service_identity.utils.handleException
+
+
+fun Application.configureStatusExceptions(){
+    install(StatusPages){
+        exception<Throwable> { call, cause ->
+            handleException(cause, call)
+        }
+    }
+}
