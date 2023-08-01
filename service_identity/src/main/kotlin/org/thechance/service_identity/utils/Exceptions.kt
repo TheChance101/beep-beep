9b7e5e8f5d05b721805e1f2a9181abb22fe44e9c
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-02 02:18:52 +0300

    implement exceptions classes

--- service_identity/src/main/kotlin/org/thechance/service_identity/utils/Exceptions.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/utils/Exceptions.kt
@@ -1,2 +1,9 @@
 package org.thechance.service_identity.utils
 
+class InvalidIdException():Exception()
+
+class InvalidUsernameException():Exception()
+
+class InvalidPasswordException():Exception()
+
+class IdNotFoundException():Exception()
