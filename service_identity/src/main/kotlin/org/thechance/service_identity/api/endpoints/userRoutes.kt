f1915d915423893645ead86b33fd9ee369ee9051
youssef <yr925344git@gmail.com>
2023-08-01 21:50:28 +0300

    add user routes

--- service_identity/src/main/kotlin/org/thechance/service_identity/api/endpoints/userRoutes.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/api/endpoints/userRoutes.kt
@@ -6,33 +6,47 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
+import org.thechance.service_identity.api.model.UserDto
 import org.thechance.service_identity.domain.entity.User
-import org.thechance.service_identity.domain.usecases.user.CreateUserUseCase
+import org.thechance.service_identity.domain.usecases.user.UserUseCaseContainer
 
 fun Route.userRoutes() {
 
-    val createUserUseCase: CreateUserUseCase by inject()
+    val userUseCaseContainer: UserUseCaseContainer by inject()
 
     route("/user"){
 
-        get {
-
+        get("/{id}") {
+            val id = call.parameters["id"]!!
+            call.respond(HttpStatusCode.OK, userUseCaseContainer.getUserByIdUseCase.invoke(id))
         }
 
         post{
             try {
                 val user = call.receive<User>()
-                val result = createUserUseCase.invoke(user)
+                val result = userUseCaseContainer.createUserUseCase.invoke(user)
                 call.respond(result)
             }catch (e: Exception){
                 call.respond(HttpStatusCode.BadRequest)
             }
         }
 
-        put {
+        put("/{id}") {
+            val id = call.parameters["id"]!!
+            val userDto = call.receive<UserDto>()
+            call.respond(HttpStatusCode.Created, userUseCaseContainer.updateUserUseCase.invoke(id, userDto.toUser()))
+        }
+
+        delete("/{id}") {
+            val id = call.parameters["id"]!!
+            call.respond(HttpStatusCode.OK, userUseCaseContainer.deleteUserUseCase.invoke(id))
 
         }
     }
 
+    get("/users") {
+        val users = userUseCaseContainer.getUsersUseCase.invoke()
+        call.respond(HttpStatusCode.OK, users.map { it.toUserDto() })
+    }
 
 }
