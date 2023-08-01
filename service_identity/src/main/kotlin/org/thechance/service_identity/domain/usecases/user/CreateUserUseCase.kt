5b4ffb612eedd8dde1b622192170d54d238184fb
youssef <yr925344git@gmail.com>
2023-08-01 22:09:50 +0300

    update get use cases interfaces

---
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/user/CreateUserUseCase.kt
@@ -0,0 +1,8 @@
+package org.thechance.service_identity.domain.usecases.user
+
+import org.thechance.service_identity.domain.entity.User
+
+interface CreateUserUseCase {
+    suspend fun invoke(user: User): Boolean
+}
+
