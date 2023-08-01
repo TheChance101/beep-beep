532d1de6604bc85e47327f5a1ef303827305cca2
ay0o5h <aayosh553@gmail.com>
2023-08-01 11:55:56 +0300

    add wallet routes

---
+++ service_identity/src/main/kotlin/org/thechance/service_identity/api/endpoints/walletRoutes.kt
@@ -0,0 +1,66 @@
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
+import org.thechance.service_identity.entity.Wallet
+
+fun Route.walletRoute(){
+
+    val addWallet : AddWalletUseCaseImpl by inject()
+    val getWallet : GetWalletUseCaseImpl by inject()
+    val deleteWallet :DeleteWalletUseCaseImpl by inject()
+    val updateWallet : UpdateWalletUseCaseImpl by inject()
+    val getUserWallet : GetWalletUseCaseImpl by inject()
+
+    route("/wallet"){
+        get("/{id}") {
+            try {
+                val id = call.parameters["id"]!!
+                val wallet = getWallet.invoke(id)
+                call.respond(wallet)
+            }catch (e: Exception){
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+        get("/user/{id}") {
+            try {
+                val id = call.parameters["id"]!!
+                val wallet = getUserWallet.invoke(id)
+                call.respond(wallet)
+            }catch (e: Exception){
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+        post{
+            try {
+                val wallet = call.receive<Wallet>()
+                val result = addWallet.invoke(wallet)
+                call.respond(result)
+            }catch (e: Exception){
+                call.respond(HttpStatusCode.BadRequest)
+            }
+        }
+        put("/{id}") {
+            try {
+                val id = call.parameters["id"]!!
+                val wallet = call.receive<Wallet>()
+                val result = updateWallet.invoke(id,wallet)
+                call.respond(result)
+            }catch (e: Exception){
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+        delete("/{id}") {
+            try {
+                val id = call.parameters["id"]!!
+                val result = deleteWallet.invoke(id)
+                call.respond(result)
+            }catch (e: Exception){
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+    }
+
+}
--- service_identity/src/main/kotlin/org/thechance/service_identity/plugins/Routing.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/plugins/Routing.kt
@@ -3,10 +3,12 @@ package org.thechance.service_identity.plugins
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
 import org.thechance.service_identity.api.endpoints.testRoutes
+import walletRoute
 
 fun Application.configureRouting(
 ) {
     routing {
+        walletRoute()
         testRoutes()
     }
 }
