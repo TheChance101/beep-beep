afe4befaa618b8971fb71a889f8e2b4757cbb7f0
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-08-02 01:54:00 +0300

    add get cuisines use case implementation

---
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/usecase/cuisine/GetCuisinesUseCaseImp.kt
@@ -0,0 +1,10 @@
+package org.thechance.service_restaurant.usecase.cuisine
+
+import org.koin.core.annotation.Single
+import org.thechance.service_restaurant.usecase.gateway.CuisineGateway
+
+@Single
+class GetCuisinesUseCaseImp(private val gateway: CuisineGateway) : GetCuisinesUseCase {
+    override suspend fun invoke(page : Int , limit : Int) = gateway.getCuisines(page, limit)
+
+}
