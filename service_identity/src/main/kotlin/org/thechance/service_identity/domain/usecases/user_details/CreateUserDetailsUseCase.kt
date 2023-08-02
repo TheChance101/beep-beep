bb4a7d5a632d1dbbb90f6fbe1eb5f3eb09a8a680
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-08-02 21:38:15 +0300

    delete unused usecases

--- service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/user_details/CreateUserDetailsUseCase.kt
+++
@@ -1,9 +0,0 @@
-package org.thechance.service_identity.domain.usecases.user_details
-
-import org.thechance.service_identity.domain.entity.UserDetails
-
-interface CreateUserDetailsUseCase {
-
-    suspend operator fun invoke(userDetails: UserDetails)
-
-}
