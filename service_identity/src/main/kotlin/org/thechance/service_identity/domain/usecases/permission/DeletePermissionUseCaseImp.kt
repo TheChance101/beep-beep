f477f0ec9ceeb55cb810df777e0d4fb76635e318
Elboray <elboray456@gmail.com>
2023-08-02 19:03:37 +0300

    add DeletePermissionUseCaseImp

---
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/permission/DeletePermissionUseCaseImp.kt
@@ -0,0 +1,13 @@
+package org.thechance.service_identity.domain.usecases.permission
+
+import org.thechance.service_identity.domain.gateway.PermissionGateway
+
+class DeletePermissionUseCaseImp(private val permissionGateway: PermissionGateway) : DeletePermissionUseCase {
+    override suspend fun invoke(permissionId: String): Boolean {
+        return if (permissionId.isNotEmpty()) {
+            permissionGateway.deletePermission(permissionId)
+        } else {
+            throw Throwable()
+        }
+    }
+}
