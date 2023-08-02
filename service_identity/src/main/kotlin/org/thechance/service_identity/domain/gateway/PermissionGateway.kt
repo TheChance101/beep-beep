08acc04dc593c0c692f56617d7b69c49c791f870
Elboray <elboray456@gmail.com>
2023-08-02 23:05:13 +0300

    add fun getPermission

--- service_identity/src/main/kotlin/org/thechance/service_identity/domain/gateway/PermissionGateway.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/gateway/PermissionGateway.kt
@@ -3,6 +3,7 @@ package org.thechance.service_identity.domain.gateway
 import org.thechance.service_identity.domain.entity.Permission
 
 interface PermissionGateway {
+    suspend fun getPermission(id: String): Permission?
     suspend fun addPermission(permission: Permission): Boolean
     suspend fun updatePermission(id: String, permission: Permission): Boolean
     suspend fun deletePermission(permissionId: String): Boolean
