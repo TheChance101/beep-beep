50fe9c754d9b7d7c6ccd70d19a6383b9224cb8f2
ay0o5h <aayosh553@gmail.com>
2023-08-03 02:59:25 +0300

    add operator to delete use case

--- service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/wallet/DeleteWalletUseCase.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/wallet/DeleteWalletUseCase.kt
@@ -1,5 +1,5 @@
 package org.thechance.service_identity.domain.usecases.wallet
 
 interface DeleteWalletUseCase {
-    suspend fun invoke(id: String): Boolean
+    suspend operator fun invoke(id: String): Boolean
 }
