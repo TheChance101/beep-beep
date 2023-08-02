52b1c9d1dbdb281835c37c9841aa432681a4992e
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-08-02 12:35:21 +0300

    add abstract address use cases

---
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/api/usecases/AddressUseCases.kt
@@ -0,0 +1,44 @@
+package org.thechance.service_restaurant.api.usecases
+
+import org.koin.core.annotation.Single
+import org.thechance.service_restaurant.entity.Address
+
+
+@Single
+data class AddressUseCasesContainer(
+    val getAddresses: GetAddressesUseCase,
+    val getAddressDetails: GetAddressDetailsUseCase,
+    val createAddress: CreateAddressUseCase,
+    val updateAddress: UpdateAddressUseCase,
+    val deleteAddress: DeleteAddressUseCase,
+    val addAddressToRestaurant: AddAddressToRestaurantUseCase,
+    val getAddressesInRestaurant: GetAddressesInRestaurantUseCase,
+)
+
+interface GetAddressesUseCase {
+    suspend operator fun invoke(): List<Address>
+}
+
+interface GetAddressDetailsUseCase {
+    suspend operator fun invoke(addressId: String): Address
+}
+
+interface CreateAddressUseCase {
+    suspend operator fun invoke(address: Address): Boolean
+}
+
+interface UpdateAddressUseCase {
+    suspend operator fun invoke(address: Address): Boolean
+}
+
+interface DeleteAddressUseCase {
+    suspend operator fun invoke(addressId: String): Boolean
+}
+
+interface AddAddressToRestaurantUseCase {
+    suspend operator fun invoke(restaurantId: String, addressesIds: List<String>): Boolean
+}
+
+interface GetAddressesInRestaurantUseCase {
+    suspend operator fun invoke(restaurantId: String): List<Address>
+}
