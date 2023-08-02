80d75aafeba19866cca67b84fb2108fe4fcb3b45
Elboray <elboray456@gmail.com>
2023-08-02 23:04:48 +0300

    add GetPermissionUseCaseImp

---
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/permission/GetPermissionUseCaseImp.kt
@@ -0,0 +1,13 @@
+package org.thechance.service_identity.domain.usecases.permission
+
+import org.thechance.service_identity.domain.entity.Permission
+import org.thechance.service_identity.domain.gateway.PermissionGateway
+
+class GetPermissionUseCaseImp(
+    private val permissionGateway: PermissionGateway
+) :
+    GetPermissionUseCase {
+    override suspend fun invoke(id: String): Permission {
+        return permissionGateway.getPermission(id) ?: throw Throwable("Invalid id")
+    }
+}
