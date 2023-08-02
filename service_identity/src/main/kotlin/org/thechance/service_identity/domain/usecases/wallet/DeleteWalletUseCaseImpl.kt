d6c7ccebb2419dca83027406a5bc609231249686
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-08-02 03:48:59 +0300

    delete wallet reference when delete once

--- service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/wallet/DeleteWalletUseCaseImpl.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/wallet/DeleteWalletUseCaseImpl.kt
@@ -1,14 +1,16 @@
 package org.thechance.service_identity.domain.usecases.wallet
 
-import org.thechance.service_identity.data.geteway.WalletGateWayImpl
 import org.koin.core.annotation.Single
 import org.thechance.service_identity.domain.gateway.WalletGateWay
+import org.thechance.service_identity.domain.usecases.user_details.DeleteUserWalletUseCase
 
 @Single
-class DeleteWalletUseCaseImpl (
-    private val walletGateWay: WalletGateWay
+class DeleteWalletUseCaseImpl(
+    private val walletGateWay: WalletGateWay,
+    private val deleteUserWallet: DeleteUserWalletUseCase
 ) : DeleteWalletUseCase {
     override suspend fun invoke(id : String) : Boolean{
+        deleteUserWallet(id)
       return  walletGateWay.deleteWallet(id)
     }
 }
