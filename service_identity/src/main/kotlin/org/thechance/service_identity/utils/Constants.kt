4bb5579835c41c785b0be3cc05ad2451edabe595
Elboray <elboray456@gmail.com>
2023-08-02 19:01:21 +0300

    add const val

--- service_identity/src/main/kotlin/org/thechance/service_identity/utils/Constants.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/utils/Constants.kt
@@ -4,4 +4,7 @@ object Constants {
     const val DATA_BASE_NAME = "beep_beep"
     const val WALLET_COLLECTION = "wallet"
     const val ADDRESS_COLLECTION_NAME = "address"
+    const val PERMISSION_COLLECTION_NAME = "permission"
+
+
 }
