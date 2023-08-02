8e1ea78c4f94f899c501211d5d74f2960fcc7a48
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-08-02 03:56:11 +0300

    remove redundant reference deletion

--- service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/user_details/DeleteUserWalletUseCaseImpl.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/user_details/DeleteUserWalletUseCaseImpl.kt
@@ -2,16 +2,13 @@ package org.thechance.service_identity.domain.usecases.user_details
 
 import org.koin.core.annotation.Single
 import org.thechance.service_identity.domain.gateway.UserDetailsGateway
-import org.thechance.service_identity.domain.usecases.wallet.DeleteWalletUseCase
 
 @Single
 class DeleteUserWalletUseCaseImpl(
     private val userDetailsGateway: UserDetailsGateway,
-    private val deleteWallet: DeleteWalletUseCase
 ) : DeleteUserWalletUseCase {
 
     override suspend fun invoke(walletId: String) {
-        deleteWallet(walletId)
         userDetailsGateway.deleteUserWallet(walletId)
     }
 
