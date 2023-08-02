7faf1810ced31d29f6b486d6f75f338c40043fbb
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-08-02 13:25:28 +0300

    add trip entity

---
+++ service_taxi/src/main/kotlin/org/thechance/service_taxi/domain/entity/Trip.kt
@@ -0,0 +1,17 @@
+package org.thechance.service_taxi.domain.entity
+
+data class Trip(
+    val id: String,
+    val driverId: String? = null,
+    val clientId: String? = null,
+    val from: String? = null,
+    val to: String? = null,
+    val rate: Double? = null,
+    val price: Double? = null,
+)
+
+/*
+curd
+driver/history/
+user/{id}/history/
+ */
