5c8e7b1a8f7e6d6b6745f77f2a842c79d8ababa7
Elboray <elboray456@gmail.com>
2023-08-02 19:04:35 +0300

    add fun toPermissionCollection

--- service_identity/src/main/kotlin/org/thechance/service_identity/domain/entity/Permission.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/entity/Permission.kt
@@ -1,16 +1,27 @@
 package org.thechance.service_identity.domain.entity
 
+import org.bson.types.ObjectId
+import org.litote.kmongo.id.toId
 import org.thechance.service_identity.api.model.PermissionDto
+import org.thechance.service_identity.data.collection.AddressCollection
+import org.thechance.service_identity.data.collection.PermissionCollection
 import org.thechance.service_identity.utils.PermissionType
 
 data class Permission(
-    val id:String,
+    val id: String,
     val permissionType: PermissionType
-){
+) {
     fun toPermissionDto(): PermissionDto {
         return PermissionDto(
             id = id,
             permissionType = permissionType
+        )
+    }
+
+    fun toPermissionCollection(): PermissionCollection {
+        return PermissionCollection(
+            id = ObjectId(this.id),
+            permissionType = this.permissionType
         )
     }
 }
