929af7c282d8747d8171828cd141cb03b3711c1f
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-08-02 11:55:56 +0300

    fix user wallet reference deletion

--- service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/user_details/DeleteUserWalletUseCaseImpl.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/user_details/DeleteUserWalletUseCaseImpl.kt
@@ -2,15 +2,16 @@ package org.thechance.service_identity.domain.usecases.user_details
 
 import org.koin.core.annotation.Single
 import org.thechance.service_identity.domain.gateway.UserDetailsGateway
+import org.thechance.service_identity.domain.usecases.wallet.DeleteWalletUseCase
 
 @Single
 class DeleteUserWalletUseCaseImpl(
     private val userDetailsGateway: UserDetailsGateway,
-    private val deleteUserWallet: DeleteUserWalletUseCase
+    private val deleteWallet: DeleteWalletUseCase
 ) : DeleteUserWalletUseCase {
 
     override suspend fun invoke(walletId: String) {
-        deleteUserWallet(walletId)
+        deleteWallet(walletId)
         userDetailsGateway.deleteUserWallet(walletId)
     }
 
