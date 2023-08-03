fe92a58d0a1b364e6d8733723a1a59911a087129
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-03 19:27:53 +0300

    create and implement mappers in Wallet.kt

--- service_identity/src/main/kotlin/org/thechance/service_identity/data/mappers/Wallet.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/data/mappers/Wallet.kt
@@ -1,2 +1,12 @@
 package org.thechance.service_identity.data.mappers
 
+import org.thechance.service_identity.data.collection.WalletCollection
+import org.thechance.service_identity.domain.entity.Wallet
+
+fun WalletCollection.toWallet(): Wallet {
+    return Wallet(
+        id = id.toHexString(),
+        userId = userId,
+        walletBalance = walletBalance
+    )
+}
