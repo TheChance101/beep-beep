70f06621d3c602869ed6519aa0a6a43ea17ea1ab
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-08-03 03:31:27 +0300

    add remaining user fields

--- service_identity/src/main/kotlin/org/thechance/service_identity/domain/entity/UserDetails.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/entity/UserDetails.kt
@@ -1,25 +1,10 @@
 package org.thechance.service_identity.domain.entity
 
-import org.thechance.service_identity.api.model.UserDetailsDto
 
 data class UserDetails(
-    val id: String,
     val userId: String,
-    val password: String?,
     val email: String?,
     val walletId: String?,
     val addresses: List<String>,
     val permissions: List<String>
 )
-
-fun UserDetails.toDto(): UserDetailsDto {
-    return UserDetailsDto(
-        id = id,
-        userId = userId,
-        password = password,
-        email = email,
-        walletId = walletId,
-        addresses = addresses,
-        permissions = permissions
-    )
-}
