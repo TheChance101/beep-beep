5bf777524d78e0ff2c55d020fa78dd884045d8c3
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-08-03 03:35:49 +0300

    add remaining routes

--- service_identity/src/main/kotlin/org/thechance/service_identity/api/endpoints/userRoutes.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/api/endpoints/userRoutes.kt
@@ -2,11 +2,13 @@ package org.thechance.service_identity.api.endpoints
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
-import org.thechance.service_identity.api.model.UserDto
+import org.thechance.service_identity.api.model.CreateUserDto
+import org.thechance.service_identity.api.model.UpdateUserDto
 import org.thechance.service_identity.domain.usecases.user.UserUseCaseContainer
 
 fun Route.userRoutes() {
@@ -22,19 +24,24 @@ fun Route.userRoutes() {
 
         get("/{id}") {
             val id = call.parameters["id"] ?: ""
-            val user = userUseCaseContainer.getUserByIdUseCase.invoke(id).toUserDto()
+            val user = userUseCaseContainer.getUserByIdUseCase.invoke(id).toDetailedUserDto()
             call.respond(HttpStatusCode.OK, user)
         }
 
+        get("/detailed") {
+            val users = userUseCaseContainer.getDetailedUsersUseCase()
+            call.respond(HttpStatusCode.OK, users.map { it.toDetailedUserDto() })
+        }
+
         post {
-            val user = call.receive<UserDto>()
+            val user = call.receive<CreateUserDto>()
             val result = userUseCaseContainer.createUserUseCase.invoke(user.toUser())
             call.respond(HttpStatusCode.Created, result)
         }
 
         put("/{id}") {
             val id = call.parameters["id"] ?: ""
-            val userDto = call.receive<UserDto>()
+            val userDto = call.receive<UpdateUserDto>()
             val result = userUseCaseContainer.updateUserUseCase.invoke(id, userDto.toUser())
             call.respond(HttpStatusCode.OK, result)
         }
@@ -43,10 +50,23 @@ fun Route.userRoutes() {
             val id = call.parameters["id"] ?: ""
             val result = userUseCaseContainer.deleteUserUseCase.invoke(id)
             call.respond(HttpStatusCode.OK, result)
-
         }
+
+        post("/{userId}/permission") {
+            val userId = call.parameters["userId"] ?: throw BadRequestException("User id is required")
+            val permissionId = call.receiveParameters()["permissionId"]
+                ?: throw BadRequestException("Permission id is required")
+            userUseCaseContainer.addPermissionToUser(userId, permissionId)
+            call.respond("Permission added to user successfully")
+        }
+
+        delete("/{userId}/permission") {
+            val userId = call.parameters["userId"] ?: throw BadRequestException("User id is required")
+            val permissionId = call.parameters["permissionId"]
+                ?: throw BadRequestException("Permission id is required")
+            userUseCaseContainer.removePermissionFromUser(userId, permissionId)
+            call.respond("Permission removed from user successfully")
+        }
+
     }
-
-
-
 }
