dd99060c3e8feef2642aacb9dafd9f84c0a44c3f
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-03 19:16:38 +0300

    add mapper from address collection to address entity

--- service_identity/src/main/kotlin/org/thechance/service_identity/data/mappers/Address.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/data/mappers/Address.kt
@@ -1,2 +1,17 @@
 package org.thechance.service_identity.data.mappers
 
+import org.thechance.service_identity.data.collection.AddressCollection
+import org.thechance.service_identity.domain.entity.Address
+
+fun AddressCollection.toAddress(): Address {
+    return Address(
+        id = id.toHexString(),
+        userId = userId.toString(),
+        country = country,
+        city = city,
+        street = street,
+        zipCode = zipCode,
+        houseNumber = houseNumber,
+        isDeleted = isDeleted
+    )
+}
