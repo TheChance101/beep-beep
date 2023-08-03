187edc8e0108f647d1a8289dde7a536b6accbdbb
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-03 19:29:10 +0300

    add mappers to Permission.kt

--- service_identity/src/main/kotlin/org/thechance/service_identity/data/mappers/Permission.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/data/mappers/Permission.kt
@@ -1,2 +1,13 @@
 package org.thechance.service_identity.data.mappers
 
+import org.thechance.service_identity.data.collection.PermissionCollection
+import org.thechance.service_identity.domain.entity.Permission
+
+fun PermissionCollection.toPermission(): Permission {
+    return Permission(
+        id = id.toHexString(),
+        permissionType = permissionType
+    )
+}
+
+fun List<PermissionCollection>.toEntity(): List<Permission> = map { it.toPermission() }
