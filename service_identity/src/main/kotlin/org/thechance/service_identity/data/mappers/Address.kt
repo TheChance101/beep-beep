c132e5030e9534bedb7144588b3080da52aee6a1
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-03 20:47:22 +0300

    move address mappers from Address Entity

--- service_identity/src/main/kotlin/org/thechance/service_identity/data/mappers/Address.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/data/mappers/Address.kt
@@ -1,5 +1,8 @@
 package org.thechance.service_identity.data.mappers
 
+import org.bson.types.ObjectId
+import org.litote.kmongo.id.toId
+import org.thechance.service_identity.api.model.AddressDto
 import org.thechance.service_identity.data.collection.AddressCollection
 import org.thechance.service_identity.domain.entity.Address
 
@@ -13,5 +16,28 @@ fun AddressCollection.toEntity(): Address {
         zipCode = zipCode,
         houseNumber = houseNumber,
         isDeleted = isDeleted
+    )
+}
+
+fun Address.toAddressDto(): AddressDto {
+    return AddressDto(
+        id = id,
+        userId = userId,
+        country = country,
+        city = city,
+        street = street,
+        zipCode = zipCode,
+        houseNumber = houseNumber
+    )
+}
+
+fun Address.toAddressCollection(): AddressCollection {
+    return AddressCollection(
+        userId = ObjectId(this.userId).toId(),
+        country = this.country,
+        city = this.city,
+        street = this.street,
+        zipCode = this.zipCode,
+        houseNumber = this.houseNumber
     )
 }
--- service_identity/src/main/kotlin/org/thechance/service_identity/domain/entity/Address.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/entity/Address.kt
@@ -1,10 +1,5 @@
 package org.thechance.service_identity.domain.entity
 
-import org.bson.types.ObjectId
-import org.litote.kmongo.id.toId
-import org.thechance.service_identity.api.model.AddressDto
-import org.thechance.service_identity.data.collection.AddressCollection
-
 data class Address(
     val id: String,
     val userId: String,
@@ -14,27 +9,4 @@ data class Address(
     val zipCode: Long? = null,
     val houseNumber: String? = null,
     val isDeleted: Boolean? = null
-) {
-    fun toAddressDto(): AddressDto {
-        return AddressDto(
-            id = id,
-            userId = userId,
-            country = country,
-            city = city,
-            street = street,
-            zipCode = zipCode,
-            houseNumber = houseNumber
-        )
-    }
-
-    fun toAddressCollection(): AddressCollection {
-        return AddressCollection(
-            userId = ObjectId(this.userId).toId(),
-            country = this.country,
-            city = this.city,
-            street = this.street,
-            zipCode = this.zipCode,
-            houseNumber = this.houseNumber
-        )
-    }
-}
+)
