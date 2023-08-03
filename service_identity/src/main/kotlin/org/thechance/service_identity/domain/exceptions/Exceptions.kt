6ee94b5abc389cdddf63b9c2d951cb10f248dca3
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-03 21:57:11 +0300

    remove unnecessary exception handler

--- service_identity/src/main/kotlin/org/thechance/service_identity/domain/exceptions/Exceptions.kt
+++
@@ -1,9 +0,0 @@
-package org.thechance.service_identity.domain.exceptions
-
-class InvalidIdException():Exception()
-
-class InvalidUsernameException():Exception()
-
-class InvalidPasswordException():Exception()
-
-class IdNotFoundException():Exception()
