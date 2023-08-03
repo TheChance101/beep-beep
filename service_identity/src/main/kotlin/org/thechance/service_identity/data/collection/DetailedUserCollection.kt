90b398e5e0441b7e6220f1ee2326b7da343fdbf1
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-03 19:26:31 +0300

    move mappers from DetailedUserCollection

--- service_identity/src/main/kotlin/org/thechance/service_identity/data/collection/DetailedUserCollection.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/data/collection/DetailedUserCollection.kt
@@ -24,21 +24,3 @@ data class DetailedUserCollection(
     @SerialName("details")
     val details: List<UserDetailsCollection>
 )
-
-fun DetailedUserCollection.toEntity(): User {
-    return User(
-        id = id.toHexString(),
-        fullName = fullName,
-        username = username,
-        password = password,
-        isDeleted = isDeleted,
-        email = details.first().email,
-        walletId = details.first().walletId,
-        addresses = details.first().addresses.map { it.toHexString() },
-        permissions = details.first().permissions.map { it.toHexString() }
-    )
-}
-
-fun List<DetailedUserCollection>.toEntity(): List<User> {
-    return map { it.toEntity() }
-}
