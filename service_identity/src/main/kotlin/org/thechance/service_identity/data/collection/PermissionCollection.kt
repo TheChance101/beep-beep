143744db46e003c33b1fb6c41d9155f38e8457c2
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-03 19:28:44 +0300

    move mappers from PermissionCollection

--- service_identity/src/main/kotlin/org/thechance/service_identity/data/collection/PermissionCollection.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/data/collection/PermissionCollection.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
+import org.thechance.service_identity.data.mappers.toPermission
 import org.thechance.service_identity.domain.entity.Permission
 import org.thechance.service_identity.utils.PermissionType
 
@@ -16,16 +17,7 @@ data class PermissionCollection(
     val id: ObjectId = ObjectId(),
     @SerialName("permission_type")
     val permissionType: PermissionType = PermissionType.CLIENT
-) {
-    fun toPermission(): Permission {
-        return Permission(
-            id = id.toHexString(),
-            permissionType = permissionType
-        )
-    }
-}
-
-fun List<PermissionCollection>.toEntity(): List<Permission> = map { it.toPermission() }
+)
 
 @Serializable
 data class UserPermissionsCollection(
