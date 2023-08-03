fbe60bba30baab531d04de8fa55613f94348c8d3
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-03 20:51:12 +0300

    refactor entity user mappers to move in user

--- service_identity/src/main/kotlin/org/thechance/service_identity/data/mappers/User.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/data/mappers/User.kt
@@ -1,6 +1,10 @@
 package org.thechance.service_identity.data.mappers
 
+import org.bson.types.ObjectId
+import org.thechance.service_identity.api.model.DetailedUserDto
+import org.thechance.service_identity.api.model.UserDto
 import org.thechance.service_identity.data.collection.UserCollection
+import org.thechance.service_identity.data.collection.UserDetailsCollection
 import org.thechance.service_identity.domain.entity.User
 
 fun UserCollection.toEntity(): User {
@@ -10,5 +14,46 @@ fun UserCollection.toEntity(): User {
         username = username,
         password = password,
         isDeleted = isDeleted
+    )
+}
+
+fun User.toUserDto(): UserDto {
+    return UserDto(
+        id = id,
+        fullName = fullName,
+        username = username,
+        password = password,
+    )
+}
+
+fun User.toDetailedUserDto(): DetailedUserDto {
+    return DetailedUserDto(
+        id = id,
+        fullName = fullName,
+        username = username,
+        password = password,
+        email = email,
+        walletId = walletId,
+        addresses = addresses,
+        permissions = permissions
+    )
+}
+
+fun User.toUserCollection(): UserCollection {
+    return UserCollection(
+        fullName = this.fullName,
+        username = this.username,
+        password = this.password,
+        isDeleted = this.isDeleted,
+    )
+}
+
+fun User.toDetailsCollection(userId: String): UserDetailsCollection {
+    return UserDetailsCollection(
+        userId = ObjectId(userId),
+        email = email,
+        walletId = walletId,
+        addresses = addresses.map { ObjectId(it) },
+        permissions = permissions.map { ObjectId(it) }
     )
 }
