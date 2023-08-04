970abbd5a1b1874be842a3d16e3f421a470e0f62
ay0o5h <aayosh553@gmail.com>
2023-08-04 21:30:13 +0300

    add function to user account

--- service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/useraccount/UserAccountUseCase.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/useraccount/UserAccountUseCase.kt
@@ -1,4 +1,13 @@
 package org.thechance.service_identity.domain.usecases.useraccount
 
+import org.thechance.service_identity.domain.entity.Wallet
+
 interface UserAccountUseCase {
+
+    // region: wallet
+    suspend fun getWallet(walletId: String): Wallet
+    suspend fun createWallet(wallet: Wallet): Boolean
+    suspend fun updateWallet(walletId: String, wallet: Wallet): Boolean
+
+    // endregion: wallet
 }
