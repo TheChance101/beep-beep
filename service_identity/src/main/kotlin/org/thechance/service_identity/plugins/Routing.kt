bbf61054a1a9d3cad33bd4ee95864939c0765227
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-08-04 20:31:13 +0200

    fix error in address routes

--- service_identity/src/main/kotlin/org/thechance/service_identity/plugins/Routing.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/plugins/Routing.kt
@@ -4,7 +4,7 @@ import io.ktor.server.application.*
 import org.thechance.service_identity.api.endpoints.userRoutes
 import org.thechance.service_identity.api.endpoints.walletRoute
 import io.ktor.server.routing.*
-import org.thechance.service_identity.api.endpoints.addressRoutes
+import org.thechance.service_identity.endpoints.addressRoutes
 import org.thechance.service_identity.api.endpoints.permissionRoutes
 
 fun Application.configureRouting() {
