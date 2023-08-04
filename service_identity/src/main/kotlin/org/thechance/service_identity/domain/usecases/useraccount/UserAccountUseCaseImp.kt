c6212a51fd72b2728b991e6811a500359bc74ffc
ay0o5h <aayosh553@gmail.com>
2023-08-04 21:30:21 +0300

    add function to user account imple

--- service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/useraccount/UserAccountUseCaseImp.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/useraccount/UserAccountUseCaseImp.kt
@@ -1,4 +1,20 @@
 package org.thechance.service_identity.domain.usecases.useraccount
 
-class UserAccountUseCaseImp {
+import org.thechance.service_identity.domain.entity.Wallet
+import org.thechance.service_identity.domain.gateway.DataBaseGateway
+
+class UserAccountUseCaseImp(
+    private val dataBaseGateway: DataBaseGateway
+) : UserAccountUseCase {
+    override suspend fun getWallet(walletId: String): Wallet {
+        return dataBaseGateway.getWallet(walletId)
+    }
+
+    override suspend fun createWallet(wallet: Wallet): Boolean {
+        return dataBaseGateway.createWallet(wallet)
+    }
+
+    override suspend fun updateWallet(walletId: String, wallet: Wallet): Boolean {
+        return dataBaseGateway.updateWallet(walletId,wallet)
+    }
 }
