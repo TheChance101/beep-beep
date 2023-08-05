b7fb1b292871c9c4c5c5740fe6926ef5e2b5ee9f
ay0o5h <aayosh553@gmail.com>
2023-08-05 10:10:40 +0300

    remove walletManagement from useraccount usecase

--- service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/useraccount/UserAccountUseCase.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/useraccount/UserAccountUseCase.kt
@@ -8,13 +8,6 @@ import org.thechance.service_identity.domain.entity.Wallet
 
 interface UserAccountUseCase {
 
-    // region: wallet
-    suspend fun getWallet(walletId: String): Wallet
-    suspend fun createWallet(wallet: Wallet): Boolean
-    suspend fun updateWallet(walletId: String, wallet: Wallet): Boolean
-
-    // endregion: wallet
-
 
     // region address
     suspend fun addAddress(address: Address): Boolean
