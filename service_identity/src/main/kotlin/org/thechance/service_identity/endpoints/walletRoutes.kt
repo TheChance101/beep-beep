aad56f092af7b6396935efd5b6ada0fe388b7717
ay0o5h <aayosh553@gmail.com>
2023-08-05 10:11:30 +0300

    use walletManagement in walletRoute

--- service_identity/src/main/kotlin/org/thechance/service_identity/endpoints/walletRoutes.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/endpoints/walletRoutes.kt
@@ -9,18 +9,18 @@ import org.koin.ktor.ext.inject
 import org.thechance.service_identity.api.model.WalletDto
 import org.thechance.service_identity.data.mappers.toDto
 import org.thechance.service_identity.data.mappers.toEntity
-import org.thechance.service_identity.domain.usecases.useraccount.UserAccountUseCase
+import org.thechance.service_identity.domain.usecases.useraccount.WalletManagementUseCase
 
 fun Route.walletRoute() {
 
-    val userAccountUseCase: UserAccountUseCase by inject()
+    val walletManagement: WalletManagementUseCase by inject()
 
 
     route("/wallet") {
         get("/{id}") {
             try {
                 val id = call.parameters["id"]!!
-                val wallet = userAccountUseCase.getWallet(id).toDto()
+                val wallet = walletManagement.getWallet(id).toDto()
                 call.respond(wallet)
             } catch (e: Exception) {
                 call.respond(HttpStatusCode.NotFound)
@@ -29,7 +29,7 @@ fun Route.walletRoute() {
         post {
             try {
                 val wallet = call.receive<WalletDto>()
-                val result = userAccountUseCase.createWallet(wallet.toEntity())
+                val result = walletManagement.createWallet(wallet.toEntity())
                 call.respond(result)
             } catch (e: Exception) {
                 call.respond(HttpStatusCode.BadRequest)
@@ -39,7 +39,7 @@ fun Route.walletRoute() {
             try {
                 val id = call.parameters["id"]!!
                 val wallet = call.receive<WalletDto>()
-                val result = userAccountUseCase.updateWallet(id, wallet.toEntity())
+                val result = walletManagement.updateWallet(id, wallet.toEntity())
                 call.respond(result)
             } catch (e: Exception) {
                 call.respond(HttpStatusCode.NotFound)
