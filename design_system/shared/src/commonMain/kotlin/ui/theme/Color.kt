77c7845b5ed675ff1e9694fe903d0ad2026d877e
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-08-06 15:16:40 +0200

    add colors to design system module

---
+++ design_system/shared/src/commonMain/kotlin/ui/theme/Color.kt
@@ -0,0 +1,51 @@
+package com.beepbeep.designSystem.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+// region Colors / Light
+
+val primaryLight = Color(0xFFF53D47)
+val secondaryLight = Color(0xFFFFF3F2)
+
+val contentPrimaryLight = Color(0xDE1F0000)
+val contentSecondaryLight = Color(0x991F0000)
+val contentTertiaryLight = Color(0x611F0000)
+val contentBorderLight = Color(0x141F0000)
+
+val surfaceLight = Color(0xFFFFFFFF)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val hoverLight = Color(0xFFFFFAFA)
+val backgroundLight = Color(0xFFFAFAFA)
+val disableLight = Color(0x401F0000)
+
+val successLight = Color(0xFF41BE88)
+val successContainerLight = Color(0xFFF0FFF7)
+val warningLight = Color(0xFFF2BD00)
+val warningContainerLight = Color(0xFFFFFCEB)
+
+//endregion
+
+
+// region Colors / Dark
+
+val primaryDark = Color(0xFFF53D47)
+val secondaryDark = Color(0x1AFFFFFF)
+
+val contentPrimaryDark = Color(0xDEFFFFFF)
+val contentSecondaryDark = Color(0x99FFFFFF)
+val contentTertiaryDark = Color(0x61FFFFFF)
+val contentBorderDark = Color(0x14FFEFEF)
+
+val surfaceDark = Color(0xFF1C1C1C)
+val onPrimaryDark = Color(0xFFFFFFFF)
+val hoverDark = Color(0xFF242424)
+val backgroundDark = Color(0xFF151515)
+val disableDark = Color(0x40FFFFFF)
+
+val successDark = Color(0xFF66CB9F)
+val successContainerDark = Color(0x14EBFFF4)
+val warningDark = Color(0xFFCBB567)
+val warningContainerDark = Color(0x14FFFCEB)
+
+//endregion
