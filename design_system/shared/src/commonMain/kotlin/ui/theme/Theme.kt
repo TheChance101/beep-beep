dccefb3be6f2228eaf929bef7c7c7e5a971525b6
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-06 15:14:15 +0300

    add shapes and configure it in theme

--- design_system/shared/src/commonMain/kotlin/ui/theme/Theme.kt
+++ design_system/shared/src/commonMain/kotlin/ui/theme/Theme.kt
@@ -5,35 +5,50 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.graphics.Color
 
 
+// Will Replaced by Mustafa Colors
 private val LightColors = lightColors(
     primary = Color(0xFFD0BCFF),
     secondary = Color(0xFF10BCFF),
     background = Color(0x99F0BCFF),
 )
 
-
+// Will Replaced by Mustafa Colors
 private val DarkColors = darkColors(
     primary = Color(0xFFD4BCFF),
     secondary = Color(0xF7D0BCFF),
     background = Color(0x9FD0BCFF),
 )
 
+private val localDimens = compositionLocalOf { Dimens() }
+
+val dimens: Dimens
+    @Composable
+    @ReadOnlyComposable
+    get() = localDimens.current
+
 @Composable
 fun BeepBeepTheme(
-    useDarkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (!useDarkTheme) {
+    val colorScheme = if (!darkTheme) {
         LightColors
     } else {
         DarkColors
     }
 
-    MaterialTheme(
-        colors = colors,
-        content = content
-    )
+    CompositionLocalProvider(localDimens provides Dimens()) {
+        MaterialTheme(
+            colors = colorScheme,
+            shapes = shapes,
+            // typography of Aya Will be added here
+            content = content
+        )
+    }
 }
