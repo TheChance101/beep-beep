97f482933b3f4873af10f047b44f6f3d4abadb94
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-06 14:28:44 +0300

    implement basic Beep beep theme

---
+++ design_system/shared/src/androidMain/kotlin/ui/Theme.kt
@@ -0,0 +1,39 @@
+package com.beepbeep.designSystem.ui
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+
+private val LightColors = lightColors(
+    primary = Color(0xFFD0BCFF),
+    secondary = Color(0xFF10BCFF),
+    background = Color(0x99F0BCFF),
+)
+
+
+private val DarkColors = darkColors(
+    primary = Color(0xFFD4BCFF),
+    secondary = Color(0xF7D0BCFF),
+    background = Color(0x9FD0BCFF),
+)
+
+@Composable
+fun AppTheme(
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (!useDarkTheme) {
+        LightColors
+    } else {
+        DarkColors
+    }
+
+    MaterialTheme(
+        colors = colors,
+        content = content
+    )
+}
