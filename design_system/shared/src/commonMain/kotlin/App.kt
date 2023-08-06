4c5789979cc081f899c03fac1171d1bb7e60a0f7
ay0o5h <aayosh553@gmail.com>
2023-08-07 00:37:53 +0300

    add fonts and typeoghraphy

--- design_system/shared/src/commonMain/kotlin/App.kt
+++ design_system/shared/src/commonMain/kotlin/App.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,6 +15,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.beepbeep.designSystem.ui.theme.BeepBeepTheme
+import com.beepbeep.designSystem.ui.theme.LocalTypography
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
@@ -29,7 +30,7 @@ fun DesignApp() {
                 greetingText = "Hello, ${getPlatformName()}"
                 showImage = !showImage
             }) {
-                Text(greetingText)
+                Text(greetingText, style = MaterialTheme.typography.headlineLarge)
             }
             AnimatedVisibility(showImage) {
                 Image(
--- design_system/shared/src/commonMain/kotlin/ui/theme/Theme.kt
+++ design_system/shared/src/commonMain/kotlin/ui/theme/Theme.kt
@@ -1,18 +1,20 @@
 package com.beepbeep.designSystem.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
 
 // Will Replaced by Mustafa Colors
-private val LightColors = lightColors(
+private val LightColors = darkColorScheme(
     primary = Color(0xFFD0BCFF),
     secondary = Color(0xFF10BCFF),
     background = Color(0x99F0BCFF),
@@ -21,7 +23,7 @@ private val LightColors = lightColors(
 )
 
 // Will Replaced by Mustafa Colors
-private val DarkColors = darkColors(
+private val DarkColors = lightColorScheme(
     primary = Color(0xFFD4BCFF),
     secondary = Color(0xF7D0BCFF),
     background = Color(0x9FD0BCFF),
@@ -34,22 +36,23 @@ val dimens: Dimens
     @ReadOnlyComposable
     get() = localDimens.current
 
+
 @Composable
 fun BeepBeepTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = if (!darkTheme) {
         LightColors
     } else {
         DarkColors
     }
-
     CompositionLocalProvider(localDimens provides Dimens()) {
         MaterialTheme(
-            colors = colorScheme,
+            colorScheme = colorScheme,
             shapes = shapes,
             // typography of Aya Will be added here
+            typography= Typography(),
             content = content
         )
     }
