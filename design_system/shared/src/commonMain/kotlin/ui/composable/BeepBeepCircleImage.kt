d9f0b209a162d761a93158971b81bc0be3ca88cc
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-07 04:03:33 +0300

    remove unused imports

--- design_system/shared/src/commonMain/kotlin/ui/composable/BeepBeepCircleImage.kt
+++ design_system/shared/src/commonMain/kotlin/ui/composable/BeepBeepCircleImage.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
