0d806525d275c15247fdf1a4f86a5f3295505d02
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-08-07 20:32:28 +0200

    fix text is duplicated issue

--- design_system/shared/src/commonMain/kotlin/App.kt
+++ design_system/shared/src/commonMain/kotlin/App.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,7 +17,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import com.beepbeep.designSystem.ui.composable.CheckBoxButton
-
 import com.beepbeep.designSystem.ui.composable.OutlinedButton
 import com.beepbeep.designSystem.ui.composable.PrimaryButton
 import com.beepbeep.designSystem.ui.composable.SwitchButton
