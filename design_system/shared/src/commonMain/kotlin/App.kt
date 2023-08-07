e63410151a52eb4465aa0009b829bea2fe0d86c0
ay0o5h <aayosh553@gmail.com>
2023-08-07 20:23:46 +0300

    add sun and moon images in all apps

--- design_system/shared/src/commonMain/kotlin/App.kt
+++ design_system/shared/src/commonMain/kotlin/App.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,7 +34,7 @@ fun DesignApp() {
             verticalArrangement = Arrangement.Bottom
         ) {
             CheckBoxButtonPrev()
-//            TogglePreview()
+            TogglePreview()
             SwitchPreview()
             PreviewEnabledButtons()
             PreviewDisabledButtons()
