dbd56a30e83ce2fb0cf7e7602102f6071c81b8d2
ay0o5h <aayosh553@gmail.com>
2023-08-07 17:23:56 +0300

    switchButton

--- design_system/shared/src/commonMain/kotlin/App.kt
+++ design_system/shared/src/commonMain/kotlin/App.kt
@@ -9,11 +9,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+
 import com.beepbeep.designSystem.ui.composable.OutlinedButton
 import com.beepbeep.designSystem.ui.composable.PrimaryButton
+import com.beepbeep.designSystem.ui.composable.SwitchButton
 import com.beepbeep.designSystem.ui.theme.BeepBeepTheme
 
 
@@ -24,6 +30,7 @@ fun DesignApp() {
             Modifier.fillMaxSize().background(BeepBeepTheme.colorScheme.background),
             verticalArrangement = Arrangement.Bottom
         ) {
+            tooglePreview()
             PreviewEnabledButtons()
             PreviewDisabledButtons()
         }
@@ -77,6 +84,12 @@ fun PreviewDisabledButtons() {
             Text("Button")
         }
     }
+}
+
+@Composable
+fun tooglePreview() {
+    var isToggle by remember { mutableStateOf(false) }
+    SwitchButton(selected = isToggle, onUpdate = { isToggle = it })
 }
 
 expect fun getPlatformName(): String
---
+++ design_system/shared/src/commonMain/kotlin/ui/composable/SwitchButton.kt
@@ -0,0 +1,75 @@
+package com.beepbeep.designSystem.ui.composable
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.beepbeep.designSystem.ui.theme.LocalDimens
+import com.beepbeep.designSystem.ui.theme.LocalShapes
+
+
+@Composable
+fun SwitchButton(
+    selected: Boolean,
+    modifier: Modifier = Modifier,
+    onUpdate: (Boolean) -> Unit,
+) {
+    val dimens= LocalDimens.current
+    val shape =LocalShapes.current
+
+    val targetBackgroundColor by animateColorAsState(
+        targetValue = if (selected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface
+    )
+
+    val targetBorderColor by animateColorAsState(
+        targetValue = if (selected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.tertiaryContainer
+    )
+        Box(
+            modifier = modifier.width(50.dp).background(
+              color=  targetBackgroundColor,
+                        shape = shape.large,
+            ).border(
+                width = 1.dp,
+                color =targetBorderColor,
+                shape = shape.large
+            )
+                .clickable(
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() }
+        ) {
+            onUpdate(!selected)
+        }, contentAlignment = if (selected) Alignment.TopEnd else Alignment.TopStart
+        ) {
+            Circle(
+                modifier = Modifier.padding(dimens.space4),
+                isSelected = selected
+            )
+        }
+}
+
+@Composable
+fun Circle(
+    modifier: Modifier = Modifier,
+    isSelected: Boolean = false,
+) {
+    Card(
+        shape = CircleShape, modifier = modifier.size(20.dp), elevation = 0.dp
+    ) {
+        Box(modifier = Modifier.background(if(isSelected)  Color.White else  MaterialTheme.colorScheme.primary) , )
+    }
+}
