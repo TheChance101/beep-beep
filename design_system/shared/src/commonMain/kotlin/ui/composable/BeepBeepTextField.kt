587d950cf34819f26bfbc5519f852eb51cace00a
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-08 06:31:50 +0300

    refactor BeepBeepTextField implementation

--- design_system/shared/src/commonMain/kotlin/ui/composable/BeepBeepTextField.kt
+++ design_system/shared/src/commonMain/kotlin/ui/composable/BeepBeepTextField.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Error
 import androidx.compose.material.icons.outlined.Visibility
@@ -12,10 +11,8 @@ import androidx.compose.material.icons.outlined.VisibilityOff
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,83 +20,95 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.unit.dp
 import com.beepbeep.designSystem.ui.theme.BeepBeepTheme.colorScheme
+import com.beepbeep.designSystem.ui.theme.BeepBeepTheme.dimens
 import com.beepbeep.designSystem.ui.theme.BeepBeepTheme.shapes
-import org.jetbrains.compose.resources.ExperimentalResourceApi
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalResourceApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BeepBeepTextField(
-    modifier: Modifier = Modifier,
     onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
     label: String,
     text: String,
     keyboardType: KeyboardType = KeyboardType.Text,
+    shapeRadius: Shape = shapes.medium,
     errorMessage: String = "",
     isError: Boolean = errorMessage.isNotEmpty(),
 ) {
     var showPassword by remember { mutableStateOf(false) }
+
     Column(
-        modifier = modifier.fillMaxWidth().padding(16.dp),
+        modifier = modifier.fillMaxWidth().padding(dimens.space16),
         horizontalAlignment = Alignment.Start
     ) {
         Text(
             text = label,
-            modifier = Modifier.padding(bottom = 8.dp),
+            modifier = Modifier.padding(bottom = dimens.space8),
             color = colorScheme.onPrimary
         )
+
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             value = text,
             onValueChange = onValueChange,
-            shape = shapes.medium,
+            shape = shapeRadius,
             singleLine = true,
             keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
-            colors = TextFieldDefaults.textFieldColors(
-                cursorColor = colorScheme.onTertiary,
-                errorCursorColor = colorScheme.error,
-                containerColor = if (!isError) colorScheme.background else colorScheme.secondary,
-                errorTrailingIconColor = colorScheme.error,
-            ),
             trailingIcon = {
-                if (isError) {
-                    Icon(
-                        imageVector = Icons.Outlined.Error,
-                        contentDescription = "error icon",
-                        tint = colorScheme.error
-                    )
-                }
-                if ((keyboardType == KeyboardType.Password || keyboardType == KeyboardType.NumberPassword) && !isError) {
-                    IconButton(onClick = { showPassword = !showPassword }) {
-                        Icon(
-                            imageVector = if (showPassword) Icons.Outlined.VisibilityOff else Icons.Outlined.Visibility,
-                            contentDescription = if (showPassword) "Show Password" else "Hide Password"
-                        )
-                    }
+                TrailingIcon(keyboardType, isError, showPassword) {
+                    showPassword = !showPassword
                 }
             },
-            visualTransformation =
-            if (showPassword){
-                VisualTransformation.None
-            }
-            else if (!(keyboardType == KeyboardType.Password || keyboardType == KeyboardType.NumberPassword)) {
-                VisualTransformation.None
-            } else{
-                PasswordVisualTransformation()
-            },
+            visualTransformation = BeepBeepVisualTransformation(keyboardType, showPassword),
             isError = isError,
         )
+
         if (isError) {
             Text(
                 text = errorMessage,
-                modifier = Modifier.padding(top = 8.dp),
+                modifier = Modifier.padding(top = dimens.space8),
                 color = colorScheme.primary,
             )
         }
     }
+}
 
+@Composable
+fun TrailingIcon(
+    keyboardType: KeyboardType,
+    isError: Boolean,
+    showPassword: Boolean,
+    togglePasswordVisibility: () -> Unit
+) {
+    if (isError) {
+        Icon(
+            imageVector = Icons.Outlined.Error,
+            contentDescription = "error icon",
+            tint = colorScheme.error
+        )
+    } else if ((keyboardType == KeyboardType.Password || keyboardType == KeyboardType.NumberPassword) && !isError) {
+        IconButton(onClick = { togglePasswordVisibility() }) {
+            Icon(
+                imageVector = if (showPassword) Icons.Outlined.VisibilityOff else Icons.Outlined.Visibility,
+                contentDescription = if (showPassword) "Show Password" else "Hide Password"
+            )
+        }
+    }
+}
+
+@Composable
+private fun BeepBeepVisualTransformation(
+    keyboardType: KeyboardType,
+    showPassword: Boolean
+): VisualTransformation {
+    return if (showPassword || keyboardType != KeyboardType.Password && keyboardType != KeyboardType.NumberPassword) {
+        VisualTransformation.None
+    } else {
+        PasswordVisualTransformation()
+    }
 }
