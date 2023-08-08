2f8665d64f9261d7d8ad143a09d4192ea49d676d
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-08-08 15:36:36 +0300

    add missing dependency injection

--- service_identity/src/main/kotlin/org/thechance/service_identity/plugins/Validation.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/plugins/Validation.kt
@@ -2,14 +2,21 @@ package org.thechance.service_identity.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.plugins.requestvalidation.*
+import org.koin.ktor.ext.inject
+import org.thechance.service_identity.domain.usecases.validation.IAddressValidationUseCase
+import org.thechance.service_identity.domain.usecases.validation.IUserValidationUseCase
 import org.thechance.service_identity.endpoints.validation.addressValidation
 import org.thechance.service_identity.endpoints.validation.createUserValidation
 import org.thechance.service_identity.endpoints.validation.updateUserValidation
 
 fun Application.configureValidation() {
+
+    val addressValidation: IAddressValidationUseCase by inject()
+    val userValidation: IUserValidationUseCase by inject()
+
     install(RequestValidation) {
-        addressValidation()
-        createUserValidation()
-        updateUserValidation()
+        addressValidation(addressValidation)
+        createUserValidation(userValidation)
+        updateUserValidation(userValidation)
     }
 }
