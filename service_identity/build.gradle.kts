6ddd185ff0a52e210292df5197760132ca2d5659
youssef <yr925344git@gmail.com>
2023-08-08 16:57:35 +0300

    add user validation tests

--- service_identity/build.gradle.kts
+++ service_identity/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 
--- service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/validation/IUserValidationUseCase.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/domain/usecases/validation/IUserValidationUseCase.kt
@@ -18,17 +18,17 @@ interface IUserValidationUseCase {
 @Single
 class UserValidationUseCase : IUserValidationUseCase {
 
-    override fun validateUsernameIsNotBlank(username: String): Boolean = username.isBlank()
-
     override fun validateUsername(username: String): Boolean {
         val validUserNameRegex = "[a-zA-Z0-9_]+".toRegex()
         return username.matches(validUserNameRegex)
     }
 
-    override fun validateFullNameIsNotEmpty(fullName: String): Boolean = fullName.isEmpty()
+    override fun validateUsernameIsNotBlank(username: String): Boolean = username.isNotEmpty()
 
-    override fun validatePasswordIsNotEmpty(password: String): Boolean = password.isEmpty()
+    override fun validateFullNameIsNotEmpty(fullName: String): Boolean = fullName.isNotEmpty()
 
-    override fun validatePasswordLength(password: String): Boolean = password.length < 8
+    override fun validatePasswordIsNotEmpty(password: String): Boolean = password.isNotEmpty()
+
+    override fun validatePasswordLength(password: String): Boolean = password.length > 8
 
 }
