cf5e72cb0861cde30c750978f2c500c0329adc84
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-08-10 01:37:01 +0300

    add notifications history

--- service_notification/src/main/kotlin/org/thechance/service_notification/domain/model/Notification.kt
+++ service_notification/src/main/kotlin/org/thechance/service_notification/domain/model/Notification.kt
@@ -1,9 +1,16 @@
 package org.thechance.service_notification.domain.model
 
 data class Notification(
+    val id : String,
     val title: String,
     val body: String,
     val date: Long,
-    val userId : String,
-    val isRead : Boolean
+    val userIds : List<String>,
+)
+
+data class NotificationRequest(
+    val title: String,
+    val body: String,
+    val date: Long,
+    val userIds : List<String>,
 )
