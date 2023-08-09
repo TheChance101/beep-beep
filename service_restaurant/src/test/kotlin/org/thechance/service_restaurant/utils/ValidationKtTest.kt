c48bd665e73c68db2b4326125459d595945db18c
Asia sama <sehunexo710@gmail.com>
2023-08-09 17:07:25 +0300

    add:test for restaurant validation function

--- service_restaurant/src/test/kotlin/org/thechance/service_restaurant/utils/ValidationKtTest.kt
+++ service_restaurant/src/test/kotlin/org/thechance/service_restaurant/utils/ValidationKtTest.kt
@@ -2,6 +2,11 @@ package org.thechance.service_restaurant.utils
 
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.function.Executable
+import org.thechance.service_restaurant.api.models.AddressDto
+import org.thechance.service_restaurant.api.models.RestaurantDto
+import org.thechance.service_restaurant.api.models.mappers.toEntity
 
 class ValidationKtTest {
 
@@ -55,6 +60,131 @@ class ValidationKtTest {
 
     //endregion
 
+
+    //region validationRestaurant
+    @Test
+    fun `validation restaurant should pass validation with just require fields`() {
+        // given a valid fields of restaurant that  require
+        val validRestaurant = createValidRestaurant()
+        //then no exception should be thrown and pass validation
+        validationRestaurant(validRestaurant[0].toEntity())
+    }
+
+    @Test
+    fun `validation restaurant should pass validation with all fields`() {
+        // given a valid fields of restaurant with optional fields
+        val validRestaurant = createValidRestaurant()
+        //then no exception should be thrown and pass validation
+        validationRestaurant(validRestaurant[1].toEntity())
+    }
+
+    @Test
+    fun `validation restaurant should throw exception when name is invalid`() {
+        // given a restaurant when name is less than four letter
+        val shortNameExecutable = Executable {
+            validationRestaurant(createValidRestaurant()[0].copy(name = "Asi").toEntity())
+        }
+        // given a restaurant when name is more than 25
+        val longNameExecutable = Executable {
+            validationRestaurant(createValidRestaurant()[0].copy(name = "A".repeat(26)).toEntity())
+        }
+        // given a restaurant when name is empty
+        val emptyNameExecutable = Executable {
+            validationRestaurant(createValidRestaurant()[0].copy(name = " ").toEntity())
+        }
+        // given a restaurant when name is null
+        val nullNameExecutable = Executable {
+            validationRestaurant(createValidRestaurant()[0].copy(name = null).toEntity())
+        }
+
+        // then check if throw exception
+        assertThrows(MultiErrorException::class.java, shortNameExecutable)
+        assertThrows(MultiErrorException::class.java, longNameExecutable)
+        assertThrows(MultiErrorException::class.java, emptyNameExecutable)
+        assertThrows(MultiErrorException::class.java, nullNameExecutable)
+    }
+
+    @Test
+    fun `validation restaurant should throw exception when description is invalid `() {
+        // given a restaurant when description is more than 255
+        val longDescriptionRestaurantExecutable = Executable {
+            validationRestaurant(createValidRestaurant()[0].toEntity().copy(description = "A".repeat(256)))
+        }
+        // then check if throw exception
+        assertThrows(MultiErrorException::class.java, longDescriptionRestaurantExecutable)
+    }
+
+    @Test
+    fun `validation restaurant should throw exception when rate is invalid`() {
+        // given a restaurant when rate is above upper bound  5.0
+        val upperRateExecutable = Executable {
+            validationRestaurant(createValidRestaurant()[0].toEntity().copy(rate = 5.1))
+        }
+        // given a restaurant when rate is  below lower bound 0.0
+        val lowerRateExecutable = Executable {
+            validationRestaurant(createValidRestaurant()[0].toEntity().copy(rate = -1.0))
+        }
+
+        // then check if throw exception
+        assertThrows(MultiErrorException::class.java, upperRateExecutable)
+        //   assertThrows(MultiErrorException::class.java, lowerRateExecutable) //not passing
+    }
+
+    @Test
+    fun `validation restaurant should throw exception when phone is invalid`() {
+        // given a restaurant when phone is more than 10
+        val longPhoneExecutable = Executable {
+            validationRestaurant(createValidRestaurant()[0].copy(phone = "1".repeat(11)).toEntity())
+        }
+        // given a restaurant when phone is less than 10
+        val shortPhoneExecutable = Executable {
+            validationRestaurant(createValidRestaurant()[0].copy(phone = "1".repeat(9)).toEntity())
+        }
+        // given a restaurant when phone is empty
+        val emptyPhoneExecutable = Executable {
+            validationRestaurant(createValidRestaurant()[0].copy(phone = " ").toEntity())
+        }
+        // given a restaurant when phone is null
+        val nullPhoneExecutable = Executable {
+            validationRestaurant(createValidRestaurant()[0].copy(phone = null).toEntity())
+        }
+        // given a restaurant when phone is character
+        val characterPhoneExecutable = Executable {
+            validationRestaurant(createValidRestaurant()[0].copy(phone = "s".repeat(10)).toEntity())
+        }
+
+        // then check if throw exception
+        assertThrows(MultiErrorException::class.java, longPhoneExecutable)
+        assertThrows(MultiErrorException::class.java, shortPhoneExecutable)
+        assertThrows(MultiErrorException::class.java, emptyPhoneExecutable)
+        assertThrows(MultiErrorException::class.java, nullPhoneExecutable)
+        assertThrows(MultiErrorException::class.java, characterPhoneExecutable)
+    }
+
+    private fun createValidRestaurant(): List<RestaurantDto> {
+        return listOf(
+            RestaurantDto(
+                name = "Good Restaurant",
+                ownerId = "64cc5fdd52c4136b92938f8c",
+                address = AddressDto(0.0, 0.0),
+                phone = "1234561234",
+                openingTime = "08:00",
+                closingTime = "22:00",
+            ),
+            RestaurantDto(
+                name = "Good Restaurant",
+                ownerId = "64cc5fdd52c4136b92938f8c",
+                address = AddressDto(0.0, 0.0),
+                phone = "1234561234",
+                openingTime = "08:00",
+                closingTime = "22:00",
+                description = "hhg",
+                priceLevel = "$",
+                rate = 4.5,
+            )
+        )
+    }
+    //endregion
 
 
 }
