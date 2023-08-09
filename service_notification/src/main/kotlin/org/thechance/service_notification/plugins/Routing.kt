52e5dd2efb61d4e0c819891aa30294d524c0c149
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-08-09 18:07:35 +0300

    change notification endpoints name

--- service_notification/src/main/kotlin/org/thechance/service_notification/plugins/Routing.kt
+++ service_notification/src/main/kotlin/org/thechance/service_notification/plugins/Routing.kt
@@ -3,12 +3,12 @@ package org.thechance.service_notification.plugins
 import io.ktor.server.application.Application
 import io.ktor.server.routing.routing
 import org.thechance.service_notification.endpoints.notificationHistoryRoutes
-import org.thechance.service_notification.endpoints.notificationManagementRoutes
+import org.thechance.service_notification.endpoints.notificationRoutes
 
 fun Application.configureRouting(
 ) {
     routing {
         notificationHistoryRoutes()
-        notificationManagementRoutes()
+        notificationRoutes()
     }
 }
