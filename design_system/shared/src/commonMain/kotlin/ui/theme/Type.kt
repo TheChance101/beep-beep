dbb3f8709d022077098a8db9cc0ead612069e509
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-08-09 20:46:01 +0200

    reformat code

--- design_system/shared/src/commonMain/kotlin/ui/theme/Type.kt
+++ design_system/shared/src/commonMain/kotlin/ui/theme/Type.kt
@@ -2,12 +2,10 @@ package com.beepbeep.designSystem.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import com.beepbeep.designSystem.fontResources
-import com.beepbeep.designSystem.ui.theme.BeepBeepTheme.colorScheme
 import com.beepbeep.designSystem.ui.theme.BeepBeepTheme.dimens
 
 @Composable
@@ -17,16 +15,16 @@ fun Typography(): Typography {
         headlineLarge = TextStyle(
             fontSize = dimens.fontSize24,
             lineHeight = dimens.lineHeight32,
-            fontFamily = FontFamily(fontResources( "roboto_medium")),
+            fontFamily = FontFamily(fontResources("roboto_medium")),
             fontWeight = FontWeight.W600,
         ),
         headlineMedium = TextStyle(
             fontSize = dimens.fontSize20,
-            fontFamily = FontFamily(fontResources( "roboto_medium")),
+            fontFamily = FontFamily(fontResources("roboto_medium")),
             fontWeight = FontWeight.W600,
         ),
         titleLarge = TextStyle(
-            fontFamily = FontFamily(fontResources( "roboto_medium")),
+            fontFamily = FontFamily(fontResources("roboto_medium")),
             fontSize = dimens.fontSize16,
             lineHeight = dimens.lineHeight20,
             fontWeight = FontWeight.W600,
@@ -49,8 +47,8 @@ fun Typography(): Typography {
         ),
         labelMedium = TextStyle(
             fontSize = dimens.fontSize12,
-            fontFamily = FontFamily(fontResources( "roboto_regular")),
+            fontFamily = FontFamily(fontResources("roboto_regular")),
             fontWeight = FontWeight.W400,
         ),
-        )
+    )
 }
