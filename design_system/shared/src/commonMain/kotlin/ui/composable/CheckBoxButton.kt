2572edd775d84fdf961f6239f89d958df1c6edeb
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-09 19:10:06 +0300

    fix checkbox padding start

--- design_system/shared/src/commonMain/kotlin/ui/composable/CheckBoxButton.kt
+++ design_system/shared/src/commonMain/kotlin/ui/composable/CheckBoxButton.kt
@@ -5,29 +5,43 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Checkbox
 import androidx.compose.material.CheckboxDefaults
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.LocalMinimumInteractiveComponentEnforcement
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.beepbeep.designSystem.ui.theme.BeepBeepTheme.colorScheme
+import com.beepbeep.designSystem.ui.theme.BeepBeepTheme.dimens
 import com.beepbeep.designSystem.ui.theme.BeepBeepTheme.typography
+
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun CheckBoxButton(
+    modifier: Modifier = Modifier,
     text: String = "",
-    isChecked: Boolean =false,
+    isChecked: Boolean = false,
     onCheck: (Boolean) -> Unit,
-){
-    Row(horizontalArrangement = Arrangement.Start, verticalAlignment = Alignment.CenterVertically) {
-        Checkbox(
-            checked = isChecked,
-            interactionSource = remember { MutableInteractionSource() },
-            colors=   CheckboxDefaults.colors(
-                checkmarkColor =  colorScheme.background,
-                checkedColor = colorScheme.primary,
-                uncheckedColor=colorScheme.onPrimary
-            ),
-            onCheckedChange = { onCheck(it) },
-        )
-        Text(text = text, style = typography.bodyLarge,color = colorScheme.onPrimary)
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(dimens.space16),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        CompositionLocalProvider(LocalMinimumInteractiveComponentEnforcement provides false) {
+            Checkbox(
+                checked = isChecked,
+                interactionSource = remember { MutableInteractionSource() },
+                colors = CheckboxDefaults.colors(
+                    checkmarkColor = colorScheme.background,
+                    checkedColor = colorScheme.primary,
+                    uncheckedColor = colorScheme.onPrimary
+                ),
+                onCheckedChange = { onCheck(it) },
+            )
+        }
+        Text(text = text, style = typography.bodyLarge, color = colorScheme.onPrimary)
     }
 }
