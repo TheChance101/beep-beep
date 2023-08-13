fb03806c5fdcc9c5d324fb5f576565350568b18e
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-08-13 16:39:01 +0300

    take cluster from environments variables

--- service_notification/src/main/kotlin/org/thechance/service_notification/di/NotificationModule.kt
+++ service_notification/src/main/kotlin/org/thechance/service_notification/di/NotificationModule.kt
@@ -17,7 +17,7 @@ val kmongoModule = module {
         val cluster = System.getenv("cluster")
         val username = System.getenv("username")
         val password = System.getenv("password")
-        KMongo.createClient("mongodb+srv://$username:$password@cluster0.0k23cqq.mongodb.net/")
+        KMongo.createClient("mongodb+srv://$username:$password@$cluster.mongodb.net/")
     }
 
     single {
