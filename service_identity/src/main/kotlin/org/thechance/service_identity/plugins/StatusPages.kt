f7c94f0da58a82cabd540f36ee32a1c059fe95c9
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-08-13 14:25:05 +0300

    delete unnecessary exception handling function

--- service_identity/src/main/kotlin/org/thechance/service_identity/plugins/StatusPages.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/plugins/StatusPages.kt
@@ -2,7 +2,6 @@ package org.thechance.service_identity.plugins
 
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import org.thechance.service_identity.domain.entity.*
@@ -18,13 +17,11 @@ private fun StatusPagesConfig.handleStatusPagesExceptions() {
 
     respondWithErrorCodes<RequestValidationException>(HttpStatusCode.BadRequest)
 
-    respondWithErrorCodes<NotFoundException>(HttpStatusCode.NotFound)
-
     respondWithErrorCodes<UserAlreadyExistsException>(HttpStatusCode.UnprocessableEntity)
 
     respondWithErrorCodes<ResourceNotFoundException>(HttpStatusCode.NotFound)
 
-    respondWithErrorCodes<InsufficientFundsException>(HttpStatusCode.BadRequest)
+    respondWithErrorCodes<InsufficientFundsException>(HttpStatusCode.UnprocessableEntity)
 }
 
 private inline fun <reified T : Throwable> StatusPagesConfig.respondWithErrorCodes(
