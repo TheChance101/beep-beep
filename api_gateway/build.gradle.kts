c79670295ea66ef919bd46bbf4c9dcdfd9c0e37e
Aziza <aziza.helmy35@gmail.com>
2023-08-13 18:19:18 +0300

    Api gateway | add required dependencies

--- api_gateway/build.gradle.kts
+++ api_gateway/build.gradle.kts
@@ -1,17 +1,18 @@
-import groovy.xml.dom.DOMCategory.attributes
-import org.jetbrains.kotlin.com.intellij.openapi.vfs.StandardFileSystems.jar
-
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
-val exposed_version : String by project
-val h2_version : String by project
+val exposed_version: String by project
+val h2_version: String by project
+val koin_version: String by project
+val koin_ktor: String by project
+val koin_ksp_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.10"
     id("io.ktor.plugin") version "2.2.4"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
     id("application")
+    id("com.google.devtools.ksp") version "1.8.21-1.0.11"
 }
 
 group = "org.thechance.api_gateway"
@@ -37,7 +38,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-
+    //ktor-server
     implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-swagger:$ktor_version")
     implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
@@ -46,5 +47,22 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    // koin
+    implementation("io.insert-koin:koin-ktor:$koin_ktor")
+    implementation("io.insert-koin:koin-core:$koin_version")
+    implementation("io.insert-koin:koin-annotations:$koin_ksp_version")
+    implementation("com.google.devtools.ksp:symbol-processing-api:1.8.10-1.0.9")
+    ksp("io.insert-koin:koin-ksp-compiler:$koin_ksp_version")
+    // koinLogger
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
+    //security
+    implementation("commons-codec:commons-codec:1.15")
+    //ktor-client
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-okhttp:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-client-logging-jvm:1.5.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
+
 }
 
