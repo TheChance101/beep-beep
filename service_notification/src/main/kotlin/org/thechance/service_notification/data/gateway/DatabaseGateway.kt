d94dd5ca9eb9aa5c24a71ee64a2fe0205a49ba99
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-08-14 03:31:49 +0300

    make get topics return list of topics names

--- service_notification/src/main/kotlin/org/thechance/service_notification/data/gateway/DatabaseGateway.kt
+++ service_notification/src/main/kotlin/org/thechance/service_notification/data/gateway/DatabaseGateway.kt
@@ -47,7 +47,7 @@ class DatabaseGateway(
     }
 
     override suspend fun getTopics(): List<String> {
-        return databaseContainer.topicCollection.find().toList().map { it.toString() }
+        return databaseContainer.topicCollection.find().toList().map { it.name }
     }
 
     override suspend fun isTopicAlreadyExists(name: String): Boolean {
