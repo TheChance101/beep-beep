edb27990231582cc1ee823cd322766e29c5f283e
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-08-14 03:17:18 +0300

    refactor register token use case

--- service_notification/src/main/kotlin/org/thechance/service_notification/domain/usecases/RegisterTokenUseCase.kt
+++ service_notification/src/main/kotlin/org/thechance/service_notification/domain/usecases/RegisterTokenUseCase.kt
@@ -3,19 +3,18 @@ package org.thechance.service_notification.domain.usecases
 import org.koin.core.annotation.Single
 import org.thechance.service_notification.domain.gateway.IDatabaseGateway
 
+
+interface IRegisterTokenUseCase {
+    suspend operator fun invoke(userId: String, token: String): Boolean
+}
+
 @Single
 class RegisterTokenUseCase(
     private val databaseGateway: IDatabaseGateway
 ) : IRegisterTokenUseCase {
 
-    override suspend fun invoke(userId: String, token: String, group: String): Boolean {
+    override suspend fun invoke(userId: String, token: String): Boolean {
         return databaseGateway.registerToken(userId, token)
     }
-
-}
-
-interface IRegisterTokenUseCase {
-
-    suspend operator fun invoke(userId: String, token: String, group: String): Boolean
 
 }
