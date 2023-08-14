4fc16473e64d9d71cff7443076bbc17b446fc505
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-08-14 03:03:48 +0300

    create exceptions

--- service_notification/src/main/kotlin/org/thechance/service_notification/domain/entity/Exceptions.kt
+++ service_notification/src/main/kotlin/org/thechance/service_notification/domain/entity/Exceptions.kt
@@ -7,3 +7,5 @@ class NotFoundException(message: String) : NotificationServiceExceptions(message
 class MissingRequestParameterException(message: String) : NotificationServiceExceptions(message)
 
 class InternalServerErrorException(message: String) : NotificationServiceExceptions(message)
+
+class ResourceAlreadyExistsException(message: String) : NotificationServiceExceptions(message)
