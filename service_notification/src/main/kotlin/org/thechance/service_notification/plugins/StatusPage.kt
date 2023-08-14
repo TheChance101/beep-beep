9a4f954edf7d3d22f06e7f8702a9d34a600c432f
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-08-14 03:28:09 +0300

    add new exception to status page plugin

--- service_notification/src/main/kotlin/org/thechance/service_notification/plugins/StatusPage.kt
+++ service_notification/src/main/kotlin/org/thechance/service_notification/plugins/StatusPage.kt
@@ -5,7 +5,9 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
+import org.thechance.service_notification.domain.entity.InternalServerErrorException
 import org.thechance.service_notification.domain.entity.NotFoundException
+import org.thechance.service_notification.domain.entity.ResourceAlreadyExistsException
 
 fun Application.configureStatusPage() {
     install(StatusPages) {
@@ -21,6 +23,14 @@ fun StatusPagesConfig.handleExceptions() {
 
     exception<NotFoundException> { call, cause ->
         call.respond(HttpStatusCode.NotFound, listOf(cause.message?.toInt()))
+    }
+
+    exception<InternalServerErrorException> { call, cause ->
+        call.respond(HttpStatusCode.InternalServerError, listOf(cause.message?.toInt()))
+    }
+
+    exception<ResourceAlreadyExistsException> { call, cause ->
+        call.respond(HttpStatusCode.Conflict, listOf(cause.message?.toInt()))
     }
 
 }
