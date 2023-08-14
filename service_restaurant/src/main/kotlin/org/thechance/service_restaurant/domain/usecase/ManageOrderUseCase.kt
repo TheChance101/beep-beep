303a2bfbde938849ebbb123a364b6a2d0ac9d015
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-14 11:30:25 +0300

    implement get and update order state

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/domain/usecase/ManageOrderUseCase.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/domain/usecase/ManageOrderUseCase.kt
@@ -4,8 +4,7 @@ import org.thechance.service_restaurant.domain.entity.Order
 import org.thechance.service_restaurant.domain.gateway.IRestaurantOptionsGateway
 import org.thechance.service_restaurant.domain.utils.OrderStatus
 import org.thechance.service_restaurant.domain.utils.Validation
-import org.thechance.service_restaurant.domain.utils.exceptions.MultiErrorException
-import org.thechance.service_restaurant.domain.utils.exceptions.NOT_FOUND
+import org.thechance.service_restaurant.domain.utils.exceptions.*
 
 interface IManageOrderUseCase {
     suspend fun getOrderById(orderId: String): Order
@@ -16,14 +15,23 @@ class ManageOrderUseCase(
     private val optionsGateway: IRestaurantOptionsGateway,
     private val basicValidation: Validation,
 ) : IManageOrderUseCase {
+
     override suspend fun getOrderById(orderId: String): Order {
-        require(basicValidation.isValidId(orderId)) { throw MultiErrorException(listOf(NOT_FOUND)) }
+        if (!basicValidation.isValidId(orderId)) {
+            throw MultiErrorException(listOf(INVALID_ID))
+        }
         return optionsGateway.getOrderById(orderId)!!
     }
 
 
     override suspend fun updateOrderState(orderId: String, state: OrderStatus): Order {
-
+        if (!basicValidation.isValidId(orderId)) {
+            throw MultiErrorException(listOf(INVALID_ID))
+        } else if (state.statusCode !in 0..4) {
+            throw MultiErrorException(listOf(INVALID_STATE))
+        } else {
+            return optionsGateway.updateOrderState(orderId, state)!!
+        }
     }
 
 }
