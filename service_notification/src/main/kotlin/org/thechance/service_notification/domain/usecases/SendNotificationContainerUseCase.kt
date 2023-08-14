bed92fad612421bbbf4f7f59643ce7c2a89b8cc1
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-08-14 03:16:23 +0300

    create notification management use case

--- service_notification/src/main/kotlin/org/thechance/service_notification/domain/usecases/SendNotificationContainerUseCase.kt
+++ service_notification/src/main/kotlin/org/thechance/service_notification/domain/usecases/SendNotificationContainerUseCase.kt
@@ -1,35 +1,56 @@
 package org.thechance.service_notification.domain.usecases
 
 import org.koin.core.annotation.Single
-import org.thechance.service_notification.domain.entity.NotificationRequest
+import org.thechance.service_notification.domain.entity.NotFoundException
+import org.thechance.service_notification.domain.entity.Notification
 import org.thechance.service_notification.domain.gateway.IDatabaseGateway
 import org.thechance.service_notification.domain.gateway.IPushNotificationGateway
+import org.thechance.service_notification.endpoints.TOPIC_NOT_EXISTS
+
+interface ISendNotificationContainerUseCase {
+    suspend fun sendNotificationToUser(userId: String, title: String, body: String): Boolean
+
+    suspend fun sendNotificationToTopic(topic: String, title: String, body: String): Boolean
+
+    suspend fun getNotificationHistory(page: Int, limit: Int): List<Notification>
+}
 
 @Single
 class SendNotificationContainerUseCase(
-    private val pusNotificationGateway: IPushNotificationGateway,
+    private val pushNotificationGateway: IPushNotificationGateway,
     private val databaseGateway: IDatabaseGateway,
 ) : ISendNotificationContainerUseCase {
 
-    private suspend fun sendNotification(tokens: List<String>, title: String, body: String): Boolean {
-        return pusNotificationGateway.sendNotification(tokens, title, body)
-    }
-
     override suspend fun sendNotificationToUser(userId: String, title: String, body: String): Boolean {
-        val tokens = databaseGateway.getUsersTokens(listOf(userId))
-        return sendNotification(tokens, title, body).also { updateNotificationHistory(title, body, listOf(userId)) }
+        val tokens = databaseGateway.getUserTokens(userId)
+        return pushNotificationGateway.sendNotification(tokens, title, body).also {
+            databaseGateway.addNotificationToHistory(
+                Notification(
+                    title = title,
+                    body = body,
+                    date = System.currentTimeMillis(),
+                    userId = userId
+                )
+            )
+        }
     }
 
-    private suspend fun updateNotificationHistory(title: String, body: String, userIds: List<String>) {
-        databaseGateway.addNotificationToUserHistory(
-            NotificationRequest(title = title, body = body, date = System.currentTimeMillis(), userIds = userIds)
-        )
+    override suspend fun sendNotificationToTopic(topic: String, title: String, body: String): Boolean {
+        if (!databaseGateway.isTopicAlreadyExists(topic)) throw NotFoundException(TOPIC_NOT_EXISTS)
+        return pushNotificationGateway.sendNotificationToTopic(topic, title, body).also {
+            databaseGateway.addNotificationToHistory(
+                Notification(
+                    title = title,
+                    body = body,
+                    date = System.currentTimeMillis(),
+                    topic = topic
+                )
+            )
+        }
     }
 
-}
-
-interface ISendNotificationContainerUseCase {
-
-    suspend fun sendNotificationToUser(userId: String, title: String, body: String): Boolean
+    override suspend fun getNotificationHistory(page: Int, limit: Int): List<Notification> {
+        return databaseGateway.getNotificationHistory(page, limit)
+    }
 
 }
