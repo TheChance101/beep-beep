41c97abaff3baa29a4f58f0dffd3f93070909964
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-14 11:44:11 +0300

    inject IManageOrderUseCase

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/di/UseCasesModule.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/di/UseCasesModule.kt
@@ -13,10 +13,11 @@ val UseCasesModule = module {
     single<IValidation> { Validation() }
     single<ICategoryValidationUseCase> { CategoryValidationUseCase(get()) }
     single<IRestaurantValidationUseCase> { RestaurantValidationUseCase(get()) }
-    single<IControlRestaurantsUseCase> { ControlRestaurantsUseCase(get(), get(),get()) }
+    single<IControlRestaurantsUseCase> { ControlRestaurantsUseCase(get(), get(), get()) }
     single<IDiscoverRestaurantUseCase> { DiscoverRestaurantUseCase(get(), get(), get()) }
-    single<IManageCategoryUseCase> { ManageCategoryUseCase(get(), get(),get()) }
+    single<IManageCategoryUseCase> { ManageCategoryUseCase(get(), get(), get()) }
     single<IManageCuisineUseCase> { ManageCuisineUseCase(get(), get()) }
-    single<IManageMealUseCase> { ManageMealUseCase(get(), get(), get(),get()) }
-    single<IManageRestaurantDetailsUseCase> { ManageRestaurantDetailsUseCase(get(), get(), get(),get()) }
+    single<IManageMealUseCase> { ManageMealUseCase(get(), get(), get(), get()) }
+    single<IManageRestaurantDetailsUseCase> { ManageRestaurantDetailsUseCase(get(), get(), get(), get()) }
+    single<IManageOrderUseCase> { ManageOrderUseCase(get(), get(), get()) }
 }
