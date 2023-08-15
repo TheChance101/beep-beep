3ad0a781705219858de4f9da2e319006e564a3b1
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-08-15 09:29:29 +0300

    fast paginate test

--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/ui/composables/table/BpTable.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/ui/composables/table/BpTable.kt
@@ -2,26 +2,19 @@ package org.thechance.common.ui.composables.table
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -34,14 +27,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.beepbeep.designSystem.ui.theme.BpTheme
 import com.beepbeep.designSystem.ui.theme.Theme
-import java.util.UUID
+import kotlinx.coroutines.delay
 
 /**
  * @param rowsCount number of rows in the table without header row
@@ -107,6 +99,9 @@ fun <T> BpTable(
 @Composable
 fun BpTablePreview() {
     var selectedUser by remember { mutableStateOf<String?>(null) }
+    var offset by remember { mutableStateOf(0) }
+    val pageCount = 9
+
     BpTheme(useDarkTheme = false) {
         Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
             val headers = listOf(
@@ -118,13 +113,13 @@ fun BpTablePreview() {
                 "Permission",
                 "",
             )
-            val users = getDummyUsers() * 1
+            val users = getDummyUsers() * 3
 
             BpTable(
                 data = users,
                 headers = headers,
-                rowsCount = 9,
-                offset = 0,
+                rowsCount = pageCount,
+                offset = offset,
                 modifier = Modifier.fillMaxWidth(0.9f),
             ) { user ->
                 UserRow(
@@ -137,149 +132,11 @@ fun BpTablePreview() {
             LaunchedEffect(selectedUser) {
                 if (selectedUser != null) {
                     println("Selected user: $selectedUser")
+                    delay(1000)
+                    offset += pageCount
                 }
             }
         }
     }
 }
 
-operator fun <E> List<E>.times(i: Int): List<E> {
-    return (0 until i).flatMap { this }
-}
-
-@OptIn(ExperimentalLayoutApi::class)
-@Composable
-fun RowScope.UserRow(
-    onClickEditUser: (userId: String) -> Unit,
-    position: Int,
-    user: UserRowUIState,
-    firstColumnWeight: Float = 1f,
-    otherColumnsWeight: Float = 3f,
-) {
-    Text(
-        position.toString(),
-        style = Theme.typography.titleMedium.copy(color = Theme.colors.contentTertiary),
-        modifier = Modifier.weight(firstColumnWeight),
-        maxLines = 1,
-        overflow = TextOverflow.Ellipsis,
-    )
-
-    Row(Modifier.weight(otherColumnsWeight), verticalAlignment = Alignment.CenterVertically) {
-        Image(painter = painterResource(user.photoPath), contentDescription = null)
-        Text(
-            user.fullName,
-            modifier = Modifier.padding(start = 16.dp),
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-        )
-    }
-
-    Text(
-        user.username,
-        overflow = TextOverflow.Ellipsis,
-        modifier = Modifier.weight(otherColumnsWeight),
-        maxLines = 1,
-    )
-    Text(
-        user.email,
-        overflow = TextOverflow.Ellipsis,
-        modifier = Modifier.weight(otherColumnsWeight),
-        maxLines = 1,
-    )
-    Text(
-        user.country,
-        overflow = TextOverflow.Ellipsis,
-        modifier = Modifier.weight(otherColumnsWeight),
-        maxLines = 1,
-    )
-    FlowRow(
-        Modifier.weight(otherColumnsWeight),
-        maxItemsInEachRow = 3,
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        user.permissions.forEach {
-            Icon(
-                painter = painterResource(it.iconPath),
-                contentDescription = null,
-                tint = Theme.colors.contentPrimary.copy(alpha = 0.87f),
-                modifier = Modifier.size(24.dp)
-            )
-        }
-    }
-
-    Image(
-        painter = painterResource("horizontal_dots.xml"),
-        contentDescription = null,
-        modifier = Modifier.clickable { onClickEditUser(user.id) }
-            .weight(firstColumnWeight)
-    )
-}
-
-private fun getDummyUsers() = listOf(
-    UserRowUIState(
-        id = UUID.randomUUID().toString(),
-        photoPath = "dummy_img.png",
-        fullName = "Ali Mohammed",
-        username = "@Ali_ahmed",
-        email = "Aliahmed@gmail.com",
-        country = "Egypt",
-        permissions = listOf(
-            UserRowUIState.Permission.ADMIN,
-            UserRowUIState.Permission.RESTAURANT,
-            UserRowUIState.Permission.SUPPORT,
-        ),
-    ),
-    UserRowUIState(
-        id = UUID.randomUUID().toString(),
-        photoPath = "dummy_img.png",
-        fullName = "Mohammed Sayed",
-        username = "@Mohammed_sayed",
-        email = "Mosayed@gmail.com",
-        country = "Egypt",
-        permissions = listOf(
-            UserRowUIState.Permission.END_USER,
-        ),
-    ),
-    UserRowUIState(
-        id = UUID.randomUUID().toString(),
-        photoPath = "dummy_img.png",
-        fullName = "Mohammed Sayed",
-        username = "@Mohammed_sayed",
-        email = "Mosayed@gmail.com",
-        country = "Egypt",
-        permissions = listOf(
-            UserRowUIState.Permission.DRIVER,
-            UserRowUIState.Permission.DELIVERY,
-        ),
-    ),
-    UserRowUIState(
-        id = UUID.randomUUID().toString(),
-        photoPath = "dummy_img.png",
-        fullName = "Mohammed Sayed",
-        username = "@Mohammed_sayed",
-        email = "Mosayed@gmail.com",
-        country = "Egypt",
-        permissions = listOf(
-            UserRowUIState.Permission.END_USER,
-        ),
-    ),
-)
-
-class UserRowUIState(
-    val id: String,
-    val photoPath: String,
-    val fullName: String,
-    val username: String,
-    val email: String,
-    val country: String,
-    val permissions: List<Permission>,
-) {
-    enum class Permission(val iconPath: String) {
-        RESTAURANT("outline_restaurants.xml"),
-        DRIVER("wheel.xml"),
-        END_USER("end_user.xml"),
-        SUPPORT("support.xml"),
-        DELIVERY("delivery_guy.xml"),
-        ADMIN("chart.xml"),
-    }
-}
