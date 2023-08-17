13fd90f6820e645a61b5a620c8bb9fd49b124452
ay0o5h <aayosh553@gmail.com>
2023-08-17 10:21:12 +0300

    change name

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/di/HelperModule.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/di/HelperModule.kt
@@ -1,7 +1,8 @@
 package org.thechance.service_restaurant.di
 
 import org.koin.dsl.module
+import org.thechance.service_restaurant.api.utils.SocketHandler
 
 val HelperModule = module {
-    single<OrderSocketHandler> { OrderSocketHandler() }
+    single<SocketHandler> { SocketHandler() }
 }
