5a9b0d12973c5fd16407b5f2f1a96e0e6f11ddb5
Nada Feteiha <nada.feteiha@gmail.com>
2023-08-17 11:08:04 -0700

    add BasicTextField

--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/ui/composables/table/TotalItemsIndicator.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/ui/composables/table/TotalItemsIndicator.kt
@@ -1,16 +1,31 @@
 package org.thechance.common.ui.composables.table
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.beepbeep.designSystem.ui.composable.BpOutlinedButton
+import com.beepbeep.designSystem.ui.composable.BpTextField
 import com.beepbeep.designSystem.ui.theme.Theme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TotalItemsIndicator(
     modifier: Modifier = Modifier,
@@ -24,12 +39,17 @@ fun TotalItemsIndicator(
         horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
 
-        BpOutlinedButton(
-            modifier = modifier.size(32.dp),
-            title = "$numberItemInPage",
-            onClick = {},
-            enabled = false
+        BasicTextField(
+            modifier = Modifier
+                .border(
+                    width = 2.dp, color = Color.Gray,
+                    shape = RoundedCornerShape(8.dp)
+                ).padding(8.dp).width(40.dp),
+            value = "100",
+            onValueChange = {},
+            textStyle = TextStyle().copy(textAlign = TextAlign.Center)
         )
+
 
         Text(
             "item out of $totalItems item",
