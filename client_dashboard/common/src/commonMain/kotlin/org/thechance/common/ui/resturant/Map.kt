c35ce66e71aeb3683a1d23d4920fb61579a5c6a1
Asia sama <sehunexo710@gmail.com>
2023-08-18 20:33:34 +0300

    add event to map

--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/ui/resturant/Map.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/ui/resturant/Map.kt
@@ -7,6 +7,9 @@ import javafx.scene.web.WebEngine
 import javafx.scene.web.WebView
 import org.thechance.common.Testttt
 import java.awt.BorderLayout
+import java.awt.event.ActionEvent
+import java.awt.event.FocusEvent
+import javax.swing.AbstractAction
 import javax.swing.JPanel
 import javax.swing.SwingUtilities
 import javax.swing.border.EmptyBorder
@@ -19,15 +22,23 @@ fun createWebViewComponent(): JFXPanel {
         val scene = Scene(webView)
         jfxPanel.scene = scene
 
-        webEngine.load("https://www.google.com/maps/@47.8071501,-122.2181212,14z?entry=ttu")
-//          webEngine.load("https://www.openstreetmap.org/#map=4/58.1/97.4")
+
+      //  webEngine.load("https://www.google.com/maps/@47.8071501,-122.2181212,14z?entry=ttu")
+        webEngine.load("https://www.openstreetmap.org/#map=15/33.2901/44.4034")
 
 
         webEngine.locationProperty().addListener { a, b, newLocation ->
-            println("Location: ${newLocation}")//.split(",")[0].substringAfter("@")}")
+            println("Location: ${newLocation.split("#map=")[1].substringBefore("#map=")}")
+           // println("Location: $newLocation")
 //            println("a: $a")
 //            println("b: $b")
         }
+        jfxPanel.actionMap.put("requestFocus", object : AbstractAction() {
+            override fun actionPerformed(e: ActionEvent?) {
+                println("requestFocus")
+                jfxPanel.requestFocusInWindow(FocusEvent.Cause.MOUSE_EVENT)
+            }
+        })
 
         webEngine.javaScriptEnabledProperty().set(true)
         println("userAgent: ${webEngine.userAgent}")
@@ -59,20 +70,21 @@ fun SwingComponent4(): JPanel {
         val jfxPanel = JFXPanel()
         add(jfxPanel, BorderLayout.CENTER)
 
+        jfxPanel.requestFocusInWindow(FocusEvent.Cause.MOUSE_EVENT)
         Platform.runLater {
             val webView = WebView()
             val webEngine: WebEngine = webView.engine
             val scene = Scene(webView)
             jfxPanel.scene = scene
+
             val htmlResource = Testttt::class.java.getResource("/testweb.html")
             val htmlContent = htmlResource?.readText()?.trimIndent()
 
             webEngine.loadContent(htmlContent)
+            webEngine.javaScriptEnabledProperty().set(true)
 
-            webEngine.locationProperty().addListener { a, b, newLocation ->
-                println("Location: ${newLocation}")
-            }
         }
+
     }
     SwingUtilities.invokeLater {
         jPanel.revalidate()
@@ -80,3 +92,4 @@ fun SwingComponent4(): JPanel {
     }
     return jPanel
 }
+
--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/ui/resturant/RestaurantDialog.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/ui/resturant/RestaurantDialog.kt
@@ -2,6 +2,7 @@ package org.thechance.common.ui.resturant
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -95,14 +97,12 @@ fun RestaurantDialog(
                         hint = ""
                     )
                 }
-
-                SwingPanel(
-                    modifier = Modifier.fillMaxSize(),
-                    factory = {
-                        SwingComponent4()
-                    }
-                )
-
+                Box(Modifier.clip(RoundedCornerShape(24.dp)).fillMaxSize()) {
+                    SwingPanel(
+                        modifier = Modifier.matchParentSize(),
+                        factory = { SwingComponent5() },
+                    )
+                }
             }
         }
     }
