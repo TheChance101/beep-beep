c86112905f424e1d289b646e250232ba0eedb9c9
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-08-19 02:15:28 +0300

    add user authentication use case

---
+++ client_end_user/shared/src/commonMain/kotlin/domain/usecase/UserAuthenticationUseCase.kt
@@ -0,0 +1,31 @@
+package domain.usecase
+
+import domain.entity.Tokens
+import domain.gateway.IRemoteGateway
+
+class UserAuthenticationUseCase (private val remoteGateway: IRemoteGateway) : IUserAuthenticationUseCase {
+
+    override suspend fun createUser(fullName: String, username: String, password: String, email: String): Boolean {
+        return remoteGateway.createUser(fullName, username, password, email)
+    }
+
+    override suspend fun loginUser(userName: String, password: String): Tokens {
+        return remoteGateway.loginUser(userName, password)
+    }
+
+}
+
+interface IUserAuthenticationUseCase{
+    suspend fun createUser(
+        fullName: String,
+        username: String,
+        password: String,
+        email: String
+    ): Boolean
+
+    suspend fun loginUser(
+        userName: String,
+        password: String,
+    ): Tokens
+
+}
