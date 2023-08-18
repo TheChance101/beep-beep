a8036383c9c603993a9b597e8a10197a93270a23
Nada Feteiha <nada.feteiha@gmail.com>
2023-08-17 17:29:17 -0700

    fix horizontal dots color

--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/composables/table/UserUIState.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/composables/table/UserUIState.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -110,7 +111,8 @@ fun RowScope.UserRow(
         painter = painterResource("horizontal_dots.xml"),
         contentDescription = null,
         modifier = Modifier.clickable { onClickEditUser(user.id) }
-            .weight(firstColumnWeight)
+            .weight(firstColumnWeight),
+        colorFilter = ColorFilter.tint(color = Theme.colors.contentPrimary)
     )
 }
 
