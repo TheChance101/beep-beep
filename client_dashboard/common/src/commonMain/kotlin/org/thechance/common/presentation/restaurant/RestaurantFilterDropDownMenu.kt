386ed41e9758ce29450710e8c106fca5ab3b9ea6
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-08-18 15:37:31 +0300

    refactor spaces in RestaurantFilterDropDownMenu

--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/restaurant/RestaurantFilterDropDownMenu.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/restaurant/RestaurantFilterDropDownMenu.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
@@ -75,7 +76,7 @@ private fun RestaurantFilterDropDownMenuContent(
             selectedIcon = painterResource(if (isSystemInDarkTheme()) "ic_filled_star_dark.svg" else "ic_filled_star_light.svg"),
             halfSelectedIcon = painterResource(if (isSystemInDarkTheme()) "ic_half_filled_star_dark.svg" else "ic_half_filled_star_light.svg"),
             notSelectedIcon = painterResource(if (isSystemInDarkTheme()) "ic_star_dark.svg" else "ic_star_light.svg"),
-            iconsSize = Theme.dimens.space32,
+            iconsSize = Theme.dimens.space24,
             iconsPadding = PaddingValues(horizontal = Theme.dimens.space8),
             modifier = Modifier.fillMaxWidth().padding(top = Theme.dimens.space16)
                 .background(color = Theme.colors.background)
@@ -96,7 +97,7 @@ private fun RestaurantFilterDropDownMenuContent(
             enabledIconsColor = Theme.colors.success,
             disabledIconsColor = Theme.colors.disable,
             iconsPadding = PaddingValues(horizontal = Theme.dimens.space8),
-            iconsSize = Theme.dimens.space32,
+            iconsSize = Theme.dimens.space16,
             modifier = Modifier.fillMaxWidth().padding(top = Theme.dimens.space16)
                 .background(color = Theme.colors.background)
                 .padding(horizontal = Theme.dimens.space24, vertical = Theme.dimens.space16),
@@ -110,12 +111,12 @@ private fun RestaurantFilterDropDownMenuContent(
             BpTextButton(
                 text = "Cancel",
                 onClick = {},
-                modifier = Modifier.padding(end = Theme.dimens.space16).weight(1f)
+                modifier = Modifier.padding(end = Theme.dimens.space16).height(Theme.dimens.space32).weight(1f)
             )
             BpOutlinedButton(
                 title = "Save",
                 onClick = {},
-                modifier = Modifier.weight(3f)
+                modifier = Modifier.height(Theme.dimens.space32).weight(3f)
             )
         }
     }
