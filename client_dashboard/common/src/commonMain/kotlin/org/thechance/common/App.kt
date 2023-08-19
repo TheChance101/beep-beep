623609e434920a89776a60701aac38340a24ab5a
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-08-19 16:10:23 +0300

    add full screen dialog composable

--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/App.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/App.kt
@@ -1,19 +1,37 @@
 package org.thechance.common
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.unit.toSize
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
 import com.beepbeep.designSystem.ui.theme.BpTheme
 import org.thechance.common.presentation.login.LoginScreen
 
+val LocalScreenSize = compositionLocalOf<Size> { error("provide") }
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun App() {
-    BpTheme {
-        Navigator(LoginScreen){
-            SlideTransition(it)
+    var size by remember { mutableStateOf(Size.Zero) }
+
+    CompositionLocalProvider(LocalScreenSize provides size) {
+        Box(Modifier.onSizeChanged { size = it.toSize() }) {
+            BpTheme {
+                Navigator(LoginScreen) {
+                    SlideTransition(it)
+                }
+            }
         }
     }
 }
