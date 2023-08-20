39a6b45f29ea8011f3eb613f6c7652f45ccec3c9
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-08-20 16:59:25 +0300

    create toggle permission logic

--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/users/UserScreenModel.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/users/UserScreenModel.kt
@@ -32,4 +32,46 @@ class UserScreenModel : StateScreenModel<UserScreenUiState>(UserScreenUiState())
     fun onDismissDropDownMenu() {
         mutableState.update { it.copy(isFilterDropdownMenuExpanded = false) }
     }
+
+    fun togglePermission(
+        username: String,
+        permissionUiState: UserScreenUiState.PermissionUiState
+    ) {
+        val user = findUserByUsername(username) ?: return
+        val permissions = getUpdatedPermissions(user.permissions, permissionUiState)
+        updateUserPermissions(username, permissions)
+    }
+
+    private fun findUserByUsername(username: String): UserScreenUiState.UserUiState? {
+        return mutableState.value.users.find { it.username == username }
+    }
+
+    private fun getUpdatedPermissions(
+        permissions: List<UserScreenUiState.PermissionUiState>,
+        permissionUiState: UserScreenUiState.PermissionUiState
+    ): List<UserScreenUiState.PermissionUiState> {
+        return if (permissions.contains(permissionUiState)) {
+            if (permissions.size == 1) return permissions
+            permissions.filterNot { it == permissionUiState }
+        } else {
+            permissions.plus(permissionUiState)
+        }
+    }
+
+    private fun updateUserPermissions(
+        username: String,
+        permissions: List<UserScreenUiState.PermissionUiState>
+    ) {
+        mutableState.update {
+            it.copy(
+                users = it.users.map { userUiState ->
+                    if (userUiState.username == username) {
+                        userUiState.copy(permissions = permissions)
+                    } else {
+                        userUiState
+                    }
+                }
+            )
+        }
+    }
 }
