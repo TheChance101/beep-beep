55841f3067075cc5b62a8b87fbc82417f5382e2e
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-08-21 20:58:48 +0300

    resolve conflict

--- client_restaurant/shared/src/commonMain/kotlin/App.kt
+++ client_restaurant/shared/src/commonMain/kotlin/App.kt
@@ -1,39 +1,22 @@
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.beepbeep.designSystem.ui.theme.Theme
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
-import resources.BpRestaurantTheme
-import resources.Resources
-import cafe.adriel.voyager.core.model.ScreenModel
-import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
-import com.beepbeep.designSystem.ui.theme.BpTheme
 import di.appModule
 import org.koin.compose.KoinApplication
-import org.koin.compose.koinInject
-import org.koin.core.parameter.ParametersDefinition
-import org.koin.core.qualifier.Qualifier
-import org.koin.mp.KoinPlatform
 import presentation.login.LoginScreen
-import presentation.login.LoginScreenModel
+import resources.BpRestaurantTheme
 
-@OptIn(ExperimentalResourceApi::class)
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun App() {
     KoinApplication(application = {
         modules(appModule())
     }) {
         BpRestaurantTheme {
-            Column(modifier = Modifier.fillMaxSize().background(Theme.colors.background)) {
-                Image(painter = painterResource(Resources.images.bpIcon), contentDescription = null)
-                Text(Resources.strings.beepBeep)
+            Navigator(LoginScreen()){
+                SlideTransition(it)
             }
         }
     }
