bfdf2a9c9d238d7369a7dd99aad71f88f9107a3f
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-21 03:44:54 +0300

    refactor IRemoteGateWay functions

--- client_restaurant/shared/src/commonMain/kotlin/domain/gateway/IRemoteGateWay.kt
+++ client_restaurant/shared/src/commonMain/kotlin/domain/gateway/IRemoteGateWay.kt
@@ -7,17 +7,27 @@ import domain.entity.Restaurant
 
 interface IRemoteGateWay {
 
-    // need to add in restaurant service
+    //region restaurant
     suspend fun getRestaurantsByOwnerId(ownerId: String): List<Restaurant>
+    suspend fun updateRestaurantInfo(restaurant: Restaurant): Restaurant
+    suspend fun getRestaurantInfo(restaurantId: String): Restaurant
+    //endregion restaurant
+
+    //region meal
+    suspend fun getMealsByRestaurantId(restaurantId: String): List<Meal>
     suspend fun getMealById(mealId: String): Meal
+    suspend fun addMeal(meal: Meal): Meal
+    suspend fun updateMeal(meal: Meal): Meal
+    //endregion meal
+
+    //region order
     suspend fun getCurrentOrders(restaurantId: String): List<Order>
     suspend fun getOrdersHistory(restaurantId: String): List<Order>
-    suspend fun getRestaurantInfo(restaurantId: String): Restaurant
     suspend fun updateOrderState(orderId: String, orderState: Int): Order
-    suspend fun updateRestaurantInfo(restaurant: Restaurant): Restaurant
-    suspend fun updateMeal(meal: Meal): Meal
-    suspend fun addMeal(meal: Meal): Meal
+    //endregion order
+
+    //region category
     suspend fun getCategoriesByRestaurantId(restaurantId: String): Category
-    suspend fun getMealsByRestaurantId(restaurantId: String): List<Meal>
+    //endregion category
 
 }
