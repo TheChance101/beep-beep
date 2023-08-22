34d7d6476dee11b758a4594ee6b428916debf689
ay0o5h <aayosh553@gmail.com>
2023-08-22 11:28:40 +0300

    create screen Ui

---
+++ client_restaurant/shared/src/commonMain/kotlin/presentation/meals/MealsScreenModel.kt
@@ -0,0 +1,25 @@
+package presentation.meals
+
+import cafe.adriel.voyager.core.model.coroutineScope
+import kotlinx.coroutines.CoroutineScope
+import presentation.base.BaseScreenModel
+
+class MealsScreenModel : BaseScreenModel<MealsScreenUIState,MealsScreenUIEffect>(MealsScreenUIState()),MealScreenInteractionListener{
+
+    override val viewModelScope: CoroutineScope
+        get() = coroutineScope
+
+    override fun onClickBack() {
+        sendNewEffect(MealsScreenUIEffect.Back)
+    }
+
+    override fun onClickMeal() {
+        sendNewEffect(MealsScreenUIEffect.NavigateToMealDetails)
+    }
+
+    override fun onClickCousinType(type: String) {
+        updateState { it.copy(selectedCousin = type) }
+    }
+
+
+}
