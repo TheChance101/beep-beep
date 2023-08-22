936ffc336ef30f8242719702d06b0fbc12217520
Nada Feteiha <nada.feteiha@gmail.com>
2023-08-21 17:05:03 -0700

    add meal state

---
+++ client_restaurant/shared/src/commonMain/kotlin/presentation/meal/MealScreenUIState.kt
@@ -0,0 +1,16 @@
+package presentation.meal
+
+data class MealScreenUIState(
+    val name: String = "",
+    val description: String = "",
+    val price: Double = 0.0,
+    val currency: String = "USD",
+    val isAddEnable:Boolean = false,
+    val cuisines: List<CuisineUIState> = emptyList()
+)
+
+
+data class CuisineUIState(
+    val id: String = "",
+    val name: String = ""
+)
