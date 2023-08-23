fe85916a7654068b8e7c96925970be5cddb4f1c2
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-08-23 19:36:19 +0300

    use better animation

--- client_restaurant/shared/src/commonMain/kotlin/presentation/order/order_history/composables/AnimatedTabLayout.kt
+++ client_restaurant/shared/src/commonMain/kotlin/presentation/order/order_history/composables/AnimatedTabLayout.kt
@@ -1,6 +1,6 @@
 package presentation.order.order_history.composables
 
-import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateOffsetAsState
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -15,14 +15,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.layout.positionInParent
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import com.beepbeep.designSystem.ui.theme.Theme
 
 @Composable
@@ -38,25 +49,38 @@ fun <E : Enum<*>> AnimatedTabLayout(
     borderStroke: BorderStroke? = BorderStroke(1.dp, Theme.colors.divider),
     content: @Composable (E) -> Unit
 ) {
+    var selectedTabOffset by remember { mutableStateOf(Offset.Zero) }
+    var tabSize by remember { mutableStateOf(IntSize.Zero) }
+    val radiusPx = with(LocalDensity.current) { Theme.radius.small.toPx() }
+    val selectedTabOffsetAnimated by animateOffsetAsState(selectedTabOffset)
+
     Row(
         modifier.fillMaxWidth().clip(shape).background(containerColor)
             .then(
                 Modifier.takeIf { borderStroke != null }?.border(borderStroke!!, shape) ?: Modifier
-            ),
+            ).drawBehind {
+                drawRoundRect(
+                    selectedTabColor,
+                    topLeft = selectedTabOffsetAnimated,
+                    size = tabSize.toSize(),
+                    cornerRadius = CornerRadius(radiusPx)
+                )
+            },
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center
     ) {
         tabItems.forEachIndexed { index, tab ->
             AnimatedTab(
                 tab = tab,
-                isSelected = tab == selectedTab,
                 onTabSelected = onTabSelected,
-                selectedTabColor = selectedTabColor,
-                containerColor = containerColor,
                 modifier = Modifier.padding(
                     start = if (index == 0) horizontalPadding else 0.dp,
                     end = if (index == tabItems.size - 1) horizontalPadding else 0.dp
-                ),
+                ).onGloballyPositioned { coordinates ->
+                    if (tab == selectedTab) {
+                        selectedTabOffset = coordinates.positionInParent()
+                    }
+                }.onSizeChanged { size -> if (tab == selectedTab) { tabSize = size } },
                 content = content
             )
         }
@@ -66,21 +90,14 @@ fun <E : Enum<*>> AnimatedTabLayout(
 @Composable
 private fun <E : Enum<*>> RowScope.AnimatedTab(
     tab: E,
-    isSelected: Boolean,
     onTabSelected: (E) -> Unit,
-    selectedTabColor: Color,
-    containerColor: Color,
     modifier: Modifier = Modifier,
     shape: Shape = RoundedCornerShape(Theme.radius.small),
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
     content: @Composable (E) -> Unit
 ) {
-    val color by animateColorAsState(if (isSelected) selectedTabColor else containerColor)
-
     Box(
-        modifier.weight(1f)
-            .clip(shape).background(color, shape)
-            .clickable(
+        modifier.weight(1f).clip(shape).clickable(
                 interactionSource = interactionSource,
                 indication = null,
                 onClick = { onTabSelected(tab) }
