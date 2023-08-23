bf714963355c1c886c01d1347620e46722b70d33
haidy <haidyabugom3a@gmail.com>
2023-08-23 19:46:54 +0300

    complete restaurant screen model

--- client_restaurant/shared/src/commonMain/kotlin/di/ScreenModule.kt
+++ client_restaurant/shared/src/commonMain/kotlin/di/ScreenModule.kt
@@ -4,8 +4,10 @@ import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 import presentation.login.LoginScreenModel
 import presentation.main.MainScreenModel
+import presentation.info.RestaurantInfoScreenModel
 
 val screenModule = module {
     factoryOf(::LoginScreenModel)
     factoryOf(::MainScreenModel)
+    factoryOf(::RestaurantInfoScreenModel)
 }
--- client_restaurant/shared/src/commonMain/kotlin/presentation/info/RestaurantInfoInteractionListener.kt
+++ client_restaurant/shared/src/commonMain/kotlin/presentation/info/RestaurantInfoInteractionListener.kt
@@ -3,11 +3,11 @@ package presentation.info
 import presentation.base.BaseInteractionListener
 
 interface RestaurantInfoInteractionListener: BaseInteractionListener {
-    fun onRestaurantNameChanged()
-    fun onPhoneNumberChanged()
-    fun onStartWorkingHourChanged()
-    fun onEndWorkingHourChanged()
-    fun onDescriptionChanged()
+    fun onRestaurantNameChange(name: String)
+    fun onPhoneNumberChange(phoneNum: String)
+    fun onOpeningTimeChange(openingTime: String)
+    fun onClosingTimeChange(closingTime: String)
+    fun onDescriptionChanged(description: String)
     fun onClickSave()
     fun onClickLogout()
 }
