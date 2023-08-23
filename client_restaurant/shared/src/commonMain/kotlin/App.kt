c13746e513f898004029d8b790a35d23478ec872
Nada Feteiha <nada.feteiha@gmail.com>
2023-08-23 06:15:27 -0700

    call image picker

--- client_restaurant/shared/src/commonMain/kotlin/App.kt
+++ client_restaurant/shared/src/commonMain/kotlin/App.kt
@@ -1,16 +1,25 @@
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
+import presentation.image.ImagePicker
 import presentation.login.LoginScreen
 import resources.BpRestaurantTheme
 
+object Picker {
+    lateinit var imagePicker: ImagePicker
+}
+
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun App() {
+fun App(imagePicker: ImagePicker) {
+    Picker.imagePicker = imagePicker
+
     BpRestaurantTheme {
         Navigator(LoginScreen()) {
             SlideTransition(it)
         }
     }
 }
+
