436501dd8142499d7c0e2193e078f0e5795f31af
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-23 21:57:24 +0300

    try to fix order screen

--- client_restaurant/shared/src/commonMain/kotlin/presentation/composables/OrderStateHeader.kt
+++ client_restaurant/shared/src/commonMain/kotlin/presentation/composables/OrderStateHeader.kt
@@ -1,21 +1,19 @@
 package presentation.composables
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridItemScope
-import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridScope
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridItemSpan
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyGridItemScope
+import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.runtime.Composable
 
-@OptIn(ExperimentalFoundationApi::class)
+/*@OptIn(ExperimentalFoundationApi::class)
 fun LazyStaggeredGridScope.header(
     content: @Composable LazyStaggeredGridItemScope.() -> Unit
 ) {
     item(span = StaggeredGridItemSpan.FullLine, content = content)
-}
+}*/
 
-/*
 fun LazyGridScope.header(
     content: @Composable LazyGridItemScope.() -> Unit
 ) {
     item(span = { GridItemSpan(this.maxCurrentLineSpan) }, content = content)
-}*/
+}
--- client_restaurant/shared/src/commonMain/kotlin/presentation/order/OrderScreen.kt
+++ client_restaurant/shared/src/commonMain/kotlin/presentation/order/OrderScreen.kt
@@ -4,21 +4,33 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.navigator.Navigator
@@ -27,7 +39,6 @@ import com.beepbeep.designSystem.ui.theme.Theme
 import presentation.base.BaseScreen
 import presentation.composables.OrderCard
 import presentation.composables.OrderTextButton
-import presentation.composables.header
 import presentation.login.LoginScreen
 import resources.Resources
 
@@ -46,11 +57,15 @@ class OrderScreen :
         }
     }
 
-    @OptIn(ExperimentalFoundationApi::class)
+    @OptIn(ExperimentalFoundationApi::class, ExperimentalLayoutApi::class)
     @Composable
     override fun onRender(state: OrderScreenUiState, listener: OrderScreenInteractionListener) {
 
-        Column(modifier = Modifier.fillMaxSize().background(Theme.colors.background)) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Theme.colors.background)
+        ) {
 
             BpAppBar(
                 modifier = Modifier
@@ -81,6 +96,189 @@ class OrderScreen :
                 }
             }
 
+            FlowRow(
+                modifier = Modifier,
+                maxItemsInEachRow = 2,
+            ) {
+
+/*                LazyColumn(
+                    modifier = Modifier.widthIn(max = 380.dp),
+                    contentPadding = PaddingValues(
+                        bottom = Theme.dimens.space16,
+                        start = Theme.dimens.space16,
+                        end = Theme.dimens.space16
+                    )
+                ) {
+                    stickyHeader(key = state.inCookingOrders) {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Theme.colors.background)
+                                .padding(vertical = Theme.dimens.space16),
+                            text = Resources.strings.inCookingOrders,
+                            style = Theme.typography.titleLarge,
+                            color = Theme.colors.contentPrimary
+                        )
+                    }
+                    items(state.inCookingOrders) { order ->
+                        OrderCard(order = order) {
+                            OrderTextButton(
+                                text = Resources.strings.finish,
+                                onClick = {},
+                            )
+                        }
+                    }
+                }
+                LazyColumn(
+                    modifier = Modifier.widthIn(max = 380.dp),
+                    contentPadding = PaddingValues(
+                        bottom = Theme.dimens.space16,
+                        start = Theme.dimens.space16,
+                        end = Theme.dimens.space16
+                    )
+                ) {
+                    stickyHeader(key = state.pendingOrders) {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Theme.colors.background)
+                                .padding(vertical = Theme.dimens.space16),
+                            text = Resources.strings.requestedOrders,
+                            style = Theme.typography.titleLarge,
+                            color = Theme.colors.contentPrimary
+                        )
+                    }
+
+                    items(state.pendingOrders) { order ->
+                        OrderCard(order = order) {
+                            OrderTextButton(
+                                text = Resources.strings.cancel,
+                                onClick = {},
+                                textColor = Theme.colors.contentTertiary,
+                                border = BorderStroke(0.dp, color = Theme.colors.surface)
+                            )
+                            OrderTextButton(
+                                text = Resources.strings.approve,
+                                onClick = {},
+                            )
+                        }
+                    }
+                }*/
+
+                /*                LazyColumn(
+                                    modifier = Modifier.widthIn(max = 380.dp),
+                                    contentPadding = PaddingValues(
+                                        bottom = Theme.dimens.space16,
+                                        start = Theme.dimens.space16,
+                                        end = Theme.dimens.space16
+                                    )
+                                ) {
+                                    stickyHeader(key = state.inCookingOrders) {
+                                        Text(
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .background(Theme.colors.background)
+                                                .padding(vertical = Theme.dimens.space16),
+                                            text = Resources.strings.inCookingOrders,
+                                            style = Theme.typography.titleLarge,
+                                            color = Theme.colors.contentPrimary
+                                        )
+                                    }
+                                    items(state.inCookingOrders) { order ->
+                                        OrderCard(order = order) {
+                                            OrderTextButton(
+                                                text = Resources.strings.finish,
+                                                onClick = {},
+                                            )
+                                        }
+                                    }
+
+                                    stickyHeader(key = state.pendingOrders) {
+                                        Text(
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .background(Theme.colors.background)
+                                                .padding(vertical = Theme.dimens.space16),
+                                            text = Resources.strings.requestedOrders,
+                                            style = Theme.typography.titleLarge,
+                                            color = Theme.colors.contentPrimary
+                                        )
+                                    }
+
+                                    items(state.pendingOrders) { order ->
+                                        OrderCard(order = order) {
+                                            OrderTextButton(
+                                                text = Resources.strings.cancel,
+                                                onClick = {},
+                                                textColor = Theme.colors.contentTertiary,
+                                                border = BorderStroke(0.dp, color = Theme.colors.surface)
+                                            )
+                                            OrderTextButton(
+                                                text = Resources.strings.approve,
+                                                onClick = {},
+                                            )
+                                        }
+                                    }
+                                }*/
+
+//                LazyColumn(
+//                    modifier = Modifier.widthIn(max = 380.dp),
+//                    contentPadding = PaddingValues(
+//                        bottom = Theme.dimens.space16,
+//                        start = Theme.dimens.space16,
+//                        end = Theme.dimens.space16
+//                    )
+//                ) {
+//                    stickyHeader(key = state.inCookingOrders) {
+//                        Text(
+//                            modifier = Modifier
+//                                .fillMaxWidth()
+//                                .background(Theme.colors.background)
+//                                .padding(vertical = Theme.dimens.space16),
+//                            text = Resources.strings.inCookingOrders,
+//                            style = Theme.typography.titleLarge,
+//                            color = Theme.colors.contentPrimary
+//                        )
+//                    }
+//                    items(state.inCookingOrders) { order ->
+//                        OrderCard(order = order) {
+//                            OrderTextButton(
+//                                text = Resources.strings.finish,
+//                                onClick = {},
+//                            )
+//                        }
+//                    }
+//
+//                    stickyHeader(key = state.pendingOrders) {
+//                        Text(
+//                            modifier = Modifier
+//                                .fillMaxWidth()
+//                                .background(Theme.colors.background)
+//                                .padding(vertical = Theme.dimens.space16),
+//                            text = Resources.strings.requestedOrders,
+//                            style = Theme.typography.titleLarge,
+//                            color = Theme.colors.contentPrimary
+//                        )
+//                    }
+//
+//                    items(state.pendingOrders) { order ->
+//                        OrderCard(order = order) {
+//                            OrderTextButton(
+//                                text = Resources.strings.cancel,
+//                                onClick = {},
+//                                textColor = Theme.colors.contentTertiary,
+//                                border = BorderStroke(0.dp, color = Theme.colors.surface)
+//                            )
+//                            OrderTextButton(
+//                                text = Resources.strings.approve,
+//                                onClick = {},
+//                            )
+//                        }
+//                    }
+//                }
+
+            }
+
             LazyVerticalStaggeredGrid(
                 modifier = Modifier.fillMaxSize(),
                 contentPadding = PaddingValues(Theme.dimens.space16),
@@ -89,7 +287,7 @@ class OrderScreen :
                 horizontalArrangement = Arrangement.spacedBy(Theme.dimens.space8)
             ) {
 
-                header {
+                item {
                     Text(
                         modifier = Modifier.padding(vertical = Theme.dimens.space8),
                         text = Resources.strings.inCookingOrders,
@@ -107,7 +305,7 @@ class OrderScreen :
                     }
                 }
 
-                header {
+                item {
                     Text(
                         modifier = Modifier.padding(
                             top = Theme.dimens.space24,
@@ -136,7 +334,9 @@ class OrderScreen :
                 }
 
             }
-
         }
+
+
+
     }
 }
