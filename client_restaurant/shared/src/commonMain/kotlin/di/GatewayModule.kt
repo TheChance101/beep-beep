cac38bd4edabb4ba33d4e6055fe7912f705fc1f8
Youssef <yr925344git@gmail.com>
2023-08-23 18:02:00 +0300

    add restaurant gateway to GatewayModule

--- client_restaurant/shared/src/commonMain/kotlin/di/GatewayModule.kt
+++ client_restaurant/shared/src/commonMain/kotlin/di/GatewayModule.kt
@@ -1,9 +1,12 @@
 package di
 
-import data.remote.gateway.FakeRemoteGateWay
+import data.gateway.FakeRemoteGateWay
+import data.gateway.RestaurantGateWay
 import domain.gateway.IRemoteGateWay
+import domain.gateway.IRestaurantGateWay
 import org.koin.dsl.module
 
 val GatewayModule = module {
     single<IRemoteGateWay> { FakeRemoteGateWay() }
+    single<IRestaurantGateWay> { RestaurantGateWay(get()) }
 }
