9a3f1a9294655d4d56a44b2fe3da1024be6ec731
haidy <haidyabugom3a@gmail.com>
2023-08-23 18:40:09 +0300

    add restaurant info use cases

--- client_restaurant/shared/src/commonMain/kotlin/di/UseCaseModule.kt
+++ client_restaurant/shared/src/commonMain/kotlin/di/UseCaseModule.kt
@@ -1,9 +1,12 @@
 package di
 
 import domain.usecase.GetNewOrdersUseCase
+import domain.usecase.ManageRestaurantInfoUseCase
 import domain.usecase.IGetNewOrdersUseCase
+import domain.usecase.IManageRestaurantInfoUseCase
 import org.koin.dsl.module
 
 val UseCaseModule = module {
     single<IGetNewOrdersUseCase> { GetNewOrdersUseCase(get()) }
+    single<IManageRestaurantInfoUseCase> { ManageRestaurantInfoUseCase(get()) }
 }
