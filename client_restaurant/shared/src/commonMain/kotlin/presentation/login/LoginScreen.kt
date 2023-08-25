e397da679a641a814d19155dd84aabc28f179ec1
Youssef <yr925344git@gmail.com>
2023-08-25 09:59:46 +0300

    add bottom sheet state logic

--- client_restaurant/shared/src/commonMain/kotlin/presentation/login/LoginScreen.kt
+++ client_restaurant/shared/src/commonMain/kotlin/presentation/login/LoginScreen.kt
@@ -6,31 +6,37 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.navigator.Navigator
 import com.beepbeep.designSystem.ui.composable.BpButton
-import com.beepbeep.designSystem.ui.composable.BpExpandableTextField
 import com.beepbeep.designSystem.ui.composable.BpTextField
 import com.beepbeep.designSystem.ui.theme.Theme
 import presentation.base.BaseScreen
-import presentation.composables.CustomBottomSheet
-import presentation.composables.ModalBottomSheetState
-import presentation.main.MainScreen
-import resources.Resources
+import presentation.composable.CustomBottomSheet
+import presentation.composable.ModalBottomSheetState
+import presentation.login.composable.PermissionBottomSheetContent
+import presentation.login.composable.WrongPermissionBottomSheet
+import presentation.restaurantSelection.RestaurantSelectionScreen
 
 class LoginScreen :
     BaseScreen<LoginScreenModel, LoginScreenUIState, LoginScreenUIEffect, LoginScreenInteractionListener>() {
 
+
     @Composable
     override fun Content() {
-        initScreen(getScreenModel())
+        val screenModel = rememberScreenModel { LoginScreenModel() }
+        initScreen(screenModel)
     }
 
     override fun onEffect(
@@ -38,27 +44,49 @@ class LoginScreen :
         navigator: Navigator,
     ) {
         when (effect) {
-            is LoginScreenUIEffect.Login -> navigator.push(MainScreen())
-            is LoginScreenUIEffect.Permission -> navigator.push(MainScreen())
+            is LoginScreenUIEffect.Login -> navigator.push(RestaurantSelectionScreen(ownerId = ""))
         }
     }
 
     @Composable
     override fun onRender(state: LoginScreenUIState, listener: LoginScreenInteractionListener) {
+
+        var showPermissionSheet by remember { mutableStateOf(false) }
         val sheetState = remember { ModalBottomSheetState() }
+
         Column(modifier = Modifier.fillMaxSize()) {
+
+            LaunchedEffect(sheetState.isVisible) {
+                if (!sheetState.isVisible) {
+                    showPermissionSheet = false
+                }
+            }
 
             CustomBottomSheet(
                 sheetContent = {
-                    PermissionBottomSheetContent(
-                        state = state,
-                        onCancelClick = { sheetState.dismiss() },
-                        onSubmit = {
-                            listener.onClickSubmit()
-                            sheetState.dismiss()
-                        },
-                        listener = listener,
-                    )
+                    if (showPermissionSheet) {
+                        PermissionBottomSheetContent(
+                            onCancelClick = {
+                                showPermissionSheet = false
+                                sheetState.dismiss()
+                            },
+                            onSubmit = {
+                                showPermissionSheet = false
+                                listener.onClickSubmit()
+                                sheetState.dismiss()
+                            },
+                            listener = listener,
+                            state = state
+                        )
+                    } else {
+                        WrongPermissionBottomSheet(
+                            onRequestPermissionClick = {
+                                showPermissionSheet = true
+                                listener.onClickSubmit()
+                            },
+                            onCancelClick = { sheetState.dismiss() },
+                        )
+                    }
                 },
                 sheetBackgroundColor = Theme.colors.background,
                 sheetState = sheetState,
@@ -101,62 +129,18 @@ class LoginScreen :
                 errorMessage = state.error,
             )
             BpButton(
-                onClick = { sheetState.show() },
+                onClick = {
+                    if (state.hasPermission) {
+                        sheetState.show()
+                    } else {
+                        listener.onClickLogin()
+                    }
+                },
                 title = "Login",
                 modifier = Modifier.fillMaxWidth(),
             )
         }
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    private fun PermissionBottomSheetContent(
-        state: LoginScreenUIState,
-        listener: LoginScreenInteractionListener,
-        onSubmit: () -> Unit,
-        onCancelClick: () -> Unit,
-        modifier: Modifier = Modifier,
-    ) {
-        Column(
-            modifier = modifier.wrapContentHeight()
-                .padding(top = Theme.dimens.space16).padding(horizontal = Theme.dimens.space16)
-        ) {
-            Text(
-                "Ask for permission",
-                style = Theme.typography.headlineLarge,
-            )
-            BpTextField(
-                text = state.restaurantName,
-                onValueChange = listener::onNameChanged,
-                label = Resources.strings.name,
-                keyboardType = KeyboardType.Text,
-                modifier = Modifier.fillMaxWidth(),
-            )
-            BpTextField(
-                text = state.ownerEmail,
-                onValueChange = listener::onNameChanged,
-                label = Resources.strings.name,
-                keyboardType = KeyboardType.Text,
-                modifier = Modifier.fillMaxWidth(),
-            )
-            BpExpandableTextField(
-                text = state.description,
-                onValueChange = listener::onDescriptionChanged,
-                label = Resources.strings.description,
-                keyboardType = KeyboardType.Text,
-                modifier = Modifier.fillMaxWidth(),
-            )
-            BpButton(
-                onClick = onSubmit,
-                title = "Submit",
-                modifier = Modifier.fillMaxWidth(),
-            )
-            BpButton(
-                onClick = onCancelClick,
-                title = "Cancel",
-                modifier = Modifier.fillMaxWidth(),
-            )
-        }
-    }
 
 }
