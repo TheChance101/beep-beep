f66665a450f389b26096389dec1c13026ee40b10
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-25 16:05:22 +0300

    wrap all app with image loader caching

--- client_restaurant/shared/src/commonMain/kotlin/resources/BpResources.kt
+++ client_restaurant/shared/src/commonMain/kotlin/resources/BpResources.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
 import com.beepbeep.designSystem.ui.theme.BpTheme
-
+import com.seiko.imageloader.LocalImageLoader
+import util.generateImageLoader
+import util.getPlatformContext
 
 private val localPainterResources = staticCompositionLocalOf { BpPainterLightResources }
 private val localStringResources = staticCompositionLocalOf { StringResources() }
@@ -17,17 +20,21 @@ fun BpRestaurantTheme(
     content: @Composable () -> Unit,
 ) {
     val painterResources = if (useDarkTheme) BpPainterDarkResources else BpPainterLightResources
+    val context = getPlatformContext()
 
     CompositionLocalProvider(
         localPainterResources provides painterResources,
         localStringResources provides StringResources()
     ) {
-        BpTheme {
-            content()
+        CompositionLocalProvider(
+            LocalImageLoader provides remember { generateImageLoader(context) },
+        ) {
+            BpTheme {
+                content()
+            }
         }
     }
 }
-
 
 object Resources {
     val images: PainterResources
