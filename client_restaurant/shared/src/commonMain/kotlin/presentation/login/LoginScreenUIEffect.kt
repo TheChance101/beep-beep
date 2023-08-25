0f04089d696916459e65eeea32ae9937e65732d1
Youssef <yr925344git@gmail.com>
2023-08-25 09:51:03 +0300

    remove un used permission object

--- client_restaurant/shared/src/commonMain/kotlin/presentation/login/LoginScreenUIEffect.kt
+++ client_restaurant/shared/src/commonMain/kotlin/presentation/login/LoginScreenUIEffect.kt
@@ -2,6 +2,4 @@ package presentation.login
 
 sealed class LoginScreenUIEffect {
     data class Login(val ownerId: String) : LoginScreenUIEffect()
-    object Login : LoginScreenUIEffect()
-    object Permission: LoginScreenUIEffect()
 }
