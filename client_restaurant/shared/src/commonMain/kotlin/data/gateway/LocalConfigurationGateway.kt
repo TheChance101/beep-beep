64a31543ee4dce987ff370b5223ddbc9c2fda665
Aziza <aziza.helmy35@gmail.com>
2023-08-26 10:12:12 +0300

    rename LocalConfigurationGateway

--- client_restaurant/shared/src/commonMain/kotlin/data/gateway/LocalConfigurationGateway.kt
+++ client_restaurant/shared/src/commonMain/kotlin/data/gateway/LocalConfigurationGateway.kt
@@ -1,50 +1,52 @@
 package data.gateway
 
-import data.local.model.FlagDto
-import data.local.model.TokenDto
-import domain.gateway.ILocalGateWay
+import data.local.model.UserConfigurationCollection
+import domain.gateway.ILocalConfigurationGateway
 import io.realm.kotlin.Realm
 import io.realm.kotlin.ext.query
 
-class LocalGateway(private val realm: Realm):ILocalGateWay {
+class LocalConfigurationGateway(private val realm: Realm) : ILocalConfigurationGateway {
 
-      override suspend fun saveAccessToken(token: String) {
+    override suspend fun saveAccessToken(token: String) {
         realm.write {
-            copyToRealm(TokenDto().apply {
+            copyToRealm(UserConfigurationCollection().apply {
                 this.accessToken = token
             })
         }
     }
 
     override suspend fun getAccessToken(): String {
-        return realm.query<TokenDto>().first().find()?.accessToken
+        return realm.query<UserConfigurationCollection>().first().find()?.accessToken
             ?: throw Exception("Access Token not found")
     }
 
     override suspend fun saveRefreshToken(token: String) {
         realm.write {
-            copyToRealm(TokenDto().apply {
+            copyToRealm(UserConfigurationCollection().apply {
                 this.refreshToken = token
             })
         }
     }
     override suspend fun getRefreshToken(): String {
-        return realm.query<TokenDto>().first().find()?.refreshToken
+        return realm.query<UserConfigurationCollection>().first().find()?.refreshToken
             ?: throw Exception("Refresh Token not found")
     }
 
     override suspend fun saveKeepMeLoggedInFlag(isChecked: Boolean) {
         realm.write {
-            copyToRealm(FlagDto().apply {
+            copyToRealm(UserConfigurationCollection().apply {
                 this.isKeepMeLoggedInMeChecked = isChecked
             })
         }
     }
 
     override suspend fun getKeepMeLoggedInFlag(): Boolean {
-        return realm.query<FlagDto>().first().find()?.isKeepMeLoggedInMeChecked
+        return realm.query<UserConfigurationCollection>().first().find()?.isKeepMeLoggedInMeChecked
             ?: throw Exception("Nothing ..")
     }
 
+    override suspend fun clearTokens() {
+        realm.write { delete(query<UserConfigurationCollection>()) }
+    }
 
 }
--- client_restaurant/shared/src/commonMain/kotlin/di/GatewayModule.kt
+++ client_restaurant/shared/src/commonMain/kotlin/di/GatewayModule.kt
@@ -1,9 +1,8 @@
 package di
 
 import data.gateway.FakeRemoteGateway
-import data.gateway.LocalGateway
-import data.gateway.RemoteGateway
-import domain.gateway.ILocalGateWay
+import data.gateway.LocalConfigurationGateway
+import domain.gateway.ILocalConfigurationGateway
 import domain.gateway.IRemoteGateWay
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
@@ -12,5 +11,5 @@ import org.koin.dsl.module
 val GatewayModule = module {
     singleOf(::FakeRemoteGateway){ bind<IRemoteGateWay>()}
     //singleOf(::RemoteGateway) { bind<IRemoteGateWay>() }
-    singleOf(::LocalGateway) { bind<ILocalGateWay>() }
+    singleOf(::LocalConfigurationGateway) { bind<ILocalConfigurationGateway>() }
 }
