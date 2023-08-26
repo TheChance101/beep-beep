72b308ba9586f3b3fa200efe4529a19ebbe9363d
Youssef <yr925344git@gmail.com>
2023-08-26 18:56:40 +0300

    refactor login user use case

--- client_restaurant/shared/src/commonMain/kotlin/di/GatewayModule.kt
+++ client_restaurant/shared/src/commonMain/kotlin/di/GatewayModule.kt
@@ -1,15 +1,18 @@
 package di
 
-import data.gateway.FakeRemoteGateway
-import data.gateway.LocalConfigurationGateway
-import domain.gateway.ILocalConfigurationGateway
+import data.fake.FakeRemoteGateway
+import data.local.LocalConfigurationGateway
+import data.remote.gateway.RemoteIdentityGateway
 import domain.gateway.IFakeRemoteGateWay
+import domain.gateway.ILocalConfigurationGateway
+import domain.gateway.IRemoteIdentityGateway
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val GatewayModule = module {
     //todo add remaining gateway
-    singleOf(::FakeRemoteGateway){ bind<IFakeRemoteGateWay>()}
+    singleOf(::FakeRemoteGateway) { bind<IFakeRemoteGateWay>() }
+    singleOf(::RemoteIdentityGateway) { bind<IRemoteIdentityGateway>() }
     singleOf(::LocalConfigurationGateway) { bind<ILocalConfigurationGateway>() }
 }
