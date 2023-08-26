c0b571e6c22b2fa48f3da60ddf50b066f26b9b13
Aziza <aziza.helmy35@gmail.com>
2023-08-26 11:09:42 +0300

    remove unnecessary code

--- client_restaurant/shared/src/commonMain/kotlin/data/local/model/UserConfigurationCollection.kt
+++ client_restaurant/shared/src/commonMain/kotlin/data/local/model/UserConfigurationCollection.kt
@@ -1,12 +1,8 @@
 package data.local.model
 
 import io.realm.kotlin.types.RealmObject
-import io.realm.kotlin.types.annotations.PrimaryKey
-import org.mongodb.kbson.ObjectId
 
 class UserConfigurationCollection : RealmObject {
-    @PrimaryKey
-    private var _id: ObjectId = ObjectId()
     var accessToken: String = ""
     var refreshToken: String = ""
     var isKeepMeLoggedInMeChecked: Boolean = false
