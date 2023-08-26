e7d7a04e76a9a7a8c7f364717437f4f19212869e
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-08-26 16:29:12 +0300

    make login screen is destination

--- client_restaurant/shared/src/commonMain/kotlin/App.kt
+++ client_restaurant/shared/src/commonMain/kotlin/App.kt
@@ -4,14 +4,13 @@ import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
 import presentation.login.LoginScreen
-import presentation.order.OrderScreen
 import resources.BpRestaurantTheme
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun App() {
     BpRestaurantTheme {
-        Navigator(OrderScreen()) {
+        Navigator(LoginScreen()) {
             SlideTransition(it)
         }
     }
