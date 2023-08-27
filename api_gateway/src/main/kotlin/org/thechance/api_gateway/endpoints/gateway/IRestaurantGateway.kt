0b46830c9cbe2aa3718ab6e8acbe574fe41ae473
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-08-28 01:03:03 +0300

    add post restaurant/permission

---
+++ api_gateway/src/main/kotlin/org/thechance/api_gateway/endpoints/gateway/IRestaurantGateway.kt
@@ -0,0 +1,16 @@
+package org.thechance.api_gateway.endpoints.gateway
+
+import org.thechance.api_gateway.endpoints.model.RestaurantRequestPermission
+import java.util.*
+
+interface IRestaurantGateway {
+    suspend fun createRequestPermission(
+        restaurantName: String,
+        ownerEmail: String,
+        cause: String,
+        locale: Locale
+    ): RestaurantRequestPermission
+
+    suspend fun getAllRequestPermission(): List<RestaurantRequestPermission>
+}
+
