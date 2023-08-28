5adfd3ca5bd5b1ed9d84359a9044718a0e3a97ad
haidy <haidyabugom3a@gmail.com>
2023-08-28 16:53:05 +0300

    fix cuisine resource duplication

--- api_gateway/src/main/kotlin/org/thechance/api_gateway/data/model/restaurant/CuisineResource.kt
+++
@@ -1,6 +0,0 @@
-package org.thechance.api_gateway.data.model.restaurant
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class CuisineResource(val id: String? = null, val name: String)
