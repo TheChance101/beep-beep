7d1e8ffde4acbc885e2d619f1060302d7796ad3e
Aziza <aziza.helmy35@gmail.com>
2023-08-28 17:17:13 +0300

    prepare get all cuisines method

--- client_restaurant/shared/src/commonMain/kotlin/data/gateway/remote/RemoteCuisineGateway.kt
+++ client_restaurant/shared/src/commonMain/kotlin/data/gateway/remote/RemoteCuisineGateway.kt
@@ -1,22 +1,19 @@
 package data.gateway.remote
 
+import data.remote.mapper.toEntity
+import data.remote.model.CuisineDto
 import domain.entity.Cuisine
 import domain.gateway.remote.IRemoteCuisineGateway
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
-import io.ktor.client.request.parameter
 
 
 class RemoteCuisineGateway(client: HttpClient) : IRemoteCuisineGateway,
     BaseRemoteGateway(client = client) {
 
     override suspend fun getCuisinesByRestaurantId(restaurantId: String): List<Cuisine> {
-        return tryToExecute<List<Cuisine>> {
-            get("/cuisines") {
-                parameter("page", 1)
-//                parameter("limit", limit)
-//                parameter("searchTerm", searchTerm)
-            }
-        }
+        return tryToExecute<List<CuisineDto>> {
+            get("/cuisines/$restaurantId")
+        }.map { it.toEntity() }
     }
 }
