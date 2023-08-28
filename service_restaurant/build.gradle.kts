06ae44d486841f80bd06b73451a4e607f5b30664
Nada Feteiha <nada.feteiha@gmail.com>
2023-08-28 08:20:28 -0700

    uncomment time dependencey

--- service_restaurant/build.gradle.kts
+++ service_restaurant/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-//    implementation("org.jetbrains.kotlinx:kotlinx-datetime:$kotlinx_datetime")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:$kotlinx_datetime")
     implementation("io.ktor:ktor-server-websockets-jvm:$ktor_version")
 
 //    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
