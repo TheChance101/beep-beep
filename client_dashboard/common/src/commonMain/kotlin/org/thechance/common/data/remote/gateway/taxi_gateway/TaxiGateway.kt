cb2d3c32b15c055f45ae51763570a4b85ba87f0d
Saif Alhaider <saifharith90@gmail.com>
2023-08-29 17:14:36 +0300

    refactor: optimize imports

--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/data/remote/gateway/taxi_gateway/TaxiGateway.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/data/remote/gateway/taxi_gateway/TaxiGateway.kt
@@ -1,7 +1,7 @@
 package org.thechance.common.data.remote.gateway.taxi_gateway
 
-import org.thechance.common.domain.entity.AddTaxi
 import org.thechance.common.domain.entity.DataWrapper
+import org.thechance.common.domain.entity.NewTaxiInfo
 import org.thechance.common.domain.entity.Taxi
 
 class TaxiGateway:ITaxiGateway {
@@ -9,7 +9,7 @@ class TaxiGateway:ITaxiGateway {
         TODO("Not yet implemented")
     }
 
-    override suspend fun createTaxi(taxi: AddTaxi): Taxi {
+    override suspend fun createTaxi(taxi: NewTaxiInfo): Taxi {
         TODO("Not yet implemented")
     }
 
