cf819c04ac889a232e6d88fda4f3614ee690f36b
ay0o5h <aayosh553@gmail.com>
2023-08-29 11:16:08 +0300

    covert color to long

--- service_taxi/src/main/kotlin/org/thechance/service_taxi/domain/entity/Taxi.kt
+++ service_taxi/src/main/kotlin/org/thechance/service_taxi/domain/entity/Taxi.kt
@@ -1,5 +1,7 @@
 package org.thechance.service_taxi.domain.entity
 
+import java.util.HexFormat
+
 data class Taxi(
     val id: String,
     val plateNumber: String,
@@ -11,21 +13,23 @@ data class Taxi(
 )
 
     enum class Color(val colorNumber:   Long) {
-    BLACK(0),
-    WHITE(1),
-    RED(0xFFFF0000L),
-    BLUE(3),
-    GREEN(4),
-    YELLOW(5),
-    ORANGE(6),
-    PURPLE(7),
-    PINK(8),
-    BROWN(9),
-    GRAY(10),
-    SILVER(11),
-    GOLD(12),
-    BRONZE(13),
-    OTHER(14);
+        BLACK(4278190080L),
+        WHITE(4294967295L),
+        RED(4294901760L),
+        BLUE(4278190335L),
+        GREEN(4278255360L),
+
+        ////////////////////////////
+        YELLOW(4294639360L),
+        ORANGE(4294944768L),
+        PURPLE(4288545023L),
+        PINK(4293591295L),
+        BROWN(4283578920L),
+        GRAY(4290626507L),
+        SILVER(4290822336L),
+        GOLD(4294956800L),
+        BRONZE(4291657522L),
+        OTHER(0L);
 
     companion object {
         fun getColorByColorNumber(colorNumber: Long): Color {
