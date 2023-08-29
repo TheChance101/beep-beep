689df1abfff72c45b6bac20521e9e42f7bb17a88
Aziza <aziza.helmy35@gmail.com>
2023-08-30 01:07:15 +0300

    add module of gateways

--- client_restaurant/shared/src/commonMain/kotlin/di/GatewayModule.kt
+++ client_restaurant/shared/src/commonMain/kotlin/di/GatewayModule.kt
@@ -2,8 +2,12 @@ package di
 
 import data.gateway.FakeRemoteGateWay
 import data.gateway.local.LocalConfigurationGateway
+import data.gateway.remote.RemoteCuisineGateway
+import data.gateway.remote.RemoteMealGateway
 import domain.gateway.local.ILocalConfigurationGateway
 import domain.gateway.IFakeRemoteGateway
+import domain.gateway.remote.IRemoteCuisineGateway
+import domain.gateway.remote.IRemoteMealGateway
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -11,4 +15,6 @@ import org.koin.dsl.module
 val GatewayModule = module {
     singleOf(::FakeRemoteGateWay) { bind<IFakeRemoteGateway>() }
     singleOf(::LocalConfigurationGateway) { bind<ILocalConfigurationGateway>() }
+    singleOf(::RemoteCuisineGateway) { bind<IRemoteCuisineGateway>() }
+    singleOf(::RemoteMealGateway) { bind<IRemoteMealGateway>() }
 }
