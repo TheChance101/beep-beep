fcfab22a6c108389ef91a6e415389be460058a15
Aziza <aziza.helmy35@gmail.com>
2023-08-30 19:00:56 +0300

    add refreshAccessToken method

--- client_restaurant/shared/src/commonMain/kotlin/domain/gateway/remote/IRemoteIdentityGateway.kt
+++ client_restaurant/shared/src/commonMain/kotlin/domain/gateway/remote/IRemoteIdentityGateway.kt
@@ -5,6 +5,6 @@ interface IRemoteIdentityGateway {
 
     suspend fun loginUser(userName: String, password: String): Pair<String, String>
 
-
+    suspend fun refreshAccessToken(refreshToken: String): Pair<String, String>
 
 }
