dbb70f39c81f04b533c9f7a08a4df716684c044f
Aziza <aziza.helmy35@gmail.com>
2023-08-30 22:52:58 +0300

    add serialization annotations in classes

--- client_restaurant/shared/src/commonMain/kotlin/data/remote/model/BaseResponse.kt
+++ client_restaurant/shared/src/commonMain/kotlin/data/remote/model/BaseResponse.kt
@@ -1,17 +1,24 @@
 package data.remote.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class BaseResponse<T>(
+    @SerialName("value")
     val value: T?,
+    @SerialName("isSuccess")
     val isSuccess: Boolean = true,
+    @SerialName("status")
     val status: ResponseStatus
 )
 
 @Serializable
 data class ResponseStatus(
+    @SerialName("id")
     val errorMessages: Map<String, String>? = null,
+    @SerialName("id")
     val successMessage: String? = null,
+    @SerialName("id")
     val code: Int?
 )
