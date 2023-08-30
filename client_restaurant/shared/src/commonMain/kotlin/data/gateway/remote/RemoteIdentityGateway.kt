095fd23da476e4c671a82d8ead84fb127ebfa445
Aziza <aziza.helmy35@gmail.com>
2023-08-30 19:01:23 +0300

    add refreshAccessToken method

--- client_restaurant/shared/src/commonMain/kotlin/data/gateway/remote/RemoteIdentityGateway.kt
+++ client_restaurant/shared/src/commonMain/kotlin/data/gateway/remote/RemoteIdentityGateway.kt
@@ -5,11 +5,10 @@ import data.remote.model.UserTokensDto
 import domain.gateway.remote.IRemoteIdentityGateway
 import io.ktor.client.HttpClient
 import io.ktor.client.request.forms.submitForm
-import io.ktor.client.request.header
 import io.ktor.client.request.url
 import io.ktor.http.Parameters
 
-
+//todo rename classes
 class RemoteIdentityGateway(client: HttpClient) : IRemoteIdentityGateway,
     BaseRemoteGateway(client = client) {
 
@@ -22,6 +21,21 @@ class RemoteIdentityGateway(client: HttpClient) : IRemoteIdentityGateway,
                 }
             ) {
                 url("login")
+            }
+        }.value ?: throw Exception()
+
+        return Pair(result.accessToken, result.refreshToken)
+    }
+
+
+override suspend fun refreshAccessToken(refreshToken: String): Pair<String, String> {
+     val result = tryToExecute<BaseResponse<UserTokensDto>>() {
+            submitForm(
+                formParameters = Parameters.build {
+                    append("refreshToken", refreshToken)
+                }
+            ) {
+                url("refresh-access-token")
             }
         }.value ?: throw Exception()
 
