8b42b3ef3d5617385c17604ca8651f4f21dc769c
haidy <haidyabugom3a@gmail.com>
2023-08-31 13:47:36 +0300

    fix update restaurant permissions

--- api_gateway/src/main/kotlin/org/thechance/api_gateway/util/Role.kt
+++ api_gateway/src/main/kotlin/org/thechance/api_gateway/util/Role.kt
@@ -4,7 +4,6 @@ object Role {
     const val END_USER = 1
     const val DASHBOARD_ADMIN = 2
     const val RESTAURANT_OWNER = 4
-    const val RESTAURANT_ADMIN = 3
     const val TAXI_DRIVER = 8
     const val SUPPORT = 16
     const val DELIVERY = 32
