c938335e27381493ca106db517db97e6a3198f20
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-09-01 02:36:07 +0300

    add overview screen model

--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/overview/OverviewScreenModel.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/overview/OverviewScreenModel.kt
@@ -2,9 +2,51 @@ package org.thechance.common.presentation.overview
 
 import org.thechance.common.presentation.base.BaseScreenModel
 
-
 class OverviewScreenModel : BaseScreenModel<OverviewUiState, OverviewUiEffect>(OverviewUiState()),
     OverviewInteractionListener {
 
+    override fun onMenuItemDropDownClicked() {
+        updateState { state ->
+            state.copy(
+                dropdownMenuState = state.dropdownMenuState.copy(
+                    isExpanded = !state.dropdownMenuState.isExpanded
+                )
+            )
+        }
+    }
+
+    override fun onMenuItemClicked(index: Int) {
+        updateState { state ->
+            state.copy(
+                dropdownMenuState = state.dropdownMenuState.copy(
+                    isExpanded = false,
+                    selectedIndex = index
+                )
+            )
+
+        }
+    }
+
+    override fun onDismissDropDownMenu() {
+        updateState { state ->
+            state.copy(
+                dropdownMenuState = state.dropdownMenuState.copy(
+                    isExpanded = false
+                )
+            )
+        }
+    }
+
+    override fun onViewMoreUsersClicked() {
+        sendNewEffect(OverviewUiEffect.ViewMoreUsers)
+    }
+
+    override fun onViewMoreRestaurantClicked() {
+        sendNewEffect(OverviewUiEffect.ViewMoreRestaurant)
+    }
+
+    override fun onViewMoreTaxiClicked() {
+        sendNewEffect(OverviewUiEffect.ViewMoreTaxis)
+    }
 
 }
