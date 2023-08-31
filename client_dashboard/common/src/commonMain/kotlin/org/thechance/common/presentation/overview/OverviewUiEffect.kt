1bed19f8470d31728e77ae5765e49f2738a95f7f
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-09-01 02:08:26 +0300

    add overview screen ui effects / state

--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/overview/OverviewUiEffect.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/overview/OverviewUiEffect.kt
@@ -2,4 +2,10 @@ package org.thechance.common.presentation.overview
 
 sealed interface OverviewUiEffect {
 
+    object ViewMoreUsers : OverviewUiEffect
+
+    object ViewMoreTaxis : OverviewUiEffect
+
+    object ViewMoreRestaurant : OverviewUiEffect
+
 }
--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/overview/OverviewUiState.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/overview/OverviewUiState.kt
@@ -3,4 +3,27 @@ package org.thechance.common.presentation.overview
 
 data class OverviewUiState(
     val isLoading: Boolean = false,
+    val users: List<User> = userlist,
+    val dropdownMenuState: DropdownMenuState = DropdownMenuState(),
 )
+
+data class User(
+    val name: String,
+    val image: String,
+    val role: String,
+)
+
+data class DropdownMenuState(
+    val isExpanded: Boolean = false,
+    val items: List<String> = listOf("Daily", "Weekly", "Monthly"),
+    val selectedIndex: Int = 0,
+)
+
+val userlist = listOf(
+    User("Aa", "dummy_img.png", "Admin"),
+    User("Bb", "dummy_img.png", "Admin"),
+    User("Cc", "dummy_img.png", "Admin"),
+    User("Dd", "dummy_img.png", "Admin"),
+)
+
+
