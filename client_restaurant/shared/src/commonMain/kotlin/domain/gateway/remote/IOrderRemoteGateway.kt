803ff71540947cd0fb92ca473b24a10acecdb963
Aziza <aziza.helmy35@gmail.com>
2023-09-01 19:57:22 +0300

    add remaining methods in OrderRemoteGateway

--- client_restaurant/shared/src/commonMain/kotlin/domain/gateway/remote/IOrderRemoteGateway.kt
+++ client_restaurant/shared/src/commonMain/kotlin/domain/gateway/remote/IOrderRemoteGateway.kt
@@ -1,13 +1,22 @@
 package domain.gateway.remote
 
 import domain.entity.Order
+import kotlinx.coroutines.flow.Flow
 
 
 interface IOrderRemoteGateway {
 
-    suspend fun getCurrentOrders(restaurantId: String): List<Order>?
-    suspend fun getOrdersHistory(restaurantId: String): List<Order>
-    suspend fun updateOrderState(orderId: String, orderState: Int): Order?
-    suspend fun getOrderById(orderId: String): Order?
+    suspend fun getCurrentOrders(restaurantId: String): Flow<Order>?//todo nullable just for now
+    suspend fun getActiveOrders(restaurantId: String): List<Order>
+    suspend fun updateOrderState(orderId: String, orderState: Int): Order
+    suspend fun getOrdersHistory(restaurantId: String, page: Int, limit: Int): List<Order>
 
+    suspend fun getOrdersRevenueByDaysBefore(
+        restaurantId: String,
+        daysBack: Int
+    ): List<Map<Int, Double>>
+
+    suspend fun getOrdersCountByDaysBefore(
+        restaurantId: String, daysBack: Int
+    ): List<Map<Int, Int>>
 }
