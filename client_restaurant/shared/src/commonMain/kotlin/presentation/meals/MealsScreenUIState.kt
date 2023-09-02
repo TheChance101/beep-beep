f3c8bdf8cf3d42bdc666f8c205f12dba9a0a1c15
Aziza <aziza.helmy35@gmail.com>
2023-09-03 02:04:29 +0300

    show image in MealsScreenUIState

--- client_restaurant/shared/src/commonMain/kotlin/presentation/meals/MealsScreenUIState.kt
+++ client_restaurant/shared/src/commonMain/kotlin/presentation/meals/MealsScreenUIState.kt
@@ -35,7 +35,7 @@ data class MealUIState(
     val id: String,
     val name: String,
     val price: String,
-    val imageUrl: String="https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800",
+    val imageUrl: String,
 )
 
 fun Meal.toMealUIState(): MealUIState {
@@ -43,7 +43,7 @@ fun Meal.toMealUIState(): MealUIState {
         id = id,
         name = name,
         price = "\$ $price",
-        imageUrl = imageUrl,
+        imageUrl = "https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800",
     )
 }
 
