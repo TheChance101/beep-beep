9f427ba275cf86d121ee65b7821db36dbf3831b7
Youssef <yr925344git@gmail.com>
2023-09-02 16:08:10 +0300

    refactor identity gateway

--- client_restaurant/shared/src/commonMain/kotlin/data/gateway/remote/IdentityRemoteGateway.kt
+++ client_restaurant/shared/src/commonMain/kotlin/data/gateway/remote/IdentityRemoteGateway.kt
@@ -1,30 +1,32 @@
 package data.gateway.remote
 
+import data.remote.mapper.toEntity
 import data.remote.model.BaseResponse
+import data.remote.model.SessionDto
 import data.remote.model.UserTokensDto
+import domain.entity.Session
 import domain.gateway.remote.IIdentityRemoteGateway
 import io.ktor.client.HttpClient
 import io.ktor.client.request.forms.submitForm
 import io.ktor.client.request.url
 import io.ktor.http.Parameters
+import presentation.base.InvalidCredentialsException
 
 class IdentityRemoteGateway(client: HttpClient) : IIdentityRemoteGateway,
     BaseRemoteGateway(client = client) {
 
-    override suspend fun loginUser(userName: String, password: String): Pair<String, String> {
-        val result = tryToExecute<BaseResponse<UserTokensDto>> {
+    override suspend fun loginUser(userName: String, password: String): Session {
+        val result = tryToExecute<BaseResponse<SessionDto>> {
             submitForm(
+                url = ("/login"),
                 formParameters = Parameters.build {
                     append("username", userName)
                     append("password", password)
-
                 }
-            ) {
-                url("/login")
-            }
-        }.value ?: throw Exception()
+            )
+        }.value
 
-        return Pair(result.accessToken, result.refreshToken)
+        return result?.toEntity() ?: throw InvalidCredentialsException("Invalid Credential")
     }
 
 
