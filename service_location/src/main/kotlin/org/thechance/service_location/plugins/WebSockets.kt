01406602c6025a783ee87dc6df5eb3c9661fe2db
Aziza <aziza.helmy35@gmail.com>
2023-09-03 18:44:26 +0300

    handle WebSockets.kt

--- service_location/src/main/kotlin/org/thechance/service_location/plugins/WebSockets.kt
+++ service_location/src/main/kotlin/org/thechance/service_location/plugins/WebSockets.kt
@@ -1,10 +1,11 @@
 package org.thechance.service_location.plugins
 
-import io.ktor.client.plugins.websocket.*
 import io.ktor.serialization.kotlinx.*
 import io.ktor.server.application.*
+import io.ktor.server.websocket.*
 import kotlinx.serialization.json.Json
 import java.time.Duration
+
 
 fun Application.configureSockets() {
     install(WebSockets) {
