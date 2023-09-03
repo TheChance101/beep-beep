c82f38310d9c406b6801e19f68b01773ba932b26
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-09-03 11:29:10 +0200

    save user configuration on realm db

---
+++ client_end_user/shared/src/commonMain/kotlin/domain/gateway/local/ILocalConfigurationGateway.kt
@@ -0,0 +1,13 @@
+package domain.gateway.local
+
+interface ILocalConfigurationGateway {
+
+    suspend fun saveAccessToken(token: String)
+    suspend fun getAccessToken(): String
+    suspend fun saveRefreshToken(token: String)
+    suspend fun getRefreshToken(): String
+    suspend fun saveKeepMeLoggedInFlag(isChecked: Boolean)
+    suspend fun getKeepMeLoggedInFlag(): Boolean
+    suspend fun clearTokens()
+
+}
