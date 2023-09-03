af12e0a137109f28f5ad521ecc679ccdbdca8749
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-09-04 00:02:06 +0300

    refactor using bottom nav in design system

--- client_end_user/shared/src/commonMain/kotlin/presentation/main/MainContainer.kt
+++ client_end_user/shared/src/commonMain/kotlin/presentation/main/MainContainer.kt
@@ -1,18 +1,8 @@
 package presentation.main
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -24,24 +14,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.positionInWindow
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.tab.CurrentTab
 import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
-import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabNavigator
-import com.beepbeep.designSystem.ui.theme.Theme
+import com.beepbeep.designSystem.ui.composable.BpNavigationBar
+import com.beepbeep.designSystem.ui.composable.BpNavigationBarItem
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
-import resources.Resources
+import presentation.composable.exitinstion.drawTopIndicator
+import presentation.composable.exitinstion.toPx
 
 
 object MainContainer : Screen {
@@ -56,7 +42,9 @@ object MainContainer : Screen {
                     Text(text = tabNavigator.current.options.title)
                 },
                 bottomBar = {
-                    MainBottomBar()
+                    val tabNavigator = LocalTabNavigator.current
+                    val tabs = rememberTabsContainer()
+                    BottomBar(tabs, tabNavigator)
                 },
                 content = {
                     Column(Modifier.fillMaxSize().padding(it)) {
@@ -67,112 +55,38 @@ object MainContainer : Screen {
         }
     }
 
-    @Composable
-    private fun MainBottomBar(){
-        val tabNavigator = LocalTabNavigator.current
-        var selectedItemPositionPx by remember { mutableStateOf(0f) }
-        val selectedItemPositionAnimated by animateFloatAsState(selectedItemPositionPx)
-        val colors = Theme.colors
-
-        Row(
-            Modifier.fillMaxWidth().height(64.dp).background(colors.surface)
-                .drawBehind {
-                    drawLine(
-                        color = colors.divider,
-                        strokeWidth = 1.dp.toPx(),
-                        start = Offset.Zero,
-                        end = Offset.Zero.copy(x = size.width)
-                    )
-                    drawLine(
-                        color = colors.primary,
-                        strokeWidth = 1.dp.toPx(),
-                        start = Offset.Zero.copy(x = selectedItemPositionAnimated + 4.dp.toPx()),
-                        end = Offset.Zero.copy(x = selectedItemPositionAnimated + 40.dp.toPx() + 4.dp.toPx()),
-                        cap = StrokeCap.Round
-                    )
-                }.padding(horizontal = 16.dp),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            TabNavigationItem(
-                tab = HomeTab,
-                selectedIconResource = Resources.images.homeFilled,
-                unSelectedIconResource = Resources.images.homeOutlined,
-                modifier = Modifier.weight(1f),
-                onSelectChange = { tab -> tabNavigator.current = tab },
-                onPositioned = { selectedItemPositionPx = it }
-            )
-            TabNavigationItem(
-                tab = SearchTab,
-                selectedIconResource = Resources.images.searchFilled,
-                unSelectedIconResource = Resources.images.searchOutlined,
-                modifier = Modifier.weight(1f),
-                onSelectChange = { tab -> tabNavigator.current = tab },
-                onPositioned = { selectedItemPositionPx = it }
-            )
-            TabNavigationItem(
-                tab = OrdersTab,
-                selectedIconResource = Resources.images.ordersFilled,
-                unSelectedIconResource = Resources.images.ordersOutlined,
-                modifier = Modifier.weight(1f),
-                onSelectChange = { tab -> tabNavigator.current = tab },
-                onPositioned = { selectedItemPositionPx = it }
-            )
-            TabNavigationItem(
-                tab = NotificationsTab,
-                selectedIconResource = Resources.images.notificationsFilled,
-                unSelectedIconResource = Resources.images.notificationsOutlined,
-                modifier = Modifier.weight(1f),
-                onSelectChange = { tab -> tabNavigator.current = tab },
-                onPositioned = { selectedItemPositionPx = it }
-            )
-            TabNavigationItem(
-                tab = ProfileTab,
-                selectedIconResource = Resources.images.profileFilled,
-                unSelectedIconResource = Resources.images.profileOutlined,
-                modifier = Modifier.weight(1f),
-                onSelectChange = { tab -> tabNavigator.current = tab },
-                onPositioned = { selectedItemPositionPx = it }
-            )
-        }
-    }
-
     @OptIn(ExperimentalResourceApi::class)
     @Composable
-    private fun RowScope.TabNavigationItem(
-        tab: Tab,
-        selectedIconResource: String,
-        unSelectedIconResource: String,
-        selected: Boolean = LocalTabNavigator.current.current == tab,
-        onSelectChange: (Tab) -> Unit,
-        onPositioned: (Float) -> Unit,
-        modifier: Modifier = Modifier,
-    ) {
-        val drawable = if (selected) selectedIconResource else unSelectedIconResource
-        val tint by animateColorAsState(if (selected) Theme.colors.primary else Theme.colors.contentTertiary)
-        var itemPosition by remember { mutableStateOf(0f) }
-        val iconSizePx = with(LocalDensity.current) { 24.dp.toPx() }
+    private fun BottomBar(tabs: List<TabContainer>, tabNavigator: TabNavigator) {
+        var xIndicatorOffset by remember { mutableStateOf(Float.NaN) }
+        val xOffsetAnimated by animateFloatAsState(targetValue = xIndicatorOffset)
+        val indicatorWidthPx = 40.dp.toPx()
+        val iconSizePx = 24.dp.toPx()
 
-        Column(
-            modifier.clickable(
-                remember { MutableInteractionSource() },
-                null
-            ) { onSelectChange(tab); onPositioned(itemPosition) },
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(4.dp)
-        ) {
-            Icon(
-                painter = painterResource(drawable),
-                tint = tint,
-                contentDescription = tab.options.title,
-                modifier = Modifier.size(24.dp).onGloballyPositioned {
-                    itemPosition = it.positionInWindow().x - iconSizePx / 2
-                    if (selected) {
-                        onPositioned(itemPosition)
-                    }
-                }
-            )
-            AnimatedVisibility(visible = selected) {
-                Text(text = tab.options.title, style = Theme.typography.caption.copy(color = tint))
+        BpNavigationBar(modifier = Modifier.drawTopIndicator(xOffsetAnimated)) {
+            tabs.forEach { tabContainer ->
+                val selected = tabNavigator.current == tabContainer.tab
+                val drawable =
+                    if (selected) tabContainer.selectedIcon else tabContainer.unSelectedIcon
+
+                BpNavigationBarItem(
+                    icon = { color ->
+                        Icon(
+                            painter = painterResource(drawable),
+                            contentDescription = null,
+                            tint = color,
+                            modifier = Modifier.size(24.dp).onGloballyPositioned {
+                                if (selected) {
+                                    xIndicatorOffset =
+                                        it.positionInRoot().x + (iconSizePx - indicatorWidthPx) / 2
+                                }
+                            }
+                        )
+                    },
+                    label = { Text(text = tabContainer.tab.options.title, style = it) },
+                    selected = selected,
+                    onClick = { tabNavigator.current = tabContainer.tab },
+                )
             }
         }
     }
