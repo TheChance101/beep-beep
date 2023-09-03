d70ae38c9d203e25c55ba3122ccd73a9062b0bbc
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-09-03 14:41:03 +0300

    add BaseScreen and BaseScreenModel

---
+++ client_taxi_driver/shared/src/commonMain/kotlin/presentation/base/BaseScreenModel.kt
@@ -0,0 +1,108 @@
+package presentation.base
+
+import cafe.adriel.voyager.core.model.ScreenModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import org.koin.core.component.KoinComponent
+
+abstract class BaseScreenModel<S, E>(initialState: S) : ScreenModel, KoinComponent {
+
+    abstract val viewModelScope: CoroutineScope
+    private val _state = MutableStateFlow(initialState)
+    val state = _state.asStateFlow()
+
+    private val _effect = MutableSharedFlow<E?>()
+    val effect = _effect.asSharedFlow()
+
+    protected fun <T> tryToExecute(
+        function: suspend () -> T,
+        onSuccess: (T) -> Unit,
+        onError: (ErrorState) -> Unit,
+        inScope: CoroutineScope = viewModelScope,
+    ): Job {
+        return runWithErrorCheck(onError, inScope) {
+            val result = function()
+            onSuccess(result)
+        }
+    }
+
+    protected fun <T> tryToCollect(
+        function: suspend () -> Flow<T>,
+        onNewValue: (T) -> Unit,
+        onError: (ErrorState) -> Unit,
+        inScope: CoroutineScope = viewModelScope,
+    ): Job {
+        return runWithErrorCheck(onError, inScope) {
+            function().collect {
+                onNewValue(it)
+            }
+        }
+    }
+
+    protected fun updateState(updater: (S) -> S) {
+        _state.update(updater)
+    }
+
+    protected fun sendNewEffect(newEffect: E) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _effect.emit(newEffect)
+        }
+    }
+
+    private fun runWithErrorCheck(
+        onError: (ErrorState) -> Unit,
+        inScope: CoroutineScope = viewModelScope,
+        dispatcher: CoroutineDispatcher = Dispatchers.Unconfined,
+        function: suspend () -> Unit,
+    ): Job {
+        return inScope.launch(dispatcher) {
+            try {
+                function()
+            } catch (exception: Exception) {
+                println("darkennes ${exception.message.toString()}")
+                when (exception) {
+                    is NoInternetException -> onError(ErrorState.NoInternet)
+                    is PermissionDenied -> onError(ErrorState.HasNoPermission)
+                    is ClientSideException -> onError(ErrorState.RequestFailed)
+                    is ServerSideException -> onError(ErrorState.RequestFailed)
+                    is UnAuthorizedException -> onError(ErrorState.UnAuthorized)
+                    is UserNotFoundException -> onError(ErrorState.UserNotExist(exception.errorMessage))
+                    is NotFoundedException -> onError(ErrorState.NotFound)
+                    is InvalidCredentialsException -> onError(
+                        ErrorState.InvalidCredentials(
+                            exception.message.toString()
+                        )
+                    )
+
+                    is UnknownErrorException -> onError(ErrorState.UnknownError(exception.message.toString()))
+                    else -> onError(ErrorState.UnknownError(exception.message.toString()))
+                }
+            }
+        }
+    }
+
+//    private fun <T> Flow<T>.throttleFirst(periodMillis: Long): Flow<T> {
+//        require(periodMillis > 0)
+//        return flow {
+//            var lastTime = 0L
+//            collect { value ->
+//                val currentTime = Clock.System.now().toEpochMilliseconds()
+//                if (currentTime - lastTime >= periodMillis) {
+//                    lastTime = currentTime
+//                    emit(value)
+//                }
+//            }
+//        }
+//    }
+
+}
