83236ea61b5f642bbb25b6efccef88e724e389ac
Aziza <aziza.helmy35@gmail.com>
2023-09-06 18:26:45 +0300

    add LocationDto

---
+++ service_location/src/main/kotlin/org/thechance/service_location/data/model/LocationDto.kt
@@ -0,0 +1,10 @@
+package org.thechance.service_location.data.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LocationDto(
+    @SerialName("latitude") val latitude: Double? = null,
+    @SerialName("longitude") val longitude: Double? = null
+)
