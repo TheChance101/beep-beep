d8bec1bb306f844c1ae8c73e90e1f9c34e9b3c57
HassanAyman <hassan3ayman@gmail.com>
2023-09-06 19:35:55 +0300

    implement broadCastLocation method

--- service_location/src/main/kotlin/org/thechance/service_location/util/SocketHandler.kt
+++ service_location/src/main/kotlin/org/thechance/service_location/util/SocketHandler.kt
@@ -1,10 +1,34 @@
 package org.thechance.service_location.util
 
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.flowOn
 import org.thechance.service_location.data.model.WebSocketLocation
+import org.thechance.service_location.data.utils.MultiErrorException
 import java.util.concurrent.ConcurrentHashMap
 
-
 class SocketHandler {
-    val location:ConcurrentHashMap<String,WebSocketLocation> = ConcurrentHashMap()
+    val location: ConcurrentHashMap<String,WebSocketLocation> = ConcurrentHashMap()
+
+    suspend fun broadcastLocation(locationId: String) {
+
+        val ownerSession = location[locationId]?.ownerSession
+        val locations = location[locationId]?.locations
+
+
+        try {
+            locations
+                ?.drop(1)
+                ?.flowOn(Dispatchers.IO)
+                ?.collect { location -> ownerSession?.sendSerialized(location) }
+        } catch (e: MultiErrorException) {
+            ownerSession?.send(e.errorCodes.toString())
+            ownerSession?.close()
+        } finally {
+            this.location.remove(locationId)
+        }
+    }
 
 }
