d14f7b070837e31e6ef0d5ec97e50a9fb17b8fd3
Youssef <yr925344git@gmail.com>
2023-09-06 20:04:16 +0300

    change port number

--- service_location/src/main/kotlin/org/thechance/service_location/Application.kt
+++ service_location/src/main/kotlin/org/thechance/service_location/Application.kt
@@ -6,7 +6,7 @@ import io.ktor.server.netty.*
 import org.thechance.service_location.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8081, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
