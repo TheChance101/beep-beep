b5e2c0737813ce41a09f2671bfc08c34974cc852
Asia sama <sehunexo710@gmail.com>
2023-09-06 19:51:57 +0300

    handle sender location web socket

---
+++ service_location/src/main/kotlin/org/thechance/service_location/endpoints/LocationRoutes.kt
@@ -0,0 +1,29 @@
+package org.thechance.service_location.endpoints
+
+
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.koin.ktor.ext.inject
+import org.thechance.service_location.data.model.LocationDto
+import org.thechance.service_location.util.SocketHandler
+
+fun Route.locationRoutes() {
+
+    val socketHandler: SocketHandler by inject()
+
+    route("/location") {
+
+        webSocket("/sender/{tripId}") {
+            val tripId = call.parameters["tripId"]?.trim().orEmpty()
+            for (frame in incoming) {
+                if (frame is Frame.Text) {
+                    val location = Json.decodeFromString<LocationDto>(frame.readText())
+                    socketHandler.location[tripId]?.locations?.emit(location)
+                }
+            }
+        }
+    }
+}
