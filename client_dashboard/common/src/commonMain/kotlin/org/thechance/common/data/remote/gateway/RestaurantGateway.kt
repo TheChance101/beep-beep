5b706d828ae40362a78da2e6055c343be042e179
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-09-08 02:27:07 +0300

    add create cuisine endpoint call

--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/data/remote/gateway/RestaurantGateway.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/data/remote/gateway/RestaurantGateway.kt
@@ -1,30 +1,59 @@
 package org.thechance.common.data.remote.gateway
 
+import io.ktor.client.*
+import io.ktor.client.request.forms.*
+import io.ktor.http.*
+import org.thechance.common.data.remote.model.CuisineDto
+import org.thechance.common.data.remote.model.ServerResponse
 import org.thechance.common.domain.entity.DataWrapper
 import org.thechance.common.domain.entity.NewRestaurantInfo
 import org.thechance.common.domain.entity.Restaurant
 import org.thechance.common.domain.getway.IRestaurantGateway
 
-class RestaurantGateway : BaseGateway(), IRestaurantGateway {
+class RestaurantGateway(private val client: HttpClient) : BaseGateway(), IRestaurantGateway {
 
     override suspend fun createRestaurant(restaurant: NewRestaurantInfo): Restaurant {
-        TODO("Not yet implemented")
+        return Restaurant(
+            id = "1",
+            name = restaurant.name,
+            ownerUsername = restaurant.ownerUsername,
+            phoneNumber = restaurant.phoneNumber,
+            rating = "4.5".toDouble(),
+            priceLevel = 4,
+            workingHours = restaurant.workingHours
+        )
     }
 
     override suspend fun deleteRestaurants(restaurant: Restaurant): Restaurant {
-        TODO("Not yet implemented")
+        return Restaurant(
+            id = "1",
+            name = restaurant.name,
+            ownerUsername = restaurant.ownerUsername,
+            phoneNumber = restaurant.phoneNumber,
+            rating = "4.5".toDouble(),
+            priceLevel = 4,
+            workingHours = restaurant.workingHours
+        )
     }
 
     override suspend fun getCuisines(): List<String> {
-        TODO("Not yet implemented")
+        return listOf("Italian", "Chinese", "Mexican", "American", "Indian", "Japanese", "Thai")
     }
 
     override suspend fun createCuisine(cuisineName: String): String {
-        TODO("Not yet implemented")
+        println(cuisineName)
+        return tryToExecute<ServerResponse<CuisineDto>>(client) {
+            submitForm(
+                url = "/cuisine",
+                formParameters = parameters {
+                    append("name", cuisineName)
+                },
+            )
+        }.value?.name ?: ""
     }
 
     override suspend fun deleteCuisine(cuisineName: String): String {
-        TODO("Not yet implemented")
+        return ""
     }
 
     override suspend fun getRestaurants(
@@ -34,7 +63,7 @@ class RestaurantGateway : BaseGateway(), IRestaurantGateway {
         rating: Double?,
         priceLevel: Int?
     ): DataWrapper<Restaurant> {
-        TODO("Not yet implemented")
+        return DataWrapper(10, 1, listOf())
     }
 
 }
