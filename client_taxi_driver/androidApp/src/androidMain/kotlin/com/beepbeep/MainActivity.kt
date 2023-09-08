6eddd9199f85f749a2f162f22da8f086dd3e8bf4
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-09-08 18:50:56 +0300

    make the map work locally

--- client_taxi_driver/androidApp/src/androidMain/kotlin/com/beepbeep/MainActivity.kt
+++ client_taxi_driver/androidApp/src/androidMain/kotlin/com/beepbeep/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.beepbeep
 
-import MainView
 import android.os.Bundle
 import android.webkit.WebView
 import androidx.activity.compose.setContent
@@ -9,18 +8,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewState
-import com.google.accompanist.web.rememberWebViewStateWithHTMLData
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            AccompanishWebClient(url="http://192.168.0.199:5500/map/index.html")
+            accompanistWebClient(url = "File:///android_asset/covid-19/map/index.html")
         }
     }
+
     @Composable
-    fun AccompanishWebClient(
+    fun accompanistWebClient(
         modifier: Modifier = Modifier,
         url: String,
     ) {
@@ -30,8 +29,9 @@ class MainActivity : AppCompatActivity() {
             modifier = modifier,
             state = webViewState,
             captureBackPresses = true,
-            onCreated = { it : WebView ->
+            onCreated = { it: WebView ->
                 it.settings.javaScriptEnabled = true
+                it.settings.allowFileAccess = true
             }
         )
     }
