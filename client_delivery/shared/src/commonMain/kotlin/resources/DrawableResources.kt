a41416fa8be83003b17dac73e2b23b683ae671a4
Youssef <yr925344git@gmail.com>
2023-09-09 23:09:28 +0300

    add drawable resources

---
+++ client_delivery/shared/src/commonMain/kotlin/resources/DrawableResources.kt
@@ -0,0 +1,12 @@
+package resources
+
+data class DrawableResources(
+    val bpIcon: String = "ic_beep_beep_icon.xml",
+    val bpLogo: String = "ic_beep_beep_logo.xml",
+    val backgroundPattern: String = "background_pattern.png"
+)
+
+val BpDrawableDarkResources = DrawableResources(
+    // not needed but just in case
+    backgroundPattern = "background_pattern.png",
+)
