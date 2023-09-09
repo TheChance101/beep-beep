f757e1a6ab855c5174951903aa09f4204e11b964
Youssef <yr925344git@gmail.com>
2023-09-10 00:35:17 +0300

    remove wrong files

--- client_delivery/shared/src/commonMain/kotlin/domain/entity/Session.kt
+++
@@ -1,8 +0,0 @@
-package domain.entity
-
-data class Session(
-    val accessToken: String,
-    val refreshToken: String,
-    val accessTokenExpirationDate: Long,
-    val refreshTokenExpirationDate: Long,
-)
