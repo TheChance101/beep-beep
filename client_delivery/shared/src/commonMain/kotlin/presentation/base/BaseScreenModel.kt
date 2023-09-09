384efc2c5e0bf26aec2062115e13d21eafb84b92
Youssef <yr925344git@gmail.com>
2023-09-10 00:00:56 +0300

    add base screen model

---
+++ client_delivery/shared/src/commonMain/kotlin/presentation/base/BaseScreenModel.kt
@@ -0,0 +1,121 @@
+package presentation.base
+
+import cafe.adriel.voyager.core.model.ScreenModel
+import domain.utils.InvalidCredentialsException
+import domain.utils.InvalidPasswordException
+import domain.utils.InvalidUsernameException
+import domain.utils.NetworkNotSupportedException
+import domain.utils.NoInternetException
+import domain.utils.UnAuthorizedException
+import domain.utils.UserAlreadyExistException
+import domain.utils.UserNotFoundException
+import domain.utils.WifiDisabledException
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.datetime.Clock
+import org.koin.core.component.KoinComponent
+
+abstract class BaseScreenModel<S, E>(initialState: S) : ScreenModel, KoinComponent {
+
+    abstract val viewModelScope: CoroutineScope
+    private val _state = MutableStateFlow(initialState)
+    val state = _state.asStateFlow()
+
+    private val _effect = MutableSharedFlow<E?>()
+    val effect = _effect.asSharedFlow().throttleFirst(500).mapNotNull { it }
+
+    protected fun <T> tryToExecute(
+        function: suspend () -> T,
+        onSuccess: (T) -> Unit,
+        onError: (ErrorState) -> Unit,
+        inScope: CoroutineScope = viewModelScope
+    ): Job {
+        return runWithErrorCheck(onError, inScope) {
+            val result = function()
+            onSuccess(result)
+        }
+    }
+
+    protected fun <T> tryToCollect(
+        function: suspend () -> Flow<T>,
+        onNewValue: (T) -> Unit,
+        onError: (ErrorState) -> Unit,
+        inScope: CoroutineScope = viewModelScope
+    ): Job {
+        return runWithErrorCheck(onError, inScope) {
+            function().collect {
+                onNewValue(it)
+            }
+        }
+    }
+
+    protected fun updateState(updater: (S) -> S) {
+        _state.update(updater)
+    }
+
+    protected fun sendNewEffect(newEffect: E) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _effect.emit(newEffect)
+        }
+    }
+
+    private fun runWithErrorCheck(
+        onError: (ErrorState) -> Unit,
+        inScope: CoroutineScope = viewModelScope,
+        dispatcher: CoroutineDispatcher = Dispatchers.Unconfined,
+        function: suspend () -> Unit
+    ): Job {
+        return inScope.launch(dispatcher) {
+            try {
+                function()
+            } catch (e: WifiDisabledException) {
+                onError(ErrorState.WifiDisabled)
+            } catch (e: NoInternetException) {
+                onError(ErrorState.NoInternet)
+            } catch (e: NetworkNotSupportedException) {
+                onError(ErrorState.NetworkNotSupported)
+            } catch (e: InvalidUsernameException) {
+                onError(ErrorState.InvalidUsername)
+            } catch (e: InvalidPasswordException) {
+                onError(ErrorState.InvalidPassword)
+            } catch (e: UnAuthorizedException) {
+                onError(ErrorState.UnAuthorized)
+            } catch (e: InvalidCredentialsException) {
+                onError(ErrorState.WrongPassword(e.message ?: ""))
+            } catch (e: UserNotFoundException) {
+                onError(ErrorState.UserNotFound(e.message ?: ""))
+            } catch (e: UserAlreadyExistException) {
+                onError(ErrorState.UserAlreadyExists(e.message ?: ""))
+            } catch (e: Exception) {
+                onError(ErrorState.RequestFailed)
+            }
+        }
+    }
+
+    private fun <T> Flow<T>.throttleFirst(periodMillis: Long): Flow<T> {
+        require(periodMillis > 0)
+        return flow {
+            var lastTime = 0L
+            collect { value ->
+                val currentTime = Clock.System.now().toEpochMilliseconds()
+                if (currentTime - lastTime >= periodMillis) {
+                    lastTime = currentTime
+                    emit(value)
+                }
+            }
+        }
+    }
+
+}
