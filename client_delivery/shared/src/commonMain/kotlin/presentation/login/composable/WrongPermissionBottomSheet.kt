43768cff099516aeda390486baa4fb020c96a7fb
Aziza <aziza.helmy35@gmail.com>
2023-09-10 21:03:24 +0300

    Delivery app | enhance the design

--- client_delivery/shared/src/commonMain/kotlin/presentation/login/composable/WrongPermissionBottomSheet.kt
+++ client_delivery/shared/src/commonMain/kotlin/presentation/login/composable/WrongPermissionBottomSheet.kt
@@ -3,10 +3,12 @@ package presentation.login.composable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import com.beepbeep.designSystem.ui.composable.BpButton
@@ -14,12 +16,18 @@ import com.beepbeep.designSystem.ui.composable.BpTransparentButton
 import com.beepbeep.designSystem.ui.theme.Theme
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
+import presentation.login.LoginScreenInteractionListener
 import resources.Resources
 
 @OptIn(ExperimentalResourceApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun WrongPermissionBottomSheet() {
-    Column() {
+fun WrongPermissionBottomSheet(listener: LoginScreenInteractionListener,
+                               modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.wrapContentHeight()
+            .padding(horizontal = Theme.dimens.space16, vertical = Theme.dimens.space24)
+    ) {
         Icon(
             painter = painterResource(Resources.images.errorIcon),
             tint = Theme.colors.primary,
@@ -38,7 +46,7 @@ fun WrongPermissionBottomSheet() {
             style = Theme.typography.body,
             color = Theme.colors.contentSecondary,
             textAlign = TextAlign.Center,
-            modifier = Modifier.
+            modifier = Modifier.padding(bottom = Theme.dimens.space24)
         )
         BpButton(
             onClick = {},
