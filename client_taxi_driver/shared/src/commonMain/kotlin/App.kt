b084199275fc0c0a5568d96562c5c88c54d2c6b0
karrar mohammed <karrar.m.habeeb@gmail.com>
2023-09-10 22:21:14 +0300

    make koin work with ios

--- client_taxi_driver/shared/src/commonMain/kotlin/App.kt
+++ client_taxi_driver/shared/src/commonMain/kotlin/App.kt
@@ -1,11 +1,8 @@
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
-import di.appModule
-import org.koin.core.context.startKoin
 import presentation.main.MainScreen
 
 @Composable
 fun App() {
-    startKoin { modules(appModule()) }
     Navigator(MainScreen())
 }
