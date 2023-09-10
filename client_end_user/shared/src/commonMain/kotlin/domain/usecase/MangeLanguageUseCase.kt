0069f642b12c274910dd977540efd60f96d0809d
ay0o5h <aayosh553@gmail.com>
2023-09-10 13:30:39 +0300

    remove type

--- client_end_user/shared/src/commonMain/kotlin/domain/usecase/MangeLanguageUseCase.kt
+++ client_end_user/shared/src/commonMain/kotlin/domain/usecase/MangeLanguageUseCase.kt
@@ -3,16 +3,15 @@ package domain.usecase
 import domain.gateway.local.ILocalConfigurationGateway
 
 interface IMangeLanguageUseCase {
-    suspend fun saveLanguageCode(code: String): Boolean
+    suspend fun saveLanguageCode(code: String)
     suspend fun getLanguageCode(): String
 }
 
 class MangeLanguageUseCase(
     private val localGateway: ILocalConfigurationGateway,
 ) : IMangeLanguageUseCase {
-    override suspend fun saveLanguageCode(code: String): Boolean {
-        localGateway.saveLanguageCode(code)
-        return true
+    override suspend fun saveLanguageCode(code: String) {
+       return localGateway.saveLanguageCode(code)
     }
 
     override suspend fun getLanguageCode(): String {
