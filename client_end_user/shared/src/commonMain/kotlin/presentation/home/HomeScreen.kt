2ca117fa7fd52e4aa0781947f9293f81516acaee
haidy <haidyabugom3a@gmail.com>
2023-09-10 18:44:20 +0300

    fix when statement in home screen

--- client_end_user/shared/src/commonMain/kotlin/presentation/home/HomeScreen.kt
+++ client_end_user/shared/src/commonMain/kotlin/presentation/home/HomeScreen.kt
@@ -65,7 +65,7 @@ class HomeScreen :
             is HomeScreenUiEffect.NavigateToOrderTaxi -> println("Navigate to Order Taxi screen")
             is HomeScreenUiEffect.ScrollDownToRecommendedRestaurants -> println("Scroll down home screen")
             is HomeScreenUiEffect.NavigateToOfferItem -> println("Navigate to offer item details ${effect.offerId}")
-            HomeScreenUiEffect.NavigateToSearch -> println("Navigate to Search Screen")
+            is HomeScreenUiEffect.NavigateToSearch -> println("Navigate to Search Screen")
             is HomeScreenUiEffect.NavigateToOrderDetails ->  println("Navigate to order details ${effect.orderId}")
         }
     }
