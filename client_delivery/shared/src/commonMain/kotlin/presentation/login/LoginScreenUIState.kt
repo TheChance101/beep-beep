515ae7ef19c4cf36af8ac8bfcbe5b241da2e3b1d
Aziza <aziza.helmy35@gmail.com>
2023-09-10 18:22:35 +0300

    Delivery app | add ui states of permission

--- client_delivery/shared/src/commonMain/kotlin/presentation/login/LoginScreenUIState.kt
+++ client_delivery/shared/src/commonMain/kotlin/presentation/login/LoginScreenUIState.kt
@@ -13,5 +13,11 @@ data class LoginScreenUIState(
     val isPasswordError: Boolean = false,
     val usernameErrorMsg: String = "",
     val passwordErrorMsg: String = "",
+    //permission
+    val restaurantName: String = "",
+    val description: String = "",
+    val ownerEmail: String = "",
     val hasPermission: Boolean = false,
+    val showPermissionSheet: Boolean = false,
+
 )
