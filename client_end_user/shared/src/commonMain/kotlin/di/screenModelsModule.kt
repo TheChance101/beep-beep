17ce3a48af16c9f523a6a826528cb1cdb4d8835d
ay0o5h <aayosh553@gmail.com>
2023-09-10 10:22:28 +0300

    add language screen

--- client_end_user/shared/src/commonMain/kotlin/di/screenModelsModule.kt
+++ client_end_user/shared/src/commonMain/kotlin/di/screenModelsModule.kt
@@ -5,9 +5,11 @@ import org.koin.dsl.module
 import presentation.login.LoginScreenModel
 import presentation.home.HomeScreenModel
 import presentation.main.MainScreenModel
+import presentation.pickLanguage.PickLanguageScreenModel
 
 val screenModelsModule = module {
     factoryOf(::LoginScreenModel)
     factoryOf(::HomeScreenModel)
     factoryOf(::MainScreenModel)
+    factoryOf(::PickLanguageScreenModel)
 }
