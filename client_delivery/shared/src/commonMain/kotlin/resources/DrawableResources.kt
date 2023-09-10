c9f6395ea742c952b40b323b0787ac782c023325
Aziza <aziza.helmy35@gmail.com>
2023-09-10 17:26:37 +0300

    Delivery app | add strings in StringResources

--- client_delivery/shared/src/commonMain/kotlin/resources/DrawableResources.kt
+++ client_delivery/shared/src/commonMain/kotlin/resources/DrawableResources.kt
@@ -4,6 +4,7 @@ data class DrawableResources(
     val bpIcon: String = "ic_beep_beep_icon.xml",
     val bpLogo: String = "ic_beep_beep_logo.xml",
     val backgroundPattern: String = "background_pattern.png"
+    val errorIcon: String = "ic_error_icon.xml"
 )
 
 val BpDrawableDarkResources = DrawableResources(
