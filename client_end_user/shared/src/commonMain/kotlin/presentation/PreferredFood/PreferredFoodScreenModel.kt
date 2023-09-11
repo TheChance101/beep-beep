d34d49ab9dff76252d63d819999e2225bbc5429f
ay0o5h <aayosh553@gmail.com>
2023-09-11 13:38:22 +0300

    complete screen model functions

--- client_end_user/shared/src/commonMain/kotlin/presentation/PreferredFood/PreferredFoodScreenModel.kt
+++ client_end_user/shared/src/commonMain/kotlin/presentation/PreferredFood/PreferredFoodScreenModel.kt
@@ -1,6 +1,8 @@
 package presentation.PreferredFood
 import cafe.adriel.voyager.core.model.coroutineScope
+import domain.usecase.IMangePreferredFoodUseCase
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import presentation.PreferredFood.FoodUIState
 import presentation.PreferredFood.PreferredFoodInteractionListener
 import presentation.PreferredFood.PreferredFoodUIEffect
@@ -8,31 +10,21 @@ import presentation.PreferredFood.PreferredFoodUIState
 import presentation.base.BaseScreenModel
 import presentation.base.ErrorState
 
-class PreferredFoodScreenModel() :BaseScreenModel<PreferredFoodUIState, PreferredFoodUIEffect,>(PreferredFoodUIState()),
+class PreferredFoodScreenModel(
+ private val  mangePreferredFood: IMangePreferredFoodUseCase
+) :BaseScreenModel<PreferredFoodUIState, PreferredFoodUIEffect,>(PreferredFoodUIState()),
     PreferredFoodInteractionListener {
     override val viewModelScope: CoroutineScope = coroutineScope
 
 
-    init {
 
-    }
     override fun onPreferredFoodClicked(foodUIState: FoodUIState) {
-        tryToExecute(
-            function = {
-//                manageLanguage.saveLanguageCode(language.code)
-            },
-            onSuccess = {
-                onSavedPreferredFoodSuccess()
-            },
-            ::onSavedPreferredFoodError
-        )
+        viewModelScope.launch {
+            mangePreferredFood.savePreferredFood(foodUIState.name)
+            onSavedPreferredFoodSuccess()
+        }
     }
     private fun onSavedPreferredFoodSuccess() {
         sendNewEffect(PreferredFoodUIEffect.NavigateToPreferredScreen)
     }
-
-    private fun onSavedPreferredFoodError(errorState: ErrorState) {
-        println(errorState.toString())
-    }
-
 }
