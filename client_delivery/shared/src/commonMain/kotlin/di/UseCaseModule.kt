a77eed0a84559a522e9e62433df6b79b536f8729
HassanAyman <hassan3ayman@gmail.com>
2023-09-12 00:35:26 +0300

    Delivery app | add managelogin to the useCase module

--- client_delivery/shared/src/commonMain/kotlin/di/UseCaseModule.kt
+++ client_delivery/shared/src/commonMain/kotlin/di/UseCaseModule.kt
@@ -1,8 +1,11 @@
 package di
 
-
+import domain.usecase.IManageLoginUserUseCase
+import domain.usecase.ManageLoginUserUseCase
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val useCaseModule = module {
-
+    singleOf(::ManageLoginUserUseCase) { bind<IManageLoginUserUseCase>() }
 }
