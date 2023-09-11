c604d26b5ca204b1627fa4e37b1eb46dd1d57c63
Youssef <yr925344git@gmail.com>
2023-09-11 19:17:16 +0300

    add authorizationInterceptor

--- client_delivery/shared/src/commonMain/kotlin/di/GatewayModule.kt
+++ client_delivery/shared/src/commonMain/kotlin/di/GatewayModule.kt
@@ -1,11 +1,14 @@
 package di
 
+import data.gateway.local.LocalConfigurationGateway
 import data.gateway.remote.FakeRemoteGateway
 import domain.gateway.IFakeRemoteGateway
+import domain.gateway.local.ILocalConfigurationGateway
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val gatewayModule = module {
     singleOf(::FakeRemoteGateway) { bind<IFakeRemoteGateway>() }
+    singleOf(::LocalConfigurationGateway) { bind<ILocalConfigurationGateway>() }
 }
