60dd05efa2e4de3a0e98c27a570ae25ba7d90e16
HassanAyman <hassan3ayman@gmail.com>
2023-09-11 23:44:29 +0300

    Delivery app | add manage login useCase

---
+++ client_delivery/shared/src/commonMain/kotlin/domain/usecase/ManageLoginUserUseCase.kt
@@ -0,0 +1,50 @@
+package domain.usecase
+
+import domain.gateway.local.ILocalConfigurationGateway
+import domain.gateway.remote.IIdentityRemoteGateway
+import domain.utils.InvalidPasswordException
+import domain.utils.InvalidUsernameException
+
+interface IManageLoginUserUseCase {
+
+    suspend fun loginUser(
+        userName: String,
+        password: String,
+        isKeepMeLoggedInChecked: Boolean
+    )
+
+    suspend fun getKeepMeLoggedInFlag(): Boolean
+
+}
+
+class ManageLoginUserUseCase(
+    private val remoteGateway: IIdentityRemoteGateway,
+    private val localGateWay: ILocalConfigurationGateway
+) : IManageLoginUserUseCase {
+
+    override suspend fun loginUser(
+        userName: String,
+        password: String,
+        isKeepMeLoggedInChecked: Boolean
+    ) {
+        if (validateLoginFields(userName, password)) {
+            val userTokens = remoteGateway.loginUser(userName, password)
+            localGateWay.saveAccessToken(userTokens.accessToken)
+            localGateWay.saveRefreshToken(userTokens.refreshToken)
+            localGateWay.saveKeepMeLoggedInFlag(isKeepMeLoggedInChecked)
+        }
+    }
+
+    override suspend fun getKeepMeLoggedInFlag(): Boolean {
+        return localGateWay.getKeepMeLoggedInFlag()
+    }
+
+    private fun validateLoginFields(username: String, password: String): Boolean {
+        if (username.isEmpty() || "[a-zA-Z0-9_]+".toRegex().matches(username).not()) {
+            throw InvalidUsernameException("")
+        }else if (password.isEmpty() || password.length < 8) {
+            throw InvalidPasswordException("")
+        } else return true
+    }
+
+}
