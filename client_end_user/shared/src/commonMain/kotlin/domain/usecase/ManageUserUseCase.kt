f65f51a991db04e1f63bdf643f6fc52848535e6a
ay0o5h <aayosh553@gmail.com>
2023-09-11 11:54:40 +0300

    add cuisine images

--- client_end_user/shared/src/commonMain/kotlin/domain/usecase/ManageUserUseCase.kt
+++ client_end_user/shared/src/commonMain/kotlin/domain/usecase/ManageUserUseCase.kt
@@ -20,9 +20,7 @@ class ManageUserUseCase(
        return if (localGateway.getAccessToken().isNotEmpty()) {
             remoteGateway.getUsrWallet()
         } else {
-            throw AuthorizationException()
+            throw AuthorizationException.UnAuthorizedException
         }
     }
-
-
 }
