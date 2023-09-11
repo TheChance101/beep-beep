5386b86db6fd7e71e9b95c30a2ea2195fea64915
Asia sama <sehunexo710@gmail.com>
2023-09-11 20:38:09 +0300

    add all error state cases

--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/util/ErrorState.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/util/ErrorState.kt
@@ -5,9 +5,75 @@ sealed interface ErrorState {
     object NoConnection : ErrorState
 
     object UnKnownError : ErrorState
-
-    class InvalidCredentials(val errorMessage: String) : ErrorState
+    class InvalidPassword(val errorMessage: String) : ErrorState
 
     class UserNotExist(val errorMessage: String) : ErrorState
+    class UsernameCannotBeBlank(val errorMessage: String) : ErrorState
+    class InvalidUserRequestParameter(val errorMessage: String) : ErrorState
 
+    class InvalidTaxiId(val errorMessage: String) : ErrorState
+
+    class InvalidTaxiPlate(val errorMessage: String) : ErrorState
+
+    class InvalidTaxiColor(val errorMessage: String) : ErrorState
+
+    class InvalidCarType(val errorMessage: String) : ErrorState
+
+    class SeatOutOfRange(val errorMessage: String) : ErrorState
+
+    class InvalidTaxiLocation(val errorMessage: String) : ErrorState
+
+    class InvalidTaxiRate(val errorMessage: String) : ErrorState
+
+    class InvalidTaxiPrice(val errorMessage: String) : ErrorState
+
+    class TaxiAlreadyExists(val errorMessage: String) : ErrorState
+
+    class InvalidTaxiRequestParameter(val errorMessage: String) : ErrorState
+
+    class TaxiNotFound(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidId(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidName(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidLocation(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidDescription(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidPriceLevel(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidRate(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidPhone(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidTime(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidPage(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidPageLimit(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidOneOrMoreIds(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidPermissionUpdateLocation(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidUpdateParameter(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidPropertyRights(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidPrice(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidCuisineLimit(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidAddress(val errorMessage: String) : ErrorState
+
+    class RestaurantInvalidRequestParameter(val errorMessage: String) : ErrorState
+
+    class RestaurantNotFound(val errorMessage: String) : ErrorState
+
+    class RestaurantErrorAdd(val errorMessage: String) : ErrorState
+
+    class RestaurantClosed(val errorMessage: String) : ErrorState
+
+    class CuisineNameAlreadyExisted(val errorMessage: String) : ErrorState
 }
