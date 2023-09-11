d2d84b1fd6b894c365bc19b17a5ef7b7698debe7
ay0o5h <aayosh553@gmail.com>
2023-09-11 18:12:39 +0300

    add splash and app icon for android

--- client_end_user/androidApp/src/androidMain/kotlin/com/beepbeep/MainActivity.kt
+++ client_end_user/androidApp/src/androidMain/kotlin/com/beepbeep/MainActivity.kt
@@ -4,9 +4,11 @@ import MainView
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         setContent {
