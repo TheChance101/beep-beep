0faff01e0fbf30b93b589a746005f1e0d363e0e0
AyaAbdelaiz <ayaabdelaziz355@gmail.com>
2023-09-12 18:09:44 +0300

    implement login screen ui

--- client_support/common/src/commonMain/kotlin/org/thechance/common/presentation/resources/DrawableResources.kt
+++ client_support/common/src/commonMain/kotlin/org/thechance/common/presentation/resources/DrawableResources.kt
@@ -2,6 +2,7 @@ package org.thechance.common.presentation.resources
 
 data class DrawableResources(
     val login: String = "img_login_light.png",
+    val beepBeepLogoExpanded: String = "ic_beepbeep_logo_expanded.svg",
 )
 
 
