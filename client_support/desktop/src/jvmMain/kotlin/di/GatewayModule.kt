e7a725ac2752673a318ca52370adc2a21d3c9380
AyaAbdelaiz <ayaabdelaziz355@gmail.com>
2023-09-12 19:42:43 +0300

    add fake identity gateway

--- client_support/desktop/src/jvmMain/kotlin/di/GatewayModule.kt
+++ client_support/desktop/src/jvmMain/kotlin/di/GatewayModule.kt
@@ -3,11 +3,15 @@ package di
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import org.thechance.common.data.fake_gateway.IdentityFakeGateway
 import org.thechance.common.data.gateway.IdentityGateway
 import org.thechance.common.domain.gateway.IIdentityGateway
 
 val GatewayModule = module {
 
-    singleOf(::IdentityGateway) { bind<IIdentityGateway>()}
+    singleOf(::IdentityGateway) { bind<IIdentityGateway>() }
+
+    //Fake gateway
+//    singleOf(::IdentityFakeGateway) { bind<IIdentityGateway>() }
 
 }
