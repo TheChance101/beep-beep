9f1bb916ac58be1bf1b4980d4878caf71cd8d7ab
Nada Feteiha <nada.feteiha@gmail.com>
2023-09-12 09:35:18 -0700

    reset to default setting

--- api_gateway/src/main/kotlin/org/thechance/api_gateway/Application.kt
+++ api_gateway/src/main/kotlin/org/thechance/api_gateway/Application.kt
@@ -8,7 +8,7 @@ import org.thechance.api_gateway.plugins.*
 
 fun main() {
     embeddedServer(
-        Netty, port = 8086, host = "0.0.0.0", module = Application::module).start(wait = true)
+        Netty, port = 8080, host = "0.0.0.0", module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
