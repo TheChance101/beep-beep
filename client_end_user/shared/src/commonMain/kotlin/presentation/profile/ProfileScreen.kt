8e542e96d73da879ddbcaefa5e82d6db55221df1
ay0o5h <aayosh553@gmail.com>
2023-09-12 10:48:29 +0300

    add profile screen

--- client_end_user/shared/src/commonMain/kotlin/presentation/profile/ProfileScreen.kt
+++ client_end_user/shared/src/commonMain/kotlin/presentation/profile/ProfileScreen.kt
@@ -3,13 +3,30 @@ package presentation.profile
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.Navigator
+import presentation.base.BaseScreen
+import presentation.main.MainContainer
 import resources.Resources
 
-class ProfileScreen: Screen {
+class ProfileScreen:
+    BaseScreen<ProfileScreenModel, ProfileUIState, ProfileUIEffect, ProfileInteractionListener>() {
 
     @Composable
     override fun Content() {
-        Text(text = Resources.strings.profile)
+        initScreen(getScreenModel())
     }
+
+    override fun onEffect(effect: ProfileUIEffect, navigator: Navigator) {
+        when(effect){
+            is ProfileUIEffect.Logout -> navigator.replaceAll(MainContainer)
+        }
+    }
+
+
+    @Composable
+    override fun onRender(state: ProfileUIState, listener: ProfileInteractionListener) {
+        TODO("Not yet implemented")
+    }
+
 
 }
