05b3f269cc8f730f1ed7cc2a93ce69070d3efd8b
AyaAbdelaiz <ayaabdelaziz355@gmail.com>
2023-09-12 15:46:54 +0300

    provide local storage module dependency

--- client_support/common/src/commonMain/kotlin/org/thechance/common/data/local/model/UserConfigurationCollection.kt
+++ client_support/common/src/commonMain/kotlin/org/thechance/common/data/local/model/UserConfigurationCollection.kt
@@ -1,6 +1,8 @@
 package org.thechance.common.data.local.model
 
+import io.realm.kotlin.types.RealmObject
 
-class UserConfigurationCollection {
+
+class UserConfigurationCollection : RealmObject{
 
 }
