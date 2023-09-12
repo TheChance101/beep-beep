4c739ce786b1e913eb604c5b45540debe73f37f0
AyaAbdelaiz <ayaabdelaziz355@gmail.com>
2023-09-12 16:22:06 +0300

    add user configuration collection

--- client_support/common/src/commonMain/kotlin/org/thechance/common/data/local/model/UserConfigurationCollection.kt
+++ client_support/common/src/commonMain/kotlin/org/thechance/common/data/local/model/UserConfigurationCollection.kt
@@ -4,5 +4,8 @@ import io.realm.kotlin.types.RealmObject
 
 
 class UserConfigurationCollection : RealmObject{
-
+    var id: Int = 0
+    var keepLoggedIn: Boolean = false
+    var accessToken: String = ""
+    var refreshToken: String = ""
 }
