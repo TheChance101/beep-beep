d7859f16c5f9e910554efd6283e0405be323f62f
AyaAbdelaiz <ayaabdelaziz355@gmail.com>
2023-09-12 17:01:07 +0300

    add login user use case

--- client_support/common/src/commonMain/kotlin/org/thechance/common/data/gateway/UserGateway.kt
+++
@@ -1,4 +0,0 @@
-package org.thechance.common.data.gateway
-
-class UserGateway {
-}
---
+++ client_support/desktop/src/jvmMain/kotlin/di/UseCaseModule.kt
@@ -0,0 +1,13 @@
+package di
+
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.module
+import org.thechance.common.data.gateway.IdentityGateway
+import org.thechance.common.domain.gateway.IIdentityGateway
+
+val UseCaseModule = module {
+
+    singleOf(::IdentityGateway) { bind<IIdentityGateway>()}
+
+}
