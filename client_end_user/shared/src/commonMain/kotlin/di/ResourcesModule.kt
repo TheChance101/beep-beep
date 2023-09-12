2bafec8b491d853619c6dca85e30d8c72e9ea5d2
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-09-12 13:11:04 +0200

    check the language in  resources module

--- client_end_user/shared/src/commonMain/kotlin/di/ResourcesModule.kt
+++ client_end_user/shared/src/commonMain/kotlin/di/ResourcesModule.kt
@@ -3,9 +3,17 @@ package di
 import org.koin.dsl.module
 import resources.strings.ar.Arabic
 import resources.strings.en.English
-import resources.strings.IStringResources
+import util.userCountryCode
+import util.userLanguage
 
 val resourcesModule = module {
-    single<IStringResources> { Arabic() }
-    single<IStringResources> { English() }
+    single {
+        val language = userLanguage
+        val countryCode = userCountryCode
+        when (language) {
+            "ar" -> Arabic()
+            else -> English()
+        }
+    }
 }
+
