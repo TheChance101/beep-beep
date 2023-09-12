474edd5bed6500d09bbc6c81e386286e611a0bb5
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-09-12 13:10:33 +0300

    solve conflict

--- client_end_user/shared/src/commonMain/kotlin/di/screenModelsModule.kt
+++ client_end_user/shared/src/commonMain/kotlin/di/screenModelsModule.kt
@@ -3,6 +3,8 @@ package di
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 import presentation.auth.login.LoginScreenModel
+import presentation.auth.signup.registration.RegistrationScreenModel
+import presentation.auth.signup.registrationSubmit.RegistrationSubmitScreenModel
 import presentation.home.HomeScreenModel
 import presentation.main.MainScreenModel
 import presentation.preferredMeal.PreferredScreenModel
