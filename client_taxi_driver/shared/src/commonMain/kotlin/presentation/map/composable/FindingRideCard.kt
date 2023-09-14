e90e97e7f9598d2f2358187e9bb986317d8fa84d
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-09-14 17:15:04 +0300

    add FindingRideCard

---
+++ client_taxi_driver/shared/src/commonMain/kotlin/presentation/map/composable/FindingRideCard.kt
@@ -0,0 +1,43 @@
+package presentation.map.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.beepbeep.designSystem.ui.theme.Theme
+import presentation.resources.Resources
+
+@Composable
+fun FindingRideCard(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp, bottom = 21.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 24.dp)
+                .background(Theme.colors.surface, RoundedCornerShape(8.dp))
+                .padding(horizontal = 14.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = Resources.strings.mapScreenFindingRequest,
+                color = Theme.colors.contentPrimary,
+                style = Theme.typography.headline,
+            )
+
+            LinearProgressIndicator(modifier = Modifier.padding(top = 18.dp))
+        }
+    }
+}
