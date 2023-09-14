79cb83328d6cfff765fdd46fa8f1cd7e479abfc5
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-09-15 00:41:29 +0300

    add FindingRideCard for MapScreen

--- client_taxi_driver/shared/src/commonMain/kotlin/presentation/map/MapScreen.kt
+++ client_taxi_driver/shared/src/commonMain/kotlin/presentation/map/MapScreen.kt
@@ -1,18 +1,16 @@
 package presentation.map
 
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
-import org.jetbrains.compose.resources.ExperimentalResourceApi
 import presentation.base.BaseScreen
+import presentation.map.composable.FindingRideCard
 
 class MapScreen :
     BaseScreen<MapScreenModel, MapScreenUiState, MapUiEffect, MapScreenInteractionListener>() {
 
     @Composable
-    @OptIn(ExperimentalResourceApi::class, ExperimentalMaterial3Api::class)
     override fun onRender(state: MapScreenUiState, listener: MapScreenInteractionListener) {
-        TODO("Not yet implemented")
+        FindingRideCard()
     }
 
     override fun onEffect(effect: MapUiEffect, navigator: Navigator) {
--- client_taxi_driver/shared/src/commonMain/kotlin/presentation/map/composable/FindingRideCard.kt
+++ client_taxi_driver/shared/src/commonMain/kotlin/presentation/map/composable/FindingRideCard.kt
@@ -3,7 +3,6 @@ package presentation.map.composable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,23 +20,27 @@ fun FindingRideCard(
     modifier: Modifier = Modifier,
 ) {
     Box(
-        modifier = modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp, bottom = 21.dp)
+        modifier = modifier.fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, bottom = 21.dp)
     ) {
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(vertical = 24.dp)
+                .fillMaxWidth()
                 .background(Theme.colors.surface, RoundedCornerShape(8.dp))
                 .padding(horizontal = 14.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
+                modifier = Modifier.padding(top = 24.dp).align(Alignment.CenterHorizontally),
                 text = Resources.strings.mapScreenFindingRequest,
                 color = Theme.colors.contentPrimary,
                 style = Theme.typography.headline,
             )
 
-            LinearProgressIndicator(modifier = Modifier.padding(top = 18.dp))
+            LinearProgressIndicator(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(top = 18.dp, bottom = 24.dp, start = 16.dp, end = 16.dp),
+                color = Theme.colors.primary
+            )
         }
     }
 }
