3950cf46daa73268dc01f5536d96aca100159cf7
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-09-14 17:15:22 +0300

    add noRippleEffect modifier

--- client_taxi_driver/shared/src/commonMain/kotlin/presentation/resources/StringResources.kt
+++ client_taxi_driver/shared/src/commonMain/kotlin/presentation/resources/StringResources.kt
@@ -3,5 +3,6 @@ package presentation.resources
 data class StringResources(
     val start: String = "start",
     val mainScreenTitle: String = "Foodies are waiting!",
-    val mainScreenSubTitle: String = "Tap Start and become their food superhero"
+    val mainScreenSubTitle: String = "Tap Start and become their food superhero",
+    val mapScreenFindingRequest: String = "Finding ride requests",
 )
