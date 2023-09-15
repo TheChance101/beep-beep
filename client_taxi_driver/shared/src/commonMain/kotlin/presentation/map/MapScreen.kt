ba2498537b53bd613d65d0c7b23e018193b49fa3
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-09-15 16:52:32 +0300

    add MapCard composable function

--- client_taxi_driver/shared/src/commonMain/kotlin/presentation/map/MapScreen.kt
+++ client_taxi_driver/shared/src/commonMain/kotlin/presentation/map/MapScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.Navigator
 import com.beepbeep.designSystem.ui.composable.BpAppBar
@@ -63,7 +64,7 @@ class MapScreen :
                     )
                 }
             }
-            Order(modifier = Modifier.align(Alignment.BottomCenter))
+            FindingRideCard(modifier = Modifier.align(Alignment.BottomCenter))
         }
     }
 
@@ -78,166 +79,161 @@ class MapScreen :
 
 
     @Composable
+    private fun MapCard(
+        modifier: Modifier = Modifier,
+        content: @Composable () -> Unit,
+    ) {
+        Card(
+            modifier = modifier.fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, bottom = 20.dp),
+            colors = CardDefaults.cardColors(containerColor = Theme.colors.surface),
+            shape = RoundedCornerShape(8.dp),
+            elevation = CardDefaults.elevatedCardElevation(0.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 24.dp)
+            ) {
+                content()
+            }
+        }
+    }
+
+
+    @Composable
     private fun FindingRideCard(
         modifier: Modifier = Modifier,
     ) {
-        Card(
-            modifier = modifier.fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp, bottom = 21.dp),
-            colors = CardDefaults.cardColors(containerColor = Theme.colors.surface),
-            shape = RoundedCornerShape(8.dp),
-            elevation = CardDefaults.elevatedCardElevation(0.dp),
+        MapCard(
+            modifier = modifier,
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp, vertical = 24.dp)
-            ) {
-                Text(
-                    modifier = Modifier.align(Alignment.CenterHorizontally),
-                    text = Resources.strings.findingRequest,
-                    color = Theme.colors.contentPrimary,
-                    style = Theme.typography.headline,
-                )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = Resources.strings.findingRequest,
+                color = Theme.colors.contentPrimary,
+                style = Theme.typography.headline,
+                textAlign = TextAlign.Center
+            )
 
-                LinearProgressIndicator(
-                    modifier = Modifier.fillMaxWidth()
-                        .padding(top = 18.dp),
-                    color = Theme.colors.primary,
-                    trackColor = Theme.colors.secondary,
-                    strokeCap = StrokeCap.Round
-                )
-            }
+            LinearProgressIndicator(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(top = 18.dp),
+                color = Theme.colors.primary,
+                trackColor = Theme.colors.secondary,
+                strokeCap = StrokeCap.Round
+            )
         }
     }
 
 
     @Composable
-    private fun NewOrder(
+    private fun NewOrderCard(
         modifier: Modifier = Modifier,
     ) {
-        Card(
-            modifier = modifier.fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp, bottom = 20.dp),
-            colors = CardDefaults.cardColors(containerColor = Theme.colors.surface),
-            shape = RoundedCornerShape(8.dp),
-            elevation = CardDefaults.elevatedCardElevation(0.dp),
+        MapCard(
+            modifier = modifier,
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp, vertical = 24.dp)
-            ) {
-                Text(
-                    text = Resources.strings.newOrder,
-                    color = Theme.colors.contentPrimary,
-                    style = Theme.typography.headline,
+            Text(
+                text = Resources.strings.newOrder,
+                color = Theme.colors.contentPrimary,
+                style = Theme.typography.headline,
+            )
+
+            Text(
+                modifier = Modifier.padding(top = 32.dp),
+                text = Resources.strings.name,
+                color = Theme.colors.contentPrimary,
+                style = Theme.typography.titleLarge,
+            )
+
+            Row(modifier = Modifier.padding(top = 10.dp)) {
+                Image(
+                    painter = painterResource(Resources.images.mapPoint),
+                    contentDescription = null
                 )
 
                 Text(
-                    modifier = Modifier.padding(top = 32.dp),
+                    modifier = Modifier.padding(start = 4.dp),
                     text = Resources.strings.name,
-                    color = Theme.colors.contentPrimary,
-                    style = Theme.typography.titleLarge,
+                    color = Theme.colors.contentSecondary,
+                    style = Theme.typography.body,
+                )
+            }
+
+            BpButton(
+                modifier = Modifier.fillMaxWidth().padding(top = 32.dp),
+                title = Resources.strings.acceptRequest,
+                onClick = {})
+
+            BpButton(
+                modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                title = Resources.strings.cancel,
+                containerColor = Color.Transparent,
+                contentColor = Theme.colors.contentTertiary,
+                onClick = {})
+        }
+    }
+
+
+    @Composable
+    private fun OrderCard(
+        modifier: Modifier = Modifier,
+    ) {
+        MapCard(
+            modifier = modifier
+        ) {
+            Text(
+                text = Resources.strings.name,
+                color = Theme.colors.contentPrimary,
+                style = Theme.typography.titleLarge,
+            )
+
+            Row(modifier = Modifier.padding(top = 30.dp)) {
+                Image(
+                    painter = painterResource(Resources.images.currentLocation),
+                    contentDescription = null
                 )
 
-                Row(modifier = Modifier.padding(top = 10.dp)) {
-                    Image(
-                        painter = painterResource(Resources.images.mapPoint),
-                        contentDescription = null
-                    )
-
+                Column(modifier = Modifier.padding(start = 8.dp)) {
                     Text(
-                        modifier = Modifier.padding(start = 4.dp),
+                        text = Resources.strings.pickUp,
+                        color = Theme.colors.contentTertiary,
+                        style = Theme.typography.caption,
+                    )
+                    Text(
                         text = Resources.strings.name,
-                        color = Theme.colors.contentSecondary,
+                        color = Theme.colors.contentPrimary,
                         style = Theme.typography.body,
                     )
                 }
-
-                BpButton(
-                    modifier = Modifier.fillMaxWidth().padding(top = 32.dp),
-                    title = Resources.strings.acceptRequest,
-                    onClick = {})
-
-                BpButton(
-                    modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
-                    title = Resources.strings.cancel,
-                    containerColor = Color.Transparent,
-                    contentColor = Theme.colors.contentTertiary,
-                    onClick = {})
             }
-        }
-    }
 
-
-    @Composable
-    private fun Order(
-        modifier: Modifier = Modifier,
-    ) {
-        Card(
-            modifier = modifier.fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp, bottom = 20.dp),
-            colors = CardDefaults.cardColors(containerColor = Theme.colors.surface),
-            shape = RoundedCornerShape(8.dp),
-            elevation = CardDefaults.elevatedCardElevation(0.dp),
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp, vertical = 24.dp)
-            ) {
-                Text(
-                    text = Resources.strings.name,
-                    color = Theme.colors.contentPrimary,
-                    style = Theme.typography.titleLarge,
+            Row(modifier = Modifier.padding(top = 24.dp)) {
+                Image(
+                    painter = painterResource(Resources.images.location),
+                    contentDescription = null
                 )
 
-                Row(modifier = Modifier.padding(top = 30.dp)) {
-                    Image(
-                        painter = painterResource(Resources.images.currentLocation),
-                        contentDescription = null
+                Column(modifier = Modifier.padding(start = 8.dp)) {
+                    Text(
+                        text = Resources.strings.dropOff,
+                        color = Theme.colors.contentTertiary,
+                        style = Theme.typography.caption,
                     )
-
-                    Column(modifier = Modifier.padding(start = 8.dp)) {
-                        Text(
-                            text = Resources.strings.pickUp,
-                            color = Theme.colors.contentTertiary,
-                            style = Theme.typography.caption,
-                        )
-                        Text(
-                            text = Resources.strings.name,
-                            color = Theme.colors.contentPrimary,
-                            style = Theme.typography.body,
-                        )
-                    }
-                }
-
-                Row(modifier = Modifier.padding(top = 24.dp)) {
-                    Image(
-                        painter = painterResource(Resources.images.location),
-                        contentDescription = null
+                    Text(
+                        text = Resources.strings.name,
+                        color = Theme.colors.contentPrimary,
+                        style = Theme.typography.body,
                     )
-
-                    Column(modifier = Modifier.padding(start = 8.dp)) {
-                        Text(
-                            text = Resources.strings.dropOff,
-                            color = Theme.colors.contentTertiary,
-                            style = Theme.typography.caption,
-                        )
-                        Text(
-                            text = Resources.strings.name,
-                            color = Theme.colors.contentPrimary,
-                            style = Theme.typography.body,
-                        )
-                    }
                 }
-
-                BpButton(
-                    modifier = Modifier.fillMaxWidth().padding(top = 24.dp),
-                    title = Resources.strings.arrived,
-                    onClick = {})
             }
+
+            BpButton(
+                modifier = Modifier.fillMaxWidth().padding(top = 24.dp),
+                title = Resources.strings.arrived,
+                onClick = {})
         }
     }
 }
+
