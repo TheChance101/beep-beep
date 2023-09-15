bae8752b33447db40a4316be8f04770bf21b0400
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-09-15 04:09:16 +0300

    add NewOrder composable function for map screen

--- client_taxi_driver/shared/src/commonMain/kotlin/presentation/map/MapScreen.kt
+++ client_taxi_driver/shared/src/commonMain/kotlin/presentation/map/MapScreen.kt
@@ -1,21 +1,35 @@
 package presentation.map
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.Navigator
 import com.beepbeep.designSystem.ui.composable.BpAppBar
+import com.beepbeep.designSystem.ui.composable.BpButton
+import com.beepbeep.designSystem.ui.theme.Theme
 import presentation.base.BaseScreen
-import presentation.map.composable.FindingRideCard
 import presentation.resources.Resources
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MapScreen :
     BaseScreen<MapScreenModel, MapScreenUiState, MapUiEffect, MapScreenInteractionListener>() {
 
@@ -34,7 +48,7 @@ class MapScreen :
                     contentDescription = null
                 )
             }
-            FindingRideCard(modifier = Modifier.align(Alignment.BottomCenter))
+            NewOrder(modifier = Modifier.align(Alignment.BottomCenter))
         }
     }
 
@@ -47,4 +61,94 @@ class MapScreen :
         initScreen(getScreenModel())
     }
 
+
+    @Composable
+    private fun FindingRideCard(
+        modifier: Modifier = Modifier,
+    ) {
+        Card(
+            modifier = modifier.fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, bottom = 21.dp),
+            colors = CardDefaults.cardColors(containerColor = Theme.colors.surface),
+            shape = RoundedCornerShape(8.dp),
+            elevation = CardDefaults.elevatedCardElevation(0.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 24.dp)
+            ) {
+                Text(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    text = Resources.strings.mapScreenFindingRequest,
+                    color = Theme.colors.contentPrimary,
+                    style = Theme.typography.headline,
+                )
+
+                LinearProgressIndicator(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(top = 18.dp),
+                    color = Theme.colors.primary,
+                    trackColor = Theme.colors.secondary,
+                    strokeCap = StrokeCap.Round
+                )
+            }
+        }
+    }
+
+
+    @Composable
+    private fun NewOrder(
+        modifier: Modifier = Modifier,
+    ) {
+        Card(
+            modifier = modifier.fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, bottom = 20.dp),
+            colors = CardDefaults.cardColors(containerColor = Theme.colors.surface),
+            shape = RoundedCornerShape(8.dp),
+            elevation = CardDefaults.elevatedCardElevation(0.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 24.dp)
+            ) {
+                Text(
+                    text = Resources.strings.newOrder,
+                    color = Theme.colors.contentPrimary,
+                    style = Theme.typography.headline,
+                )
+
+                Text(
+                    modifier = Modifier.padding(top = 32.dp),
+                    text = Resources.strings.name,
+                    color = Theme.colors.contentPrimary,
+                    style = Theme.typography.headline,
+                )
+
+                Row(modifier = Modifier.padding(top = 10.dp)) {
+                    Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+
+                    Text(
+                        modifier = Modifier.padding(start = 4.dp),
+                        text = Resources.strings.name,
+                        color = Theme.colors.contentSecondary,
+                        style = Theme.typography.titleMedium,
+                    )
+                }
+
+                BpButton(
+                    modifier = Modifier.fillMaxWidth().padding(top = 32.dp),
+                    title = Resources.strings.acceptRequest,
+                    onClick = {})
+
+                BpButton(
+                    modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                    title = Resources.strings.cancel,
+                    containerColor = Color.Transparent,
+                    contentColor = Theme.colors.contentTertiary,
+                    onClick = {})
+            }
+        }
+    }
 }
--- client_taxi_driver/shared/src/commonMain/kotlin/presentation/resources/StringResources.kt
+++ client_taxi_driver/shared/src/commonMain/kotlin/presentation/resources/StringResources.kt
@@ -6,4 +6,9 @@ data class StringResources(
     val mainScreenSubTitle: String = "Tap Start and become their food superhero",
     val mapScreenFindingRequest: String = "Finding ride requests",
     val welcomeDriver: String = "Welcome, Ali Ahmed!",
+    val newOrder: String = "New Order",
+    val name: String = "Kamel Mohamed",
+    val acceptRequest: String = "Accept Request",
+    val cancel: String = "Cancel",
+    val arrived: String = "Arrived",
 )
