36be53a8d2a3212217603b4f4367ef7f089514cb
youssef <yr925344git@gmail.com>
2023-09-16 17:21:15 +0300

    Delivery app | fix coroutine scope in map screen model

--- client_delivery/shared/src/commonMain/kotlin/presentation/map/MapScreenModel.kt
+++ client_delivery/shared/src/commonMain/kotlin/presentation/map/MapScreenModel.kt
@@ -1,12 +1,12 @@
 package presentation.map
 
+import cafe.adriel.voyager.core.model.coroutineScope
 import kotlinx.coroutines.CoroutineScope
 import presentation.base.BaseScreenModel
 
 class MapScreenModel:BaseScreenModel<MapScreenUiState,MapScreenUiEffect>(MapScreenUiState()),MapScreenInteractionsListener {
 
-    override val viewModelScope: CoroutineScope
-        get() = TODO("Not yet implemented")
+    override val viewModelScope: CoroutineScope = coroutineScope
 
     override fun onAcceptClicked() {
         TODO("Not yet implemented")
