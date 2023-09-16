cb10964f582bcd11061c59a303fb8a47eace0643
Aziza <aziza.helmy35@gmail.com>
2023-09-16 13:41:05 +0300

    Delivery app | add MapScreen.kt

---
+++ client_delivery/shared/src/commonMain/kotlin/presentation/map/MapScreen.kt
@@ -0,0 +1,20 @@
+package presentation.map
+
+import cafe.adriel.voyager.navigator.Navigator
+import presentation.base.BaseScreen
+
+
+class MapScreen:BaseScreen<MapScreenModel,MapScreenUiState,MapScreenUiEffect,MapScreenInteractionsListener>() {
+
+    override fun onRender(state: MapScreenUiState, listener: MapScreenInteractionsListener) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onEffect(effect: MapScreenUiEffect, navigator: Navigator) {
+        TODO("Not yet implemented")
+    }
+
+    override fun Content() {
+        TODO("Not yet implemented")
+    }
+}
