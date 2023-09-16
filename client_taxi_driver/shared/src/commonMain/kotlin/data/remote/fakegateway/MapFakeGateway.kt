93e0772846379df92dcbc7a583db5c9facf75aa7
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-09-16 18:45:39 +0300

    remove delay from ManageOrderUseCase to MapFakeGateway

--- client_taxi_driver/shared/src/commonMain/kotlin/data/remote/fakegateway/MapFakeGateway.kt
+++ client_taxi_driver/shared/src/commonMain/kotlin/data/remote/fakegateway/MapFakeGateway.kt
@@ -2,14 +2,18 @@ package data.remote.fakegateway
 
 import data.remote.mapper.toEntity
 import data.remote.model.OrderDto
+import domain.entity.Order
 import domain.gateway.IMapGateway
+import kotlinx.coroutines.delay
 
 class MapFakeGateway : IMapGateway {
-    override suspend fun findingNewOrder() =
-        OrderDto(
+    override suspend fun findingNewOrder(): Order {
+        delay(3000)
+        return OrderDto(
             id = "djsahdjadhjadjas45dsadas",
             passengerName = "Kamel Mohamed",
             dropOffAddress = "45, Faisal St., Riyadh, KSA",
             pickUpAddress = "Nirmala,girsls HSS",
         ).toEntity()
     }
+}
