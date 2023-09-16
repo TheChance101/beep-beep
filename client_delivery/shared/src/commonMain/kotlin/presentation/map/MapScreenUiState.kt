9666bd9a33b2948fba8f8033ae80934e269d14aa
Aziza <aziza.helmy35@gmail.com>
2023-09-16 13:14:13 +0300

    Delivery app | add MapScreenUiState.kt

---
+++ client_delivery/shared/src/commonMain/kotlin/presentation/map/MapScreenUiState.kt
@@ -0,0 +1,14 @@
+package presentation.map
+
+import presentation.base.ErrorState
+
+data class MapScreenUiState(
+    val isLoading: Boolean = true,
+    val errorState: ErrorState? = null,
+    val restaurantName: String = "",
+    val restaurantLocation: String = "",
+    val restaurantImageUrl: String? = null,
+    val orderLocation: String = "",
+    val isNewOrderFound: Boolean = false,
+    val isOrderAccepted: Boolean = false
+)
