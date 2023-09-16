5cecf4c107d672f17f9681441d8efc61eaeed9f5
Aziza <aziza.helmy35@gmail.com>
2023-09-16 13:36:56 +0300

    Delivery app | add MapScreenModel

---
+++ client_delivery/shared/src/commonMain/kotlin/presentation/map/MapScreenModel.kt
@@ -0,0 +1,19 @@
+package presentation.map
+
+import kotlinx.coroutines.CoroutineScope
+import presentation.base.BaseScreenModel
+
+class MapScreenModel:BaseScreenModel<MapScreenUiState,MapScreenUiEffect>(MapScreenUiState()),MapScreenInteractionsListener {
+
+    override val viewModelScope: CoroutineScope
+        get() = TODO("Not yet implemented")
+
+    override fun onAcceptClicked() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRejectClicked() {
+        TODO("Not yet implemented")
+    }
+
+}
