2333291b42e428606ec6eeeea005f7ec09798e65
Aziza <aziza.helmy35@gmail.com>
2023-09-17 21:51:35 +0300

    Delivery app | refactor code of NewOrderRequest.kt

--- client_delivery/shared/src/commonMain/kotlin/presentation/map/composable/NewOrderRequest.kt
+++ client_delivery/shared/src/commonMain/kotlin/presentation/map/composable/NewOrderRequest.kt
@@ -1,26 +1,12 @@
 package presentation.map.composable
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.beepbeep.designSystem.ui.composable.BpButton
 import com.beepbeep.designSystem.ui.composable.BpTransparentButton
@@ -41,88 +27,13 @@ fun NewOrderRequest(state: MapScreenUiState, listener: MapScreenInteractionsList
             style = Theme.typography.headline,
             modifier = Modifier.padding(top = 8.dp, bottom = 24.dp)
         )
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier.padding(bottom = 28.dp)
-        ) {
-            Image(
-                modifier = Modifier.size(48.dp).clip(shape = RoundedCornerShape(4.dp))
-                    .border(
-                        border = BorderStroke(width = 1.dp, color = Theme.colors.divider),
-                        shape = RoundedCornerShape(4.dp)
-                    ),
-                painter = painterResource(Resources.images.test),//just for now,then will be from state
-                contentDescription = null,
-                contentScale = ContentScale.Crop
-            )
-            Column(
-                verticalArrangement = Arrangement.spacedBy(4.dp),
-                modifier = Modifier.padding(4.dp)
-            ) {
-                Text(
-                    text = state.restaurantName,
-                    color = Theme.colors.contentPrimary,
-                    style = Theme.typography.titleLarge,
-                )
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(4.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        painter = painterResource(Resources.images.iconMapPoint),
-                        contentDescription = null,
-                        tint = Theme.colors.contentTertiary
-                    )
-                    Text(
-                        text = state.restaurantLocation,
-                        color = Theme.colors.contentSecondary,
-                        style = Theme.typography.caption,
-                    )
-                }
-            }
-        }
-        Divider(
-            color = Theme.colors.divider,
-            thickness = 1.dp
+        OrderInfo(
+            restaurantImage = painterResource(Resources.images.test),//just for now,then will be from state,
+            restaurantName = state.restaurantName,
+            restaurantLocation = state.restaurantLocation,
+            orderLocation = state.orderLocation
         )
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(12.dp),
-            modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)
-        ) {
-            Image(
-                painter = painterResource(Resources.images.iconLocation),
-                contentDescription = null, modifier = Modifier.padding(top = 8.dp)
-            )
-            Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-                Text(
-                    text = Resources.strings.deliverAt,
-                    style = Theme.typography.caption,
-                    color = Theme.colors.contentTertiary
-                )
-                Text(
-                    text = state.orderLocation,
-                    style = Theme.typography.body,
-                    color = Theme.colors.contentPrimary
-                )
-            }
-        }
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(
-                12.dp,
-                alignment = Alignment.CenterHorizontally
-            ),
-            modifier = Modifier.fillMaxWidth().padding(top = 16.dp)
-        ) {
-            DistanceInfo(
-                text = state.orderDistance,
-                icon = painterResource(Resources.images.iconPointOnMap)
-            )
-            DistanceInfo(
-                text = state.orderDuration,
-                icon = painterResource(Resources.images.iconClock)
-            )
-        }
+        DistanceInfo(orderDistance = state.orderDistance, orderDuration = state.orderDuration)
 
         BpButton(
             onClick = {},
@@ -137,27 +48,5 @@ fun NewOrderRequest(state: MapScreenUiState, listener: MapScreenInteractionsList
     }
 }
 
-@Composable
-private fun DistanceInfo(text: String, icon: Painter) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .border(
-                border = BorderStroke(width = 1.dp, color = Theme.colors.divider),
-                shape = RoundedCornerShape(8.dp)
-            )
-            .padding(vertical = 8.dp, horizontal = 44.dp)
-    ) {
-        Icon(
-            painter = icon,
-            contentDescription = null,
-            tint = Theme.colors.contentTertiary
-        )
-        Text(
-            modifier = Modifier.padding(vertical = 8.dp, horizontal = 12.dp),
-            text = text,
-            color = Theme.colors.contentSecondary,
-            style = Theme.typography.body,
-        )
-    }
-}
+
+
