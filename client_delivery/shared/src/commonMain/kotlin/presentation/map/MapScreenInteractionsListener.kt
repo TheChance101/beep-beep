2fb98cfe9498e92302990e78aebeff348fb4b4cf
HassanAyman <hassan3ayman@gmail.com>
2023-09-17 16:28:00 +0300

    Delivery app | add received and believed button clicks to listener

--- client_delivery/shared/src/commonMain/kotlin/presentation/map/MapScreenInteractionsListener.kt
+++ client_delivery/shared/src/commonMain/kotlin/presentation/map/MapScreenInteractionsListener.kt
@@ -6,5 +6,7 @@ import presentation.base.BaseInteractionListener
 interface MapScreenInteractionsListener : BaseInteractionListener {
     fun onAcceptClicked()
     fun onRejectClicked()
+    fun onReceivedClicked()
+    fun onDeliveredClicked()
     fun onCloseClicked()
 }
--- client_delivery/shared/src/commonMain/kotlin/presentation/map/MapScreenModel.kt
+++ client_delivery/shared/src/commonMain/kotlin/presentation/map/MapScreenModel.kt
@@ -2,17 +2,32 @@ package presentation.map
 
 import cafe.adriel.voyager.core.model.coroutineScope
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import presentation.base.BaseScreenModel
 
 class MapScreenModel:BaseScreenModel<MapScreenUiState,MapScreenUiEffect>(MapScreenUiState()),MapScreenInteractionsListener {
 
     override val viewModelScope: CoroutineScope = coroutineScope
-
+    init {
+        viewModelScope.launch {
+            delay(5000)
+            updateState { it.copy(isLoading = false) }
+        }
+    }
     override fun onAcceptClicked() {
-        TODO("Not yet implemented")
+        updateState { it.copy(isOrderAccepted = true) }
     }
 
     override fun onRejectClicked() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onReceivedClicked() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDeliveredClicked() {
         TODO("Not yet implemented")
     }
 
