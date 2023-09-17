7a395662612760d08af0507f26da75588d5b7d0b
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-09-18 00:05:34 +0300

    create location http client

--- client_dashboard/desktop/src/jvmMain/kotlin/di/NetworkModule.kt
+++ client_dashboard/desktop/src/jvmMain/kotlin/di/NetworkModule.kt
@@ -7,34 +7,44 @@ import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
 import io.ktor.serialization.gson.*
+import org.koin.core.qualifier.named
 import org.koin.core.scope.Scope
 import org.koin.dsl.module
 import org.thechance.common.domain.getway.IIdentityGateway
 
-
 val NetworkModule = module {
     single {
         val client = HttpClient(CIO) {
-
             expectSuccess = true
-
             install(Logging) {
                 logger = Logger.DEFAULT
                 level = LogLevel.ALL
             }
-
             defaultRequest {
                 header("Content-Type", "application/json")
                 url("https://beep-beep-api-gateway-nap2u.ondigitalocean.app")
                 header("Accept-Language", "ar")
                 header("Country-Code", "EG")
             }
-
             install(ContentNegotiation) {
                 gson()
             }
         }
         intercept(client)
+        client
+    }
+
+    single(named("locationClient")) {
+        val client = HttpClient(CIO) {
+            expectSuccess = true
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.ALL
+            }
+            install(ContentNegotiation) {
+                gson()
+            }
+        }
         client
     }
 }
