51fa8022bbcc743db75a1f834ace5d276114befe
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-09-18 18:43:48 +0300

    fix format code

--- client_taxi_driver/shared/src/commonMain/kotlin/data/remote/fakegateway/LocationFakeGateway.kt
+++ client_taxi_driver/shared/src/commonMain/kotlin/data/remote/fakegateway/LocationFakeGateway.kt
@@ -3,24 +3,21 @@ package data.remote.fakegateway
 import domain.entity.Location
 import domain.gateway.ILocationGateway
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class LocationFakeGateway : ILocationGateway {
-    override fun streamLiveLocation(): Flow<Location> {
-        return flow {
-            var lattiude = 30.044420
-            while (true) {
-                lattiude += 0.00002
-                emit(
-                    Location(
-                        lat = lattiude.toString(),
-                        lng = 31.235712.toString(),
-                        name = "",
-                    )
+    override fun streamLiveLocation() = flow {
+        var lattiude = 30.044420
+        while (true) {
+            lattiude += 0.00002
+            emit(
+                Location(
+                    lat = lattiude.toString(),
+                    lng = 31.235712.toString(),
+                    name = "",
                 )
-                delay(1000)
-            }
+            )
+            delay(1000)
         }
     }
 }
