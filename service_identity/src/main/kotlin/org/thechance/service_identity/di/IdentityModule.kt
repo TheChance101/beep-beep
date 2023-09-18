39579088a148049d5044baa2237d317895468052
Nada Feteiha <nada.feteiha@gmail.com>
2023-09-17 20:05:11 -0700

    reset to default

--- service_identity/src/main/kotlin/org/thechance/service_identity/di/IdentityModule.kt
+++ service_identity/src/main/kotlin/org/thechance/service_identity/di/IdentityModule.kt
@@ -17,11 +17,7 @@ val kmongoModule = module {
         val cluster = System.getenv("cluster")
         val username = System.getenv("username")
         val password = System.getenv("password")
-//        val connectionString = ConnectionString("mongodb+srv://$username:$password@$cluster.mongodb.net/")
-
-        val connectionString = ConnectionString("mongodb://localhost:27017")
-
-
+        val connectionString = ConnectionString("mongodb+srv://$username:$password@$cluster.mongodb.net/")
         val settings = MongoClientSettings.builder()
             .applyConnectionString(connectionString)
             .build()
