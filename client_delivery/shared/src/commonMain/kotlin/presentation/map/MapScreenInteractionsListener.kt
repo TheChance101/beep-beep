e63585308ac032e9ef8657dfff8d323f021b4cbf
Aziza <aziza.helmy35@gmail.com>
2023-09-18 18:26:24 +0300

    Delivery app | segregate the interfaces

--- client_delivery/shared/src/commonMain/kotlin/presentation/map/MapScreenInteractionsListener.kt
+++ client_delivery/shared/src/commonMain/kotlin/presentation/map/MapScreenInteractionsListener.kt
@@ -3,10 +3,17 @@ package presentation.map
 import presentation.base.BaseInteractionListener
 
 
-interface MapScreenInteractionsListener : BaseInteractionListener {
-    fun onAcceptClicked()
-    fun onRejectClicked()
-    fun onReceivedClicked()
+interface MapScreenInteractionsListener : NewOrderInteractionsListener {
+
     fun onDeliveredClicked()
     fun onCloseClicked()
+}
+
+interface NewOrderInteractionsListener : BaseInteractionListener {
+    fun onAcceptClicked()
+    fun onRejectClicked()
+}
+
+interface AcceptedOrderInteractionsListener : BaseInteractionListener {
+    fun onReceivedClicked()
 }
