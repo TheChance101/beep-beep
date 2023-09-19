06ca7796f251cdd1c854e6abc62a40c9b2e2ea76
youssef <yr925344git@gmail.com>
2023-09-19 19:27:42 +0300

    Delivery app | add map view in android and ios

---
+++ client_delivery/shared/src/commonMain/kotlin/presentation/map/composable/MapView.kt
@@ -0,0 +1,8 @@
+package presentation.map.composable
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+expect fun MapView(modifier: Modifier = Modifier)
---
+++ client_delivery/shared/src/iosMain/kotlin/presentation/map.composable/MapView.kt
@@ -0,0 +1,11 @@
+package presentation.map.composable
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+actual fun MapView(modifier: Modifier) {
+
+
+
+}
