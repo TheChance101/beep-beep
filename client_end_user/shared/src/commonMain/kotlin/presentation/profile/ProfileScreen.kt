a950f9179538bb78dae21366023d24871458bfd8
ay0o5h <aayosh553@gmail.com>
2023-09-20 08:12:56 +0300

    add info card

--- client_end_user/shared/src/commonMain/kotlin/presentation/profile/ProfileScreen.kt
+++ client_end_user/shared/src/commonMain/kotlin/presentation/profile/ProfileScreen.kt
@@ -1,9 +1,18 @@
 package presentation.profile
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import cafe.adriel.voyager.core.screen.Screen
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.Navigator
+import com.beepbeep.designSystem.ui.theme.Theme
 import presentation.base.BaseScreen
 import presentation.main.MainContainer
 import resources.Resources
@@ -25,8 +34,46 @@ class ProfileScreen:
 
     @Composable
     override fun onRender(state: ProfileUIState, listener: ProfileInteractionListener) {
-        TODO("Not yet implemented")
+        whiteCard {
+            title( Resources.strings.wallet)
+            subTitle("\$30.00",Theme.colors.primary)
+            title( Resources.strings.username)
+            subTitle("@Ali_ahmed")
+            title( Resources.strings.address)
+            subTitle("Park gavin, 123 street")
+            title( Resources.strings.email)
+            subTitle("Email@gmail.com")
+        }
     }
 
+    @Composable
+    private fun whiteCard(content: @Composable () -> Unit){
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(16.dp)
+                .clip(shape = RoundedCornerShape(Theme.radius.medium))
+                .background(Theme.colors.surface)
+                .padding(horizontal = 16.dp, vertical = 32.dp)
+        ) {
+            content()
+        }
+    }
 
+    @Composable
+    private fun title(text: String){
+        Text(
+            text = text,
+            style = Theme.typography.caption,
+            color= Theme.colors.contentTertiary,
+            modifier = Modifier.padding(top= 16.dp,bottom = 8.dp)
+        )
+    }
+
+    @Composable
+    private fun subTitle(text: String,color: Color = Theme.colors.contentPrimary){
+        Text(
+            text = text,
+            color=color,
+            style = Theme.typography.body,
+        )
+    }
 }
