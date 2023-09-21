599653df663b9b92568721215a59df3ae4b25ac1
karrar mohammed <karrar.m.habeeb@gmail.com>
2023-09-21 19:40:13 +0300

    map location library exception to domain exception

--- client_taxi_driver/shared/src/commonMain/kotlin/data/BpLocationDataSource.kt
+++ client_taxi_driver/shared/src/commonMain/kotlin/data/BpLocationDataSource.kt
@@ -1,6 +1,10 @@
 package data
 
 import dev.icerock.moko.geo.LocationTracker
+import dev.icerock.moko.permissions.DeniedAlwaysException
+import dev.icerock.moko.permissions.DeniedException
+import domain.LocationPermissionDeniedAlwaysException
+import domain.LocationPermissionDeniedException
 import domain.dataSource.IBpLocationDataSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -8,10 +12,16 @@ import kotlinx.coroutines.flow.map
 class BpLocationDataSource(private val locationTracker: LocationTracker) : IBpLocationDataSource {
 
     override suspend fun getCurrentLocation(): Flow<Pair<Double, Double>> {
-        locationTracker.startTracking()
-        return locationTracker.getLocationsFlow().map {
-            Pair(it.latitude, it.longitude)
-        }
+       try {
+            locationTracker.startTracking()
+            return locationTracker.getLocationsFlow().map {
+                Pair(it.latitude, it.longitude)
+            }
+        } catch(deniedAlways: DeniedAlwaysException) {
+           throw LocationPermissionDeniedAlwaysException(deniedAlways.message)
+       } catch(denied: DeniedException) {
+           throw LocationPermissionDeniedException(denied.message)
+       }
     }
 
     override fun stopTracking() {
