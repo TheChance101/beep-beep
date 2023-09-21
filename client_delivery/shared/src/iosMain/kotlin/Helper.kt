ecbe1b45066c185627fd26cde2825a8b61bb5711
HassanAyman <hassan3ayman@gmail.com>
2023-09-21 15:24:49 +0300

    Delivery app | add locationTracker for ios & android

--- client_delivery/shared/src/iosMain/kotlin/Helper.kt
+++ client_delivery/shared/src/iosMain/kotlin/Helper.kt
@@ -1,8 +1,15 @@
+import dev.icerock.moko.geo.LocationTracker
+import dev.icerock.moko.permissions.ios.PermissionsController
 import di.appModule
 import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+val locationTrackerModule = module {
+    single { LocationTracker(PermissionsController()) }
+}
 
 fun initKoin() {
     startKoin {
-        modules(appModule())
+        modules(appModule(), locationTrackerModule)
     }
 }
