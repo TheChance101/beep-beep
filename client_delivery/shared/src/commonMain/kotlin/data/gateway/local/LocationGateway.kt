9be2732532b57cd828423273492023fa5edb5461
HassanAyman <hassan3ayman@gmail.com>
2023-09-21 17:32:12 +0300

    Delivery app | handle access denied exception

--- client_delivery/shared/src/commonMain/kotlin/data/gateway/local/LocationGateway.kt
+++ client_delivery/shared/src/commonMain/kotlin/data/gateway/local/LocationGateway.kt
@@ -1,14 +1,20 @@
 package data.gateway.local
 
 import dev.icerock.moko.geo.LocationTracker
+import dev.icerock.moko.permissions.DeniedAlwaysException
 import domain.entity.Location
 import domain.gateway.local.ILocationGateway
+import domain.utils.LocationAccessDeniedException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class LocationGateway(private val locationTracker: LocationTracker): ILocationGateway {
     override suspend fun startTracking() {
-        locationTracker.startTracking()
+        try {
+            locationTracker.startTracking()
+        }catch (e: DeniedAlwaysException){
+            throw LocationAccessDeniedException()
+        }
     }
 
     override suspend fun getCurrentLocation(): Flow<Location> {
