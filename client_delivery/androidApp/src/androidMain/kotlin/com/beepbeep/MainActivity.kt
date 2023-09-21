549e053c5911de7a2bcdd433d1dacf42162902ac
youssef <yr925344git@gmail.com>
2023-09-21 04:09:37 +0300

    Delivery app | add moco geo location

--- client_delivery/androidApp/src/androidMain/kotlin/com/beepbeep/MainActivity.kt
+++ client_delivery/androidApp/src/androidMain/kotlin/com/beepbeep/MainActivity.kt
@@ -6,10 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
+import org.koin.android.ext.android.inject
+import dev.icerock.moko.geo.LocationTracker
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val locationTracker by inject<LocationTracker>()
+        locationTracker.bind(lifecycle, this, supportFragmentManager)
+
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             this.window.statusBarColor = ContextCompat.getColor(this, android.R.color.transparent)
