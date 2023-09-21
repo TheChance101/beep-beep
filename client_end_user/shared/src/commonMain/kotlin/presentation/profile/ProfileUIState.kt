8ccebae55551357978ca3f2708552c0eb102eaf3
ay0o5h <aayosh553@gmail.com>
2023-09-22 02:06:42 +0300

    add UserUIState

--- client_end_user/shared/src/commonMain/kotlin/presentation/profile/ProfileUIState.kt
+++ client_end_user/shared/src/commonMain/kotlin/presentation/profile/ProfileUIState.kt
@@ -1,7 +1,11 @@
 package presentation.profile
 
+import domain.entity.Address
+import domain.entity.Location
+import domain.entity.UserDetails
+
 data class ProfileUIState(
-    val user: UserUIState = UserUIState(),
+    val user: UserUIState? = UserUIState(),
     val fullName: String = "",
     val phoneNumber: String = "",
     val snackBarMessage: String = "",
@@ -14,9 +18,55 @@ data class ProfileUIState(
 )
 
 data class UserUIState(
-    val username: String = "",
+    val addresses: List<AddressUIState>? = emptyList(),
+    val country: String = "",
+    val currency: String = "",
     val email: String = "",
-    val address: String = "",
-    val wallet: String = "",
-
+    val fullName: String = "",
+    val id: String ="",
+    val permission: Int = 1,
+    val username: String ="",
+    val walletBalance: Double =0.0,
+    val phoneNumber: String =""
 )
+
+data class AddressUIState(
+    val address: String = "",
+    val id: String = "",
+    val location: LocationUIState? = LocationUIState()
+)
+
+data class LocationUIState(
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0
+)
+
+fun UserDetails.toUIState(): UserUIState {
+    return UserUIState(
+        addresses = addresses?.map { it.toUIState() },
+        country = country ?: "",
+        currency = currency ?: "",
+        email = email ?: "",
+        fullName = fullName ?: "",
+        id = id ?: "",
+        permission = permission ?: 1,
+        username = username ?: "",
+        walletBalance = walletBalance ?: 0.0,
+        phoneNumber = phoneNumber ?: ""
+    )
+}
+
+fun Address.toUIState(): AddressUIState {
+    return AddressUIState(
+        address = address ?: "",
+        id = id ?: "",
+        location = location?.toUIState()
+    )
+}
+
+fun Location.toUIState(): LocationUIState {
+    return LocationUIState(
+        latitude = latitude ?: 0.0,
+        longitude = longitude ?: 0.0
+    )
+}
