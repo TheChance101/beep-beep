944720a0808666923e406131e0b99802b3e543d3
HassanAyman <hassan3ayman@gmail.com>
2023-09-21 14:09:22 +0300

    Delivery app | add moko library dependency

--- client_delivery/shared/build.gradle.kts
+++ client_delivery/shared/build.gradle.kts
@@ -62,6 +62,8 @@ kotlin {
                 implementation(libs.ktor.client.cio)
                 implementation(libs.kotlin.serialization)
 
+                //moko
+                api(libs.moko.geo)
             }
         }
         val androidMain by getting {
