7b382a2c81670a6ed05041b429101ece06a8c205
Aziza <aziza.helmy35@gmail.com>
2023-09-22 02:32:46 +0300

    Delivery app | add MapRemoteGateway.kt with the required methods

--- client_delivery/shared/src/commonMain/kotlin/data/gateway/remote/MapRemoteGateway.kt
+++ client_delivery/shared/src/commonMain/kotlin/data/gateway/remote/MapRemoteGateway.kt
@@ -1,10 +1,31 @@
 package data.gateway.remote
 
+import data.remote.model.LocationDto
+import data.remote.model.TripDto
 import domain.gateway.remote.IMapRemoteGateway
 import io.ktor.client.HttpClient
 
 
 class MapRemoteGateway(client: HttpClient) : IMapRemoteGateway,
     BaseRemoteGateway(client = client) {
+    override suspend fun sendLocation(location: LocationDto, tripId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTripById(tripId: String): TripDto {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun approveTrip(taxiId: String, tripId: String, driverId: String): TripDto {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun finishTrip(tripId: String, driverId: String): TripDto {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteTrip(tripId: String): TripDto {
+        TODO("Not yet implemented")
+    }
 
 }
--- client_delivery/shared/src/commonMain/kotlin/domain/gateway/remote/IMapRemoteGateway.kt
+++ client_delivery/shared/src/commonMain/kotlin/domain/gateway/remote/IMapRemoteGateway.kt
@@ -1,6 +1,13 @@
 package domain.gateway.remote
 
+import data.remote.model.LocationDto
+import data.remote.model.TripDto
+
 
 interface IMapRemoteGateway {
-
+    suspend fun sendLocation(location: LocationDto, tripId: String)
+    suspend fun getTripById(tripId: String): TripDto
+    suspend fun approveTrip(taxiId: String, tripId: String, driverId: String): TripDto
+    suspend fun finishTrip(tripId: String, driverId: String): TripDto
+    suspend fun deleteTrip(tripId: String): TripDto
 }
