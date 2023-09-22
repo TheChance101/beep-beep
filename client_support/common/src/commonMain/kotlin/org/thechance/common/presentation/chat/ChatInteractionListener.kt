f789798d39a2eaa00457302ebdb9da49187ff8fd
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-09-22 18:32:56 +0300

    create appbar interactions

--- client_support/common/src/commonMain/kotlin/org/thechance/common/presentation/chat/ChatInteractionListener.kt
+++ client_support/common/src/commonMain/kotlin/org/thechance/common/presentation/chat/ChatInteractionListener.kt
@@ -3,4 +3,11 @@ package org.thechance.common.presentation.chat
 import org.thechance.common.presentation.base.BaseInteractionListener
 
 interface ChatInteractionListener : BaseInteractionListener {
+
+    fun onClickDropdownMenu()
+
+    fun onDismissDropdownMenu()
+
+    fun onClickLogOut()
+
 }
