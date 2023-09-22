dc408553ac65bb001299469600bfb8d50f8e8c8e
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-09-22 18:20:12 +0300

    create ticket and appbar ui state

--- client_support/common/src/commonMain/kotlin/org/thechance/common/presentation/chat/ChatUIState.kt
+++ client_support/common/src/commonMain/kotlin/org/thechance/common/presentation/chat/ChatUIState.kt
@@ -1,14 +1,28 @@
 package org.thechance.common.presentation.chat
 
 data class ChatUIState(
+    val ticket: TicketUIState = TicketUIState(),
     val messages: List<MessageUIState> = emptyList(),
+    val appbar: AppbarUIState = AppbarUIState(),
     val loading: Boolean = false,
 ) {
+    data class TicketUIState(
+        val id: String = "",
+        val username: String = "",
+        val userAvatarUrl: String = "",
+        val openedAt: String = "",
+    )
+
     data class MessageUIState(
         val id: String,
         val message: String,
         val isMe: Boolean,
         val avatarUrl: String,
         val showAvatar: Boolean = false,
+    )
+
+    data class AppbarUIState(
+        val username: String = "",
+        val isDropdownMenuExpanded: Boolean = false,
     )
 }
