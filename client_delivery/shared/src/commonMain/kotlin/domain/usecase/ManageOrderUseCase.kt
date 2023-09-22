fd15f21490a59443d78717ed7c4b007304019992
Aziza <aziza.helmy35@gmail.com>
2023-09-22 13:38:40 +0300

    Delivery app | add ManageOrderUseCase.kt

---
+++ client_delivery/shared/src/commonMain/kotlin/domain/usecase/ManageOrderUseCase.kt
@@ -0,0 +1,36 @@
+package domain.usecase
+
+import domain.entity.Location
+import domain.entity.Trip
+import domain.gateway.remote.IMapRemoteGateway
+
+interface IManageOrderUseCase {
+    suspend fun sendLocation(location: Location, tripId: String)
+    suspend fun getTripById(tripId: String): Trip
+    suspend fun approveTrip(taxiId: String, tripId: String, driverId: String): Trip
+    suspend fun finishTrip(tripId: String, driverId: String): Trip
+    suspend fun deleteTrip(tripId: String): Trip
+}
+
+class ManageOrderUseCase(private val remoteGateway: IMapRemoteGateway) : IManageOrderUseCase {
+    override suspend fun sendLocation(location: Location, tripId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTripById(tripId: String): Trip {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun approveTrip(taxiId: String, tripId: String, driverId: String): Trip {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun finishTrip(tripId: String, driverId: String): Trip {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteTrip(tripId: String): Trip {
+        TODO("Not yet implemented")
+    }
+
+}
