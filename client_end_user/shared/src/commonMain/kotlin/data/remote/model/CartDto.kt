06f54640aa937871a72c77cdf1715ffabe1d8241
haidy <haidyabugom3a@gmail.com>
2023-09-22 06:15:27 +0300

    add fake data for cart screen

---
+++ client_end_user/shared/src/commonMain/kotlin/data/remote/model/CartDto.kt
@@ -0,0 +1,10 @@
+package data.remote.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CartDto(
+    @SerialName("meals") val meals: List<CartMealDto>? = null,
+    @SerialName("totalPrice") val totalPrice: Double? = null
+)
---
+++ client_end_user/shared/src/commonMain/kotlin/domain/entity/Cart.kt
@@ -0,0 +1,6 @@
+package domain.entity
+
+data class Cart(
+    val meals: List<CartMeal>,
+    val totalPrice: Double
+)
