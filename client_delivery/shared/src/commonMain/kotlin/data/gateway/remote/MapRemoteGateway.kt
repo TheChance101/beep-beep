e6a57b8d8ac7e12551fb6409f48b776e852691c5
Aziza <aziza.helmy35@gmail.com>
2023-09-22 13:08:03 +0300

    Delivery app | implement all end points

--- client_delivery/shared/src/commonMain/kotlin/data/gateway/remote/MapRemoteGateway.kt
+++ client_delivery/shared/src/commonMain/kotlin/data/gateway/remote/MapRemoteGateway.kt
@@ -1,31 +1,72 @@
 package data.gateway.remote
 
+import data.remote.model.BaseResponse
 import data.remote.model.LocationDto
 import data.remote.model.TripDto
 import domain.gateway.remote.IMapRemoteGateway
 import io.ktor.client.HttpClient
+import io.ktor.client.request.delete
+import io.ktor.client.request.forms.FormDataContent
+import io.ktor.client.request.get
+import io.ktor.client.request.put
+import io.ktor.http.Parameters
+import io.ktor.util.InternalAPI
 
 
 class MapRemoteGateway(client: HttpClient) : IMapRemoteGateway,
     BaseRemoteGateway(client = client) {
     override suspend fun sendLocation(location: LocationDto, tripId: String) {
-        TODO("Not yet implemented")
+      client.tryToSendWebSocketData(
+            data = location,
+            path = "/location/sender/$tripId"
+        )
     }
 
     override suspend fun getTripById(tripId: String): TripDto {
-        TODO("Not yet implemented")
+       val result = tryToExecute<BaseResponse<TripDto>> {
+            get("/trip/$tripId")
+        }.value ?: throw Exception()
+
+        return result
     }
 
+    @OptIn(InternalAPI::class)
     override suspend fun approveTrip(taxiId: String, tripId: String, driverId: String): TripDto {
-        TODO("Not yet implemented")
+        val result = tryToExecute<BaseResponse<TripDto>> {
+            val formData = FormDataContent(Parameters.build {
+                append("tripId", tripId)
+                append("taxiId", taxiId)
+                append("driverId", driverId)
+            })
+            put("/trip/approve") {
+                body = formData
+            }
+        }.value ?: throw Exception()
+
+        return result
     }
 
+    @OptIn(InternalAPI::class)
     override suspend fun finishTrip(tripId: String, driverId: String): TripDto {
-        TODO("Not yet implemented")
+        val result = tryToExecute<BaseResponse<TripDto>> {
+            val formData = FormDataContent(Parameters.build {
+                append("tripId", tripId)
+                append("driverId", driverId)
+            })
+            put("/trip/finish$tripId") {
+                body = formData
+            }
+        }.value ?: throw Exception()
+
+        return result
     }
 
     override suspend fun deleteTrip(tripId: String): TripDto {
-        TODO("Not yet implemented")
+        val result = tryToExecute<BaseResponse<TripDto>> {
+            delete("trip/cancel$tripId")
+        }.value ?: throw Exception()
+
+        return result
     }
 
 }
