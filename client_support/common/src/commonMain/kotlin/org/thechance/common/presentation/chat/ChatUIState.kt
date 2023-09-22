60c117605e0c5f8041ccf4cd2a66d8f1e2dfb587
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-09-22 18:08:40 +0300

    create message ui state

--- client_support/common/src/commonMain/kotlin/org/thechance/common/presentation/chat/ChatUIState.kt
+++ client_support/common/src/commonMain/kotlin/org/thechance/common/presentation/chat/ChatUIState.kt
@@ -1,5 +1,14 @@
 package org.thechance.common.presentation.chat
 
 data class ChatUIState(
+    val messages: List<MessageUIState> = emptyList(),
     val loading: Boolean = false,
-)
+) {
+    data class MessageUIState(
+        val id: String,
+        val message: String,
+        val isMe: Boolean,
+        val avatarUrl: String,
+        val showAvatar: Boolean = false,
+    )
+}
