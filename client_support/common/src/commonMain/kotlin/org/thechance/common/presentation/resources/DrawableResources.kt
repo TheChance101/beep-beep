35bf8ea4467ada08e72198ee220aa39f75cb8d75
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-09-22 19:55:53 +0300

    create send icon resource

--- client_support/common/src/commonMain/kotlin/org/thechance/common/presentation/resources/DrawableResources.kt
+++ client_support/common/src/commonMain/kotlin/org/thechance/common/presentation/resources/DrawableResources.kt
@@ -5,12 +5,14 @@ data class DrawableResources(
     val beepBeepLogoExpanded: String = "ic_beepbeep_logo_expanded.svg",
     val dropDownArrow: String = "ic_drop_down_arrow_light.svg",
     val logout: String = "ic_logout.svg",
+    val send: String = "ic_send.svg",
 )
 
 
 val darkDrawableResource = DrawableResources(
     login = "img_login_dark.png",
     dropDownArrow = "ic_drop_down_arrow_dark.svg",
+    send = "ic_send.svg",
 )
 
 val lightDrawableResource = DrawableResources()
