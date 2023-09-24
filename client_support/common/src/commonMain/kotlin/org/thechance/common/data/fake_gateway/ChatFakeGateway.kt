69c5d6cb983a7cbafb5edaa9246030fdcf13a618
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-09-24 17:32:04 +0300

    create fake beer answer

--- client_support/common/src/commonMain/kotlin/org/thechance/common/data/fake_gateway/ChatFakeGateway.kt
+++ client_support/common/src/commonMain/kotlin/org/thechance/common/data/fake_gateway/ChatFakeGateway.kt
@@ -1,5 +1,6 @@
 package org.thechance.common.data.fake_gateway
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
@@ -11,6 +12,7 @@ import org.thechance.common.domain.entity.Message
 import org.thechance.common.domain.entity.NewMessage
 import org.thechance.common.domain.entity.Ticket
 import org.thechance.common.domain.gateway.IChatGateway
+import kotlin.random.Random
 
 class ChatFakeGateway : IChatGateway {
 
@@ -76,5 +78,24 @@ class ChatFakeGateway : IChatGateway {
 
     override suspend fun sendMessage(message: NewMessage) {
         messages.value += message.toDto()
+        delay(1000)
+        answerWithFakeMessage(message)
+    }
+
+    private fun answerWithFakeMessage(message: NewMessage) {
+        val fakeAnswers = listOf(
+            "Ok, thank you for your response.",
+            "I will check it.",
+            "I will get back to you soon.",
+            "That's great.",
+            "Seems like a good idea.",
+        )
+        messages.value += MessageDto(
+            id = Random.nextInt().toString(),
+            ticketId = message.ticketId,
+            message = fakeAnswers.random(),
+            senderId = (Random.nextInt(2, 1000)).toString(),
+            senderAvatar = ""
+        )
     }
 }
