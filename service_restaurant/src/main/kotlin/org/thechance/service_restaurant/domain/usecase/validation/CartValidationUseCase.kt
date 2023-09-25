bd427d29c4fb3db4623663a8864221c16e66e922
Nada Feteiha <nada.feteiha@gmail.com>
2023-09-24 22:31:08 -0700

    add cart validation

---
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/domain/usecase/validation/CartValidationUseCase.kt
@@ -0,0 +1,34 @@
+package org.thechance.service_restaurant.domain.usecase.validation
+
+import org.thechance.service_restaurant.domain.utils.IValidation
+import org.thechance.service_restaurant.domain.utils.exceptions.INVALID_ID
+import org.thechance.service_restaurant.domain.utils.exceptions.INVALID_QUANTITY
+import org.thechance.service_restaurant.domain.utils.exceptions.MultiErrorException
+
+interface ICartValidationUseCase {
+    fun validateUpdateCart(userId: String, restaurantId: String, mealId: String, quantity: Int)
+
+}
+
+class CartValidationUseCase(private val basicValidation: IValidation) : ICartValidationUseCase {
+    override fun validateUpdateCart(userId: String, restaurantId: String, mealId: String, quantity: Int) {
+        val validationErrors = mutableListOf<Int>()
+
+        if (basicValidation.isValidId(userId) ||
+            basicValidation.isValidId(restaurantId) ||
+            basicValidation.isValidId(mealId)
+        ) {
+            validationErrors.add(INVALID_ID)
+        }
+
+        if (quantity < 0) {
+            validationErrors.add(INVALID_QUANTITY)
+
+        }
+
+        if (validationErrors.isNotEmpty()) {
+            throw MultiErrorException(validationErrors)
+        }
+    }
+
+}
