4de7d12df1eba4409e6a8aaf0c5a894f0dc280bd
Nada Feteiha <nada.feteiha@gmail.com>
2023-09-24 22:27:14 -0700

    add and refactor cart, order collection

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/data/collection/relationModels/CartDetails.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/data/collection/relationModels/CartDetails.kt
@@ -1,24 +1,18 @@
 package org.thechance.service_restaurant.data.collection.relationModels
 
-import kotlinx.datetime.LocalDateTime
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
 import org.bson.types.ObjectId
 import org.thechance.service_restaurant.data.collection.CartCollection
-import org.thechance.service_restaurant.data.collection.OrderCollection
 import org.thechance.service_restaurant.data.collection.RestaurantCollection
 
 @Serializable
-data class OrderWithRestaurant(
+data class CartDetails(
     @Contextual
     val id: ObjectId,
     @Contextual
-    val userId:ObjectId,
-    val restaurant: RestaurantCollection,
-    val meals: List<CartCollection.MealCollection>,
-    val totalPrice: Double,
-    val createdAt: LocalDateTime,
-    val orderStatus: Int
+    val userId: ObjectId,
+    @Contextual
+    val restaurant: RestaurantCollection? = null,
+    val meals: List<CartCollection.MealCollection> = mutableListOf(),
 )
-
-
