bca3bce072149aa618ecb7bb04f8771b26a873bc
AyaAbdelaiz <ayaabdelaziz355@gmail.com>
2023-09-27 04:20:52 +0300

    refactor permission flow row

--- client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/composables/PermissionsFlowRow.kt
+++ client_dashboard/common/src/commonMain/kotlin/org/thechance/common/presentation/composables/PermissionsFlowRow.kt
@@ -10,15 +10,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import com.beepbeep.designSystem.ui.composable.BpChip
 import com.beepbeep.designSystem.ui.theme.Theme
-import org.thechance.common.presentation.users.UserScreenUiState
+import org.thechance.common.presentation.overview.PermissionUiState
+import org.thechance.common.presentation.resources.Resources
 import org.thechance.common.presentation.util.kms
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun PermissionsFlowRow(
-    allPermissions: List<UserScreenUiState.PermissionUiState>,
-    selectedPermissions: List<UserScreenUiState.PermissionUiState>,
-    onUserPermissionClicked: (UserScreenUiState.PermissionUiState) -> Unit,
+    allPermissions: List<PermissionUiState>,
+    selectedPermissions: List<PermissionUiState>,
+    onUserPermissionClicked: (PermissionUiState) -> Unit,
 ) {
     FlowRow(
         modifier = Modifier
@@ -27,18 +28,54 @@ fun PermissionsFlowRow(
             .padding(8.kms)
     ) {
         allPermissions.forEach { permission ->
+            println("Permission: $permission")
             BpChip(
-                label = permission.name.lowercase().capitalizeWords(),
+                label = getPermissionInfo(permission).title,
                 modifier = Modifier.padding(8.kms),
                 onClick = { onUserPermissionClicked(permission) },
-                painter = painterResource(permission.iconPath),
+                painter = painterResource(getPermissionInfo(permission).iconPath),
                 isSelected = selectedPermissions.contains(permission)
             )
         }
     }
 }
 
+@Composable
+fun getPermissionInfo(permission: PermissionUiState): PermissionInfo {
+    return when (permission) {
+        PermissionUiState.RESTAURANT_OWNER -> PermissionInfo(
+            Resources.Drawable.restaurantOutlined,
+            Resources.Strings.restaurantPermission
+        )
 
-private fun String.capitalizeWords(): String = split(" ")
-    .joinToString(" ") { it.replaceFirstChar { char -> char.uppercase() } }
-    .replace("_", " ")
+        PermissionUiState.DRIVER -> PermissionInfo(
+            Resources.Drawable.taxiOutlined,
+            Resources.Strings.taxiPermission
+        )
+
+        PermissionUiState.END_USER -> PermissionInfo(
+            Resources.Drawable.endUser,
+            Resources.Strings.endUserPermission
+        )
+
+        PermissionUiState.SUPPORT -> PermissionInfo(
+            Resources.Drawable.support,
+            Resources.Strings.supportPermission
+        )
+
+        PermissionUiState.DELIVERY -> PermissionInfo(
+            Resources.Drawable.delivery,
+            Resources.Strings.deliveryPermission
+        )
+
+        PermissionUiState.ADMIN -> PermissionInfo(
+            Resources.Drawable.dashboardAdmin,
+            Resources.Strings.adminPermission
+        )
+    }
+}
+
+data class PermissionInfo(
+    val iconPath: String,
+    val title: String,
+)
