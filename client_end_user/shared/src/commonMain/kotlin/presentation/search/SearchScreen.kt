935d1e7a514781142ebb8154a2d44c886cba16a8
Nada Feteiha <nada.feteiha@gmail.com>
2023-10-03 14:45:56 -0700

    add search screen

--- client_end_user/shared/src/commonMain/kotlin/presentation/search/SearchScreen.kt
+++ client_end_user/shared/src/commonMain/kotlin/presentation/search/SearchScreen.kt
@@ -1,8 +1,28 @@
 package presentation.search
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.Navigator
+import com.beepbeep.designSystem.ui.composable.BpSimpleTextField
+import com.beepbeep.designSystem.ui.theme.Theme
+import domain.entity.Restaurant
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 import presentation.base.BaseScreen
+import resources.Resources
+import util.getStatusBarPadding
 
 class SearchScreen :
     BaseScreen<SearchScreenModel, SearchUiState, SearchUiEffect, SearchInteractionListener>() {
@@ -12,14 +32,48 @@ class SearchScreen :
         initScreen(getScreenModel())
     }
 
+    @OptIn(ExperimentalResourceApi::class, ExperimentalFoundationApi::class)
     @Composable
     override fun onRender(state: SearchUiState, listener: SearchInteractionListener) {
-        TODO("Not yet implemented")
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Theme.colors.background)
+                .padding(getStatusBarPadding())
+                .padding(top = 16.dp)
+        ) {
+
+            stickyHeader {
+                BpSimpleTextField(
+                    state.query,
+                    hint = Resources.strings.searchHint,
+                    hintColor = Theme.colors.contentSecondary,
+                    onValueChange = listener::onSearchInputChanged,
+                    onClick = {},
+                    leadingPainter = painterResource(Resources.images.searchOutlined),
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+            }
+
+            item {
+                LazyRow {
+                    items(state.restaurants) {
+                        Text(it.name, color = Color.White)
+                    }
+                }
+            }
+
+            items(state.meals) {
+                Text(it.name, color = Color.White)
+            }
+        }
     }
 
     override fun onEffect(effect: SearchUiEffect, navigator: Navigator) {
-        TODO("Not yet implemented")
+
     }
+
+
 
 
 }
