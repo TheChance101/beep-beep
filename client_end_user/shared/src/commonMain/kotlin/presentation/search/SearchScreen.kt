0edc9795ed61745f59b2d6bb02705a234cf1f9c2
Nada Feteiha <nada.feteiha@gmail.com>
2023-10-03 15:56:56 -0700

    add Restaurant component

--- client_end_user/shared/src/commonMain/kotlin/presentation/search/SearchScreen.kt
+++ client_end_user/shared/src/commonMain/kotlin/presentation/search/SearchScreen.kt
@@ -1,26 +1,34 @@
 package presentation.search
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.Navigator
 import com.beepbeep.designSystem.ui.composable.BpSimpleTextField
 import com.beepbeep.designSystem.ui.theme.Theme
-import domain.entity.Restaurant
+import com.seiko.imageloader.rememberAsyncImagePainter
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import presentation.base.BaseScreen
+import presentation.composable.modifier.roundedBorderShape
 import resources.Resources
 import util.getStatusBarPadding
 
@@ -40,7 +48,8 @@ class SearchScreen :
                 .fillMaxSize()
                 .background(Theme.colors.background)
                 .padding(getStatusBarPadding())
-                .padding(top = 16.dp)
+                .padding(top = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
 
             stickyHeader {
@@ -56,9 +65,12 @@ class SearchScreen :
             }
 
             item {
-                LazyRow {
+                LazyRow(
+                    contentPadding = PaddingValues(horizontal = 16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
                     items(state.restaurants) {
-                        Text(it.name, color = Color.White)
+                        Restaurant(restaurant = it)
                     }
                 }
             }
@@ -74,6 +86,31 @@ class SearchScreen :
     }
 
 
+    @Composable
+    private fun Restaurant(
+        modifier: Modifier = Modifier,
+        restaurant: RestaurantUiState
+    ) {
+        Column(
+            modifier = modifier.widthIn(max = 64.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.roundedBorderShape().size(64.dp),
+                painter = rememberAsyncImagePainter(restaurant.image),
+                contentScale = ContentScale.Crop,
+                contentDescription = restaurant.name,
+            )
 
+            Text(
+                text = restaurant.name,
+                style = Theme.typography.body,
+                color = Theme.colors.contentPrimary,
+                maxLines = 2,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
 
 }
