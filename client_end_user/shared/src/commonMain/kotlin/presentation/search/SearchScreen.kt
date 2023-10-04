99365238c45d02b803e66dea0b64617e25f9ba40
Nada Feteiha <nada.feteiha@gmail.com>
2023-10-04 08:54:53 -0700

    add MealCard

--- client_end_user/shared/src/commonMain/kotlin/presentation/search/SearchScreen.kt
+++ client_end_user/shared/src/commonMain/kotlin/presentation/search/SearchScreen.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
@@ -28,9 +30,13 @@ import com.seiko.imageloader.rememberAsyncImagePainter
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import presentation.base.BaseScreen
+import presentation.composable.BpImageCard
+import presentation.composable.modifier.noRippleEffect
 import presentation.composable.modifier.roundedBorderShape
+import presentation.resturantDetails.RestaurantScreen
 import resources.Resources
 import util.getStatusBarPadding
+import util.root
 
 class SearchScreen :
     BaseScreen<SearchScreenModel, SearchUiState, SearchUiEffect, SearchInteractionListener>() {
@@ -38,6 +44,13 @@ class SearchScreen :
     @Composable
     override fun Content() {
         initScreen(getScreenModel())
+    }
+
+    override fun onEffect(effect: SearchUiEffect, navigator: Navigator) {
+        when (effect) {
+            is SearchUiEffect.NavigateToMeal -> TODO()
+            is SearchUiEffect.NavigateToRestaurant -> navigator.root?.push(RestaurantScreen)
+        }
     }
 
     @OptIn(ExperimentalResourceApi::class, ExperimentalFoundationApi::class)
@@ -50,6 +63,7 @@ class SearchScreen :
                 .padding(getStatusBarPadding())
                 .padding(top = 16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(bottom = 16.dp)
         ) {
 
             stickyHeader {
@@ -60,7 +74,7 @@ class SearchScreen :
                     onValueChange = listener::onSearchInputChanged,
                     onClick = {},
                     leadingPainter = painterResource(Resources.images.searchOutlined),
-                    modifier = Modifier.padding(horizontal = 16.dp)
+                    modifier = Modifier.background(Theme.colors.background).padding(horizontal = 16.dp)
                 )
             }
 
@@ -70,29 +84,32 @@ class SearchScreen :
                     horizontalArrangement = Arrangement.spacedBy(16.dp)
                 ) {
                     items(state.restaurants) {
-                        Restaurant(restaurant = it)
+                        Restaurant(
+                            restaurant = it,
+                            onClick = listener::onRestaurantClicked
+                        )
                     }
                 }
             }
 
             items(state.meals) {
-                Text(it.name, color = Color.White)
+                MealCard(
+                    meal = it,
+                    onClick = listener::onMealClicked
+                )
             }
         }
-    }
-
-    override fun onEffect(effect: SearchUiEffect, navigator: Navigator) {
-
     }
 
 
     @Composable
     private fun Restaurant(
         modifier: Modifier = Modifier,
-        restaurant: RestaurantUiState
+        restaurant: RestaurantUiState,
+        onClick: (String) -> Unit
     ) {
         Column(
-            modifier = modifier.widthIn(max = 64.dp),
+            modifier = modifier.widthIn(max = 64.dp).noRippleEffect { onClick(restaurant.id) },
             verticalArrangement = Arrangement.spacedBy(8.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -113,4 +130,54 @@ class SearchScreen :
         }
     }
 
+
+    @Composable
+    private fun MealCard(
+        modifier: Modifier = Modifier,
+        meal: MealUiState,
+        onClick: (String) -> Unit
+    ) {
+        Row(
+            modifier = modifier.fillMaxWidth().noRippleEffect { onClick(meal.id) }.padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            Image(
+                modifier = Modifier.roundedBorderShape().size(height = 72.dp, width = 80.dp),
+                painter = rememberAsyncImagePainter(meal.image),
+                contentScale = ContentScale.Crop,
+                contentDescription = meal.name
+            )
+            Column(
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = meal.name,
+                        style = Theme.typography.title,
+                        color = Theme.colors.contentPrimary,
+                        maxLines = 1
+                    )
+
+                    Text(
+                        text = meal.price,
+                        style = Theme.typography.body,
+                        color = Theme.colors.primary,
+                        maxLines = 1
+                    )
+                }
+                Text(
+                    text = meal.restaurantName,
+                    style = Theme.typography.body,
+                    color = Theme.colors.contentSecondary,
+                    maxLines = 1
+                )
+            }
+        }
+
+    }
 }
