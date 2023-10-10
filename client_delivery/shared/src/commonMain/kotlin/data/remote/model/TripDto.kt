cf49a5be020434b8b0eadc509fe3938d64a30598
Aziza <aziza.helmy35@gmail.com>
2023-10-10 22:31:38 +0300

    Delivery app | handle TripDto

--- client_delivery/shared/src/commonMain/kotlin/data/remote/model/TripDto.kt
+++ client_delivery/shared/src/commonMain/kotlin/data/remote/model/TripDto.kt
@@ -6,13 +6,17 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class TripDto(
     @SerialName("id")
-    val id: String? = null,
+    val id: String,
     @SerialName("taxiId")
     val taxiId: String? = null,
+    @SerialName("taxiPlateNumber")
+    val taxiPlateNumber: String? = null,
     @SerialName("driverId")
     val driverId: String? = null,
+    @SerialName("driverName")
+    val driverName: String? = null,
     @SerialName("clientId")
-    val clientId: String? = null,
+    val clientId: String,
     @SerialName("startPoint")
     val startPoint: LocationDto? = null,
     @SerialName("destination")
@@ -25,4 +29,6 @@ data class TripDto(
     val startDate: String? = null,
     @SerialName("endDate")
     val endDate: String? = null,
+    @SerialName("timeToArriveInMints")
+    val timeToArriveInMints: Int? = null,
 )
