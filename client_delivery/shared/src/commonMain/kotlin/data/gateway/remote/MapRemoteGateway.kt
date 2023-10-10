4399f3d03ae3c3166aa065a2235e385e71bd417f
Aziza <aziza.helmy35@gmail.com>
2023-10-10 22:34:04 +0300

    Delivery app | handle MapRemoteGateway

--- client_delivery/shared/src/commonMain/kotlin/data/gateway/remote/MapRemoteGateway.kt
+++ client_delivery/shared/src/commonMain/kotlin/data/gateway/remote/MapRemoteGateway.kt
@@ -1,18 +1,18 @@
 package data.gateway.remote
 
+import data.remote.mapper.toTripEntity
 import data.remote.model.BaseResponse
 import data.remote.model.LocationDto
 import data.remote.model.TripDto
+import domain.entity.Trip
 import domain.gateway.remote.IMapRemoteGateway
 import io.ktor.client.HttpClient
-import io.ktor.client.request.delete
 import io.ktor.client.request.forms.FormDataContent
 import io.ktor.client.request.get
 import io.ktor.client.request.put
 import io.ktor.http.Parameters
 import io.ktor.util.InternalAPI
 
-//TODO IMPORTANT, AFTER ADDING THE ENTITY RETURN TRIP INSTEAD OF TRIPDTO
 class MapRemoteGateway(client: HttpClient) : IMapRemoteGateway,
     BaseRemoteGateway(client = client) {
     override suspend fun sendLocation(location: LocationDto, tripId: String) {
@@ -22,16 +22,16 @@ class MapRemoteGateway(client: HttpClient) : IMapRemoteGateway,
         )
     }
 
-    override suspend fun getTripById(tripId: String): TripDto {
+    override suspend fun getTripById(tripId: String): Trip {
        val result = tryToExecute<BaseResponse<TripDto>> {
             get("/trip/$tripId")
         }.value ?: throw Exception()
 
-        return result
+        return result.toTripEntity()
     }
 
     @OptIn(InternalAPI::class)
-    override suspend fun approveTrip(taxiId: String, tripId: String, driverId: String): TripDto {
+    override suspend fun approveTrip(taxiId: String, tripId: String, driverId: String): Trip {
         val result = tryToExecute<BaseResponse<TripDto>> {
             val formData = FormDataContent(Parameters.build {
                 append("tripId", tripId)
@@ -43,11 +43,11 @@ class MapRemoteGateway(client: HttpClient) : IMapRemoteGateway,
             }
         }.value ?: throw Exception()
 
-        return result
+        return result.toTripEntity()
     }
 
     @OptIn(InternalAPI::class)
-    override suspend fun finishTrip(tripId: String, driverId: String): TripDto {
+    override suspend fun finishTrip(tripId: String, driverId: String): Trip {
         val result = tryToExecute<BaseResponse<TripDto>> {
             val formData = FormDataContent(Parameters.build {
                 append("tripId", tripId)
@@ -58,15 +58,7 @@ class MapRemoteGateway(client: HttpClient) : IMapRemoteGateway,
             }
         }.value ?: throw Exception()
 
-        return result
-    }
-
-    override suspend fun deleteTrip(tripId: String): TripDto {
-        val result = tryToExecute<BaseResponse<TripDto>> {
-            delete("trip/cancel$tripId")
-        }.value ?: throw Exception()
-
-        return result
+        return result.toTripEntity()
     }
 
 }
--- client_delivery/shared/src/commonMain/kotlin/domain/gateway/remote/IMapRemoteGateway.kt
+++ client_delivery/shared/src/commonMain/kotlin/domain/gateway/remote/IMapRemoteGateway.kt
@@ -1,14 +1,12 @@
 package domain.gateway.remote
 
 import data.remote.model.LocationDto
-import data.remote.model.TripDto
+import domain.entity.Trip
 
-//TODO IMPORTANT, AFTER ADDING THE ENTITY RETURN TRIP INSTEAD OF TRIPDTO
 
 interface IMapRemoteGateway {
     suspend fun sendLocation(location: LocationDto, tripId: String)
-    suspend fun getTripById(tripId: String): TripDto
-    suspend fun approveTrip(taxiId: String, tripId: String, driverId: String): TripDto
-    suspend fun finishTrip(tripId: String, driverId: String): TripDto
-    suspend fun deleteTrip(tripId: String): TripDto
+    suspend fun getTripById(tripId: String): Trip
+    suspend fun approveTrip(taxiId: String, tripId: String, driverId: String): Trip
+    suspend fun finishTrip(tripId: String, driverId: String): Trip
 }
