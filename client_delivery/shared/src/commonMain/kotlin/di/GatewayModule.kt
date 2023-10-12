7a991ea13acdb664c383ccbc1c868947d807ddc6
Aziza <aziza.helmy35@gmail.com>
2023-10-13 02:14:14 +0300

    Delivery app | inject Map screen

--- client_delivery/shared/src/commonMain/kotlin/di/GatewayModule.kt
+++ client_delivery/shared/src/commonMain/kotlin/di/GatewayModule.kt
@@ -4,10 +4,12 @@ import data.gateway.local.LocalConfigurationGateway
 import data.gateway.local.LocationGateway
 import data.gateway.remote.FakeRemoteGateway
 import data.gateway.remote.IdentityRemoteGateway
+import data.gateway.remote.MapRemoteGateway
 import domain.gateway.remote.IFakeRemoteGateway
 import domain.gateway.local.ILocalConfigurationGateway
 import domain.gateway.local.ILocationGateway
 import domain.gateway.remote.IIdentityRemoteGateway
+import domain.gateway.remote.IMapRemoteGateway
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -17,4 +19,5 @@ val gatewayModule = module {
     singleOf(::LocalConfigurationGateway) { bind<ILocalConfigurationGateway>() }
     singleOf(::IdentityRemoteGateway) { bind<IIdentityRemoteGateway>() }
     singleOf(::LocationGateway) { bind<ILocationGateway>() }
+    singleOf(::MapRemoteGateway) { bind<IMapRemoteGateway>() }
 }
