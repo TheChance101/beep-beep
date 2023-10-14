67bb075bebd8fc21ecd2d530be158a22a3780b5a
Aziza <aziza.helmy35@gmail.com>
2023-10-14 15:26:22 +0300

    Taxi app | fix login issue

--- client_taxi_driver/shared/src/commonMain/kotlin/di/GatewayModule.kt
+++ client_taxi_driver/shared/src/commonMain/kotlin/di/GatewayModule.kt
@@ -1,19 +1,18 @@
 package di
 
-import data.remote.gateway.IdentityRemoteGateway
-import data.remote.fakegateway.IdentityFakeGateway
-import domain.gateway.remote.IIdentityRemoteGateway
-import domain.gateway.local.ILocalConfigurationGateway
 import data.local.gateway.LocalConfigurationGateway
 import data.remote.fakegateway.MapFakeGateway
+import data.remote.gateway.IdentityRemoteGateway
 import domain.gateway.IMapGateway
+import domain.gateway.local.ILocalConfigurationGateway
+import domain.gateway.remote.IIdentityRemoteGateway
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val GatewayModule = module {
-//    singleOf(::IdentityRemoteGateway) { bind<IIdentityRemoteGateway>() }
-    singleOf(::IdentityFakeGateway) { bind<IIdentityRemoteGateway>() }
+    //singleOf(::IdentityFakeGateway) { bind<IIdentityRemoteGateway>() }
+    singleOf(::IdentityRemoteGateway) { bind<IIdentityRemoteGateway>() }
     singleOf(::LocalConfigurationGateway) { bind<ILocalConfigurationGateway>() }
     singleOf(::MapFakeGateway) { bind<IMapGateway>() }
 }
