224c04b1c6a071999a18caa69b6ac194a1383b36
Aziza <aziza.helmy35@gmail.com>
2023-10-14 18:11:25 +0300

    Taxi app | refactor login flag

---
+++ client_taxi_driver/shared/src/commonMain/kotlin/presentation/app/App.kt
@@ -0,0 +1,34 @@
+package presentation.app
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.koin.getScreenModel
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.transitions.SlideTransition
+import presentation.login.LoginScreen
+import presentation.main.MainScreen
+import presentation.resources.BpTaxiTheme
+
+@Composable
+fun App() {
+    MainApp.Content()
+}
+
+object MainApp : Screen {
+    @Composable
+    override fun Content() {
+        val appScreenModel = getScreenModel<AppScreenModel>()
+
+        val isKeptLoggedIn by appScreenModel.isKeptLoggedIn.collectAsState()
+
+        BpTaxiTheme {
+            if (isKeptLoggedIn) {
+                Navigator(MainScreen()) { SlideTransition(it) }
+            } else {
+                Navigator(LoginScreen()) { SlideTransition(it) }
+            }
+        }
+    }
+}
