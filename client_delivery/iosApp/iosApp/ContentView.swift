6cb170095ccf0fd7499c541c3883a006d19da53a
Aziza <aziza.helmy35@gmail.com>
2023-10-14 18:49:46 +0300

    Delivery app | stop screen orientation

--- client_delivery/iosApp/iosApp/ContentView.swift
+++ client_delivery/iosApp/iosApp/ContentView.swift
@@ -7,6 +7,12 @@ struct ComposeView: UIViewControllerRepresentable {
         Main_iosKt.MainViewController()
     }
 
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+    // Return a bitmask of supported orientations. If you need more,
+    // use bitwise or (see the commented return).
+    return UIInterfaceOrientationMaskPortrait;
+    // return UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskPortraitUpsideDown;
+}
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
