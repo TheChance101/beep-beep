a66014fa8433e913c3d4dd2870d51560bc024796
Nada Feteiha <nada.feteiha@gmail.com>
2023-10-17 13:16:35 -0700

    fix CategoriesRestaurants relation and mapper

--- service_restaurant/src/main/kotlin/org/thechance/service_restaurant/data/collection/mapper/CategoryMapper.kt
+++ service_restaurant/src/main/kotlin/org/thechance/service_restaurant/data/collection/mapper/CategoryMapper.kt
@@ -1,6 +1,7 @@
 package org.thechance.service_restaurant.data.collection.mapper
 
 import org.thechance.service_restaurant.data.collection.CategoryCollection
+import org.thechance.service_restaurant.data.collection.relationModels.CategoryDetails
 import org.thechance.service_restaurant.domain.entity.Category
 
 fun CategoryCollection.toEntity() = Category(
@@ -8,7 +9,15 @@ fun CategoryCollection.toEntity() = Category(
     name = name,
 )
 
+fun CategoryDetails.categoryDetailsToEntity() = Category(
+    id = id.toString(),
+    name = name,
+    restaurants = restaurants.toEntity()
+)
+
 fun List<CategoryCollection>.toEntity(): List<Category> = map { it.toEntity() }
 
+fun List<CategoryDetails>.categoryDetailsToEntity(): List<Category> = map { it.categoryDetailsToEntity() }
 
 fun Category.toCollection() = CategoryCollection(name = name)
+
