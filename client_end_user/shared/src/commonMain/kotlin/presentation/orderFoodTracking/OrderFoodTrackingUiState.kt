08b44a2f753f825f159d77b3707c870331fc6080
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-10-22 17:27:52 +0300

    add UserLocationUiState and DeliveryLocationUiState

--- client_end_user/shared/src/commonMain/kotlin/presentation/orderFoodTracking/OrderFoodTrackingUiState.kt
+++ client_end_user/shared/src/commonMain/kotlin/presentation/orderFoodTracking/OrderFoodTrackingUiState.kt
@@ -3,7 +3,15 @@ package presentation.orderFoodTracking
 data class OrderFoodTrackingUiState(
     val order: OrderUiState = OrderUiState(),
     val currentOrderStatus: FoodOrderStatus = FoodOrderStatus.ORDER_PLACED,
+    val userLocation: LocationUiState = LocationUiState(),
+    val deliveryLocation: LocationUiState = LocationUiState(),
 ) {
+
+    data class LocationUiState(
+        val latitude: Double = 0.0,
+        val longitude: Double = 0.0,
+    )
+
     data class OrderUiState(
         val estimatedTime: String = "32:00",
         val isOrderPlaced: Boolean = false,
