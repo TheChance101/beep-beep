ec86aeb9becdf8dade111d89d5512ef45989a3f9
Nada Feteiha <nada.feteiha@gmail.com>
2023-10-28 19:02:52 -0700

    fix issue in create trip

--- client_end_user/shared/src/commonMain/kotlin/presentation/orderHistory/OrderHistoryScreenModel.kt
+++ client_end_user/shared/src/commonMain/kotlin/presentation/orderHistory/OrderHistoryScreenModel.kt
@@ -41,7 +41,7 @@ class OrderHistoryScreenModel(
                 if (token.isNotEmpty()) {
                     updateState { it.copy(isLoggedIn = true) }
                     getOrdersHistory()
-//                    getTripsHistory()
+                    getTripsHistory()
                 } else {
                     updateState { it.copy(isLoggedIn = false) }
                 }
