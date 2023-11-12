2aee5a43d6496287219146c1cb917a29f5f28f16
Aziza <aziza.helmy35@gmail.com>
2023-11-12 22:51:24 +0200

    restaurant app | refactor the code

--- client_restaurant/shared/src/commonMain/kotlin/presentation/orderHistory/OrderHistoryScreenModel.kt
+++ client_restaurant/shared/src/commonMain/kotlin/presentation/orderHistory/OrderHistoryScreenModel.kt
@@ -20,32 +20,29 @@ class OrderHistoryScreenModel(
     }
 
     private fun getData() {
+        updateState { it.copy(isLoading = true) }
         tryToExecute(
-            {
-                ordersManagement.getOrdersHistory(restaurantId, 1, 10)
-            },
-            ::onOrdersSuccess,
-            ::onError
+            ::getOrdersHistory, ::onGetOrdersHistorySuccess, ::onError
         )
     }
 
-    private fun onOrdersSuccess(orders: List<Order>) {
+    private suspend fun getOrdersHistory(): List<Order> {
+        return ordersManagement.getOrdersHistory(restaurantId, 1, 10)//todo just for testing
+    }
+
+    private fun onGetOrdersHistorySuccess(orders: List<Order>) {
         val ordersUiState = orders.map { it.toOrderHistoryUiState() }
         updateState { currentState ->
-            currentState.copy(
+            currentState.copy(isLoading = false,
                 orders = ordersUiState,
                 currentOrders = ordersUiState.filter {
-                    currentState.selectedType == OrderHistoryScreenUiState.OrderSelectType.FINISHED &&
-                            it.orderState == OrderStatus.DONE ||
-                            currentState.selectedType == OrderHistoryScreenUiState.OrderSelectType.CANCELLED &&
-                            it.orderState == OrderStatus.CANCELED
-                }
-            )
+                    currentState.selectedType == OrderHistoryScreenUiState.OrderSelectType.FINISHED && it.orderState == OrderStatus.DONE || currentState.selectedType == OrderHistoryScreenUiState.OrderSelectType.CANCELLED && it.orderState == OrderStatus.CANCELED
+                })
         }
     }
 
     private fun onError(errorState: ErrorState) {
-        updateState { it.copy(errorState = errorState) }
+        updateState { it.copy(errorState = errorState, isLoading = false) }
     }
 
     override fun onClickBack() {
