d1a210839a106eb9c0828bdd6c9222ac69803f89
HassanAyman <hassan3ayman@gmail.com>
2023-11-14 20:19:47 +0200

    Taxi app | add functions to handle webSocket the receiving and sending Data

--- client_taxi_driver/shared/src/commonMain/kotlin/data/remote/gateway/BaseRemoteGateway.kt
+++ client_taxi_driver/shared/src/commonMain/kotlin/data/remote/gateway/BaseRemoteGateway.kt
@@ -1,15 +1,24 @@
 package data.remote.gateway
 
 import data.remote.model.BaseResponse
-import domain.InternetException
 import domain.InvalidPasswordException
 import domain.InvalidUserNameException
 import domain.NoInternetException
+import domain.SocketException
 import domain.UnknownErrorException
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.plugins.websocket.receiveDeserialized
+import io.ktor.client.plugins.websocket.sendSerialized
+import io.ktor.client.plugins.websocket.wss
 import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 
 abstract class BaseRemoteGateway(val client: HttpClient) {
@@ -23,10 +32,35 @@ abstract class BaseRemoteGateway(val client: HttpClient) {
             val errorMessages = e.response.body<BaseResponse<*>>().status?.errorMessages
             errorMessages?.let { throwMatchingException(it) }
             throw UnknownErrorException(e.message)
-        } catch (e: InternetException) {
+        } catch (e: UnresolvedAddressException) {
             throw NoInternetException()
         } catch (e: Exception) {
             throw UnknownErrorException(e.message.toString())
+        }
+    }
+    suspend inline fun <reified T> HttpClient.tryToExecuteWebSocket(path: String): Flow<T> {
+        return flow {
+            wss(path = path) {
+                while (true) {
+                    try {
+                        emit(receiveDeserialized<T>())
+                    } catch (e: Exception) {
+                        throw SocketException(e.message.orEmpty())
+                    }
+                }
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+    suspend inline fun <reified T> HttpClient.tryToSendWebSocketData(
+        data: T,
+        path: String,
+    ) {
+        wss(path) {
+            try {
+                sendSerialized(data)
+            } catch (e: Exception) {
+                throw SocketException(e.message.orEmpty())
+            }
         }
     }
 
