519c92e90b46fbbb2a2b6c179d2ba8f26bcea093
Nada Feteiha <nada.feteiha@gmail.com>
2023-11-15 04:00:07 -0800

    add resources

--- client_end_user/shared/src/commonMain/kotlin/resources/strings/ar/Arabic.kt
+++ client_end_user/shared/src/commonMain/kotlin/resources/strings/ar/Arabic.kt
@@ -117,4 +117,5 @@ data class Arabic(
     override val setDestination: String = "ضع المكان ",
     override val searchDestination: String = "ابحث عن مكان",
     override val trackYourRide: String = "تتبع رحلتك",
+    override val restaurants: String ="المطاعم"
 ) : IStringResources
