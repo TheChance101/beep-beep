87865c0ab9b1a9d133a84194a0e4e2fc811d3ff4
Nada Feteiha <nada.feteiha@gmail.com>
2023-11-15 03:31:12 -0800

    add restaurants to use-case

--- client_end_user/shared/src/commonMain/kotlin/domain/usecase/ExploreRestaurantUseCase.kt
+++ client_end_user/shared/src/commonMain/kotlin/domain/usecase/ExploreRestaurantUseCase.kt
@@ -4,6 +4,7 @@ import app.cash.paging.Pager
 import app.cash.paging.PagingConfig
 import app.cash.paging.PagingData
 import data.gateway.remote.pagesource.MealsPagingSource
+import data.gateway.remote.pagesource.RestaurantsPagingSource
 import domain.entity.Cuisine
 import domain.entity.Meal
 import domain.entity.Restaurant
@@ -11,6 +12,7 @@ import domain.gateway.IRestaurantGateway
 import kotlinx.coroutines.flow.Flow
 
 interface IExploreRestaurantUseCase {
+    suspend fun getRestaurants(): Flow<PagingData<Restaurant>>
     suspend fun getRestaurantDetails(restaurantId: String): Restaurant
     suspend fun getMealById(mealId: String): Meal
     suspend fun getCuisines(): List<Cuisine>
@@ -21,6 +23,7 @@ interface IExploreRestaurantUseCase {
 class ExploreRestaurantUseCase(
     private val restaurantGateway: IRestaurantGateway,
     private val mealDataSource: MealsPagingSource,
+    private val restaurants: RestaurantsPagingSource,
 ) : IExploreRestaurantUseCase {
 
 
@@ -34,6 +37,12 @@ class ExploreRestaurantUseCase(
 
     override suspend fun getCuisines(): List<Cuisine> {
         return restaurantGateway.getCuisines()
+    }
+
+    override suspend fun getRestaurants(): Flow<PagingData<Restaurant>> {
+        return Pager(config = PagingConfig(pageSize = 10),
+            pagingSourceFactory = { restaurants }
+        ).flow
     }
 
     override suspend fun getMealsInCuisine(cuisineId: String): Flow<PagingData<Meal>> {
