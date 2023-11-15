e6b9866f014d7b6ee8356c4559205f3d03915b89
NourElden <nourelden515@gmail.com>
2023-11-15 23:36:20 +0200

    fix image url when update restaurant

--- api_gateway/src/main/kotlin/org/thechance/api_gateway/data/service/ImageService.kt
+++ api_gateway/src/main/kotlin/org/thechance/api_gateway/data/service/ImageService.kt
@@ -2,6 +2,7 @@ package org.thechance.api_gateway.data.service
 
 import aws.sdk.kotlin.runtime.auth.credentials.EnvironmentCredentialsProvider
 import aws.sdk.kotlin.services.s3.S3Client
+import aws.sdk.kotlin.services.s3.model.DeleteObjectRequest
 import aws.sdk.kotlin.services.s3.model.ObjectCannedAcl
 import aws.sdk.kotlin.services.s3.model.PutObjectRequest
 import aws.smithy.kotlin.runtime.content.ByteStream
@@ -10,13 +11,16 @@ import org.koin.core.annotation.Single
 
 @Single
 class ImageService {
-    suspend fun uploadImage(image: ByteArray, name: String): String {
-        val fileName = "${name.replace(" ", "_")}.png"
+    suspend fun uploadImage(
+        image: ByteArray,
+        name: String,
+        oldImageUrl: String? = null
+    ): String {
+        val fileName = "${name.replace(" ", "_")}${Math.random()}.png"
         val bucketName = "beepbeep-resource"
         val imageUrl = "https://beepbeep-resource.fra1.digitaloceanspaces.com"
 
         val metadataVal = mutableMapOf<String, String>()
-        metadataVal["myVal"] = "test"
 
         val s3Client = S3Client.fromEnvironment {
             endpointUrl = Url.parse("https://fra1.digitaloceanspaces.com")
@@ -24,7 +28,16 @@ class ImageService {
             credentialsProvider = EnvironmentCredentialsProvider()
 
         }
-        val request = PutObjectRequest {
+
+        if (oldImageUrl != null) {
+            val deleteObjectRequest = DeleteObjectRequest {
+                bucket = bucketName
+                key = oldImageUrl.replace("$imageUrl/", "")
+            }
+            val deleteObjectResponse = s3Client.deleteObject(deleteObjectRequest)
+        }
+
+        val putObjectRequest = PutObjectRequest {
             bucket = bucketName
             key = fileName
             acl = ObjectCannedAcl.fromValue("public-read")
@@ -32,8 +45,8 @@ class ImageService {
             body = ByteStream.fromBytes(image)
         }
 
-        val response = s3Client.putObject(request)
-        println("Tag information is ${response.eTag}")
+        val putObjectResponse = s3Client.putObject(putObjectRequest)
+        println("Tag information is ${putObjectResponse.eTag}")
 
         return "$imageUrl/$fileName"
     }
