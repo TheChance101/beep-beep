90de1145a18b60139102ae8d8b3ca667d1cde8c9
NourElden <nourelden515@gmail.com>
2023-11-27 17:04:50 +0200

    restaurant app | add models and mappers

---
+++ client_restaurant/shared/src/commonMain/kotlin/data/remote/model/CuisineWithMeals.kt
@@ -0,0 +1,12 @@
+package data.remote.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CuisineWithMealsDto(
+    @SerialName("id") val id: String,
+    @SerialName("name") val name: String,
+    @SerialName("image") val image: String? = null,
+    @SerialName("meals") val meals: List<MealDto>? = null,
+)
