337d74392998551f4685e2611416e01b7b15a3f2
Nada Feteiha <nada.feteiha@gmail.com>
2023-11-27 09:17:00 -0800

    fix end user ios

--- client_end_user/shared/src/iosMain/kotlin/FireBaseMessageService.kt
+++ client_end_user/shared/src/iosMain/kotlin/FireBaseMessageService.kt
@@ -1,15 +1,10 @@
 import cocoapods.FirebaseMessaging.FIRMessaging
 import data.gateway.service.IFireBaseMessageService
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
 
 class FireBaseMessageService : IFireBaseMessageService {
 
-    override suspend fun getDeviceToken(): String = suspendCoroutine { cont ->
-        FIRMessaging.messaging().tokenWithCompletion { token, error ->
-            cont.resume(token ?: "null token")
-        }
+    override suspend fun getDeviceToken(): String {
+        return FIRMessaging.messaging().FCMToken.toString()
     }
-
 }
 
